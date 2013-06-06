ed8709f1206fb0c4ebfc1da5f3114942f81e4ff3
Katrina Owen <katrina.owen@gmail.com>
2013-06-05 19:44:23 -0500

    Refactor curriculum tests

--- test/exercism/curriculum_test.rb
+++ test/exercism/curriculum_test.rb
@@ -13,27 +13,24 @@ class CurriculumTest < MiniTest::Unit::TestCase
   attr_reader :curriculum
   def setup
     @curriculum = Curriculum.new('./test/fixtures')
+    nong = Language.new('nong', 'no', 'not')
+    curriculum.add(nong, %w(one two))
   end
 
   def test_curriculum_takes_a_path
     assert_equal './test/fixtures', curriculum.path
   end
 
-  def test_add_trail_to_curriculum
-    nong = Language.new('nong', 'no', 'not')
-    curriculum.add(nong, %w(one two))
-
+  def test_find_exercise_in_trail
     ex = Exercise.new('nong', 'one')
     assert_equal ex, curriculum.in('nong').find('one')
   end
 
   def test_get_assignment_from_trail
-    nong = Language.new('nong', 'no', 'not')
-    curriculum.add(nong, %w(one two))
-
     path = './test/fixtures/nong/one'
     assignment = curriculum.in('nong').assign('one')
     assert_equal path, assignment.path
   end
 
 end
+
