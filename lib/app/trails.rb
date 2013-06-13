1f2bd5fbe9cfea506237b4a145c23b62f79d37cf
Katrina Owen <katrina.owen@gmail.com>
2013-06-13 07:23:04 -0600

    Redirect to instructions
    If a participant starts a trail and has no submissions, redirect to the
    account page so that they can get the gem and their API key.

--- lib/app/trails.rb
+++ lib/app/trails.rb
@@ -3,7 +3,11 @@ class ExercismApp < Sinatra::Base
   post '/user/:trail/start' do |language|
     begin
       Launch.new(current_user, language).start
-      redirect '/'
+      if current_user.submissions.count == 0
+        redirect '/account'
+      else
+        redirect '/'
+      end
     rescue
       halt 400, "Language #{language} is not supported at this time."
     end
