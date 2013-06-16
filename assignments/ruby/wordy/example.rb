8456142c4c5babae7b418c7f87365c8d6a4b743e
Chuck Lauer Vose <vosechu@gmail.com>
2013-06-16 10:38:53 -0700

    combine robot examples into robot-simulator

--- assignments/ruby/wordy/example.rb
+++ assignments/ruby/wordy/example.rb
@@ -1,12 +1,3 @@
-# # Step 1 version
-# class Calculator
-#   def ask(question)
-#     question.match(/What is (-?\d+) plus (-?\d+)?/) do |m|
-#       m[1].to_i + m[2].to_i
-#     end
-#   end
-# end
-
 class WordProblem
   attr_reader :question
   def initialize(question)
