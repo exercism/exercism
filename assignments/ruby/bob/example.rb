0766891585c6f098fb7e2daf9946968a1df8f878
Katrina Owen <katrina.owen@gmail.com>
2013-06-20 08:20:54 -0600

    Reorder ruby:bob example
    Make Bob come first (simple, stright-forward) and Alice come afterwards
    (slightly over-engineered).

--- assignments/ruby/bob/example.rb
+++ assignments/ruby/bob/example.rb
@@ -1,3 +1,33 @@
+class Bob
+
+  def hey(something)
+    if silent?(something)
+      'Fine. Be that way.'
+    elsif question?(something)
+      'Sure.'
+    elsif shouting?(something)
+      'Woah, chill out!'
+    else
+      'Whatever.'
+    end
+  end
+
+  private
+
+  def question?(s)
+    s.end_with?('?')
+  end
+
+  def silent?(s)
+    s.empty?
+  end
+
+  def shouting?(s)
+    s.upcase == s
+  end
+
+end
+
 class AnswerSilence
 
   def self.handles?(input)
@@ -60,36 +90,6 @@ class Alice
 
   def handlers
     [AnswerSilence, AnswerQuestion, AnswerShout, AnswerDefault]
-  end
-
-end
-
-class Bob
-
-  def hey(something)
-    if silent?(something)
-      'Fine. Be that way.'
-    elsif question?(something)
-      'Sure.'
-    elsif shouting?(something)
-      'Woah, chill out!'
-    else
-      'Whatever.'
-    end
-  end
-
-  private
-
-  def question?(s)
-    s.end_with?('?')
-  end
-
-  def silent?(s)
-    s.empty?
-  end
-
-  def shouting?(s)
-    s.upcase == s
   end
 
 end
