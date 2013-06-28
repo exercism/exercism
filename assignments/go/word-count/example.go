b4bd7d12b0a5b4cc927466b00a416fd9e94dee4f
Katrina Owen <katrina.owen@gmail.com>
2013-06-28 13:53:58 -0600

    Add go:word-count

---
+++ assignments/go/word-count/example.go
@@ -0,0 +1,40 @@
+package wc
+
+import (
+	"strings"
+	"regexp"
+)
+
+type Histogram map[string]int
+
+func (h Histogram) Equal(o Histogram) bool {
+	return h.sameLength(o) && h.sameMappings(o)
+}
+
+func (h Histogram) sameLength(o Histogram) bool {
+	return len(h) == len(o)
+}
+
+func (h Histogram) sameMappings(o Histogram) (res bool) {
+	res = true
+	for k, _ := range h {
+		if h[k] != o[k] {
+			res = false
+		}
+	}
+	return
+}
+
+func WordCount(phrase string) Histogram {
+	counts := make(Histogram)
+	for _, word := range strings.Fields(normalize(phrase)) {
+		counts[word]++
+	}
+	return counts
+}
+
+func normalize(phrase string) string {
+	r, _ := regexp.Compile(`[^\w]`)
+	phrase = strings.ToLower(phrase)
+	return r.ReplaceAllLiteralString(phrase, " ")
+}
---
+++ assignments/go/word-count/word-count_test.go
@@ -0,0 +1,68 @@
+package wc
+
+import "testing"
+
+func assertEqual(t *testing.T, actual Histogram, expected Histogram) {
+	if !actual.Equal(expected) {
+		t.Errorf("Expected %v to equal %v", actual, expected)
+	}
+}
+
+func assertNotEqual(t *testing.T, h Histogram, other Histogram) {
+	if h.Equal(other) || other.Equal(h) {
+		t.Errorf("Expected %v to not equal %v", h, other)
+	}
+}
+
+func TestEqual(t *testing.T) {
+	h1 := Histogram{"hello": 1, "world": 2}
+	h2 := Histogram{"hello": 1, "world": 2}
+	assertEqual(t, h1, h2)
+}
+
+func TestNotEqual(t *testing.T) {
+	t.SkipNow()
+	h1 := Histogram{"word": 1}
+	h2 := Histogram{"word": 1, "games": 2}
+	assertNotEqual(t, h1, h2)
+}
+
+func TestCountOneWord(t *testing.T) {
+	t.SkipNow()
+	h := Histogram{"word": 1}
+	assertEqual(t, WordCount("word"), h)
+}
+
+func TestCountOneOfEach(t *testing.T) {
+	t.SkipNow()
+	h := Histogram{"one": 1, "of": 1, "each": 1}
+	assertEqual(t, WordCount("one of each"), h)
+}
+
+func TestCountMultipleOccurrences(t *testing.T) {
+	t.SkipNow()
+	actual := WordCount("one fish two fish red fish blue fish")
+	expected := Histogram{"one": 1, "fish": 4, "two": 1, "red": 1, "blue": 1}
+	assertEqual(t, actual, expected)
+}
+
+func TestIgnorePunctuation(t *testing.T) {
+	t.SkipNow()
+	actual := WordCount("car : carpet as java : javascript!!&@$%^&")
+	expected := Histogram{"car": 1, "carpet": 1, "as": 1, "java": 1, "javascript": 1}
+	assertEqual(t, actual, expected)
+}
+
+func TestIncludeNumbers(t *testing.T) {
+	t.SkipNow()
+	actual := WordCount("testing, 1, 2 testing")
+	expected := Histogram{"testing": 2, "1": 1, "2": 1}
+	assertEqual(t, actual, expected)
+}
+
+
+func TestNormalizeCase(t *testing.T) {
+	t.SkipNow()
+	assertEqual(t, WordCount("go Go GO"), Histogram{"go": 3})
+}
+
