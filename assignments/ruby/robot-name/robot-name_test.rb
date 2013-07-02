ad30d7a33ab702dc93bfd91a12c439ca903010f6
Katrina Owen <katrina.owen@gmail.com>
2013-07-01 20:55:02 -0600

    Improve test suite for ruby:robot-name

--- assignments/ruby/robot-name/robot-name_test.rb
+++ assignments/ruby/robot-name/robot-name_test.rb
@@ -8,24 +8,22 @@ class RobotTest < MiniTest::Unit::TestCase
   end
 
   def test_name_sticks
-    skip
     robot = Robot.new
     robot.name
     assert_equal robot.name, robot.name
   end
 
   def test_different_robots_have_different_names
-    skip
     assert Robot.new.name != Robot.new.name
   end
 
   def test_reset_name
-    skip
     robot = Robot.new
     name = robot.name
     robot.reset
     name2 = robot.name
     assert name != name2
+    assert_match /\w{2}\d{3}/, name2
   end
 end
 
