0b79b25134782c7e2acbc30e4ec064330e17c4fc
Katrina Owen <katrina.owen@gmail.com>
2013-07-02 17:51:26 -0600

    Move approvaltests config into integration helper

--- test/api_helper.rb
+++ test/api_helper.rb
@@ -3,11 +3,6 @@ require './test/integration_helper'
 require 'sinatra/base'
 require 'rack/test'
 require 'rack-flash'
-require 'approvals'
 
 require 'app'
-
-Approvals.configure do |c|
-  c.approvals_path = './test/fixtures/approvals/'
-end
 
--- test/integration_helper.rb
+++ test/integration_helper.rb
@@ -1,3 +1,9 @@
 require './test/mongo_helper'
 
+require 'approvals'
 require 'exercism'
+
+Approvals.configure do |c|
+  c.approvals_path = './test/fixtures/approvals/'
+end
+
