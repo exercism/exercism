8338b415f6312e78bc2e602e37b25b411a732288
Katrina Owen <katrina.owen@gmail.com>
2013-07-02 08:35:06 -0600

    Refactor dispatch test

--- test/services/dispatch_test.rb
+++ test/services/dispatch_test.rb
@@ -3,21 +3,22 @@ require_relative "../integration_helper"
 require 'services'
 
 class DispatchTest < MiniTest::Unit::TestCase
+  attr_reader :submission, :admin
   def setup
     exercise = Exercise.new('nong', 'one')
     @submission = Submission.on(exercise)
     @submission.user = User.create(
-      github_id: 84274928,
+      github_id: 1,
       email: "bobguy1998@example.com",
       username: "AwesomeBob",
     )
     @submission.save
-    current_user = User.create(
+
+    @admin = User.create(
       github_id: 1337,
-      email: "self@alice.com",
-      username: "l@@kinggl@ss",
+      email: "admin@example.com",
+      username: "kytrinyx",
     )
-    @nitpick = Nitpick.new(@submission.id, current_user, "Needs more monads").save
   end
 
   def teardown
@@ -25,14 +26,14 @@ class DispatchTest < MiniTest::Unit::TestCase
   end
 
   def test_send_email_upon_nitpick
+    nitpick = Nitpick.new(@submission.id, admin, "Needs more monads").save
     dispatch = Dispatch.new_nitpick(
-      nitpick: @nitpick,
+      nitpick: nitpick,
       intercept_emails: true,
       site_root: 'http://test.exercism.io'
     )
-    user = @submission.user
-    assert_equal dispatch.name, user.username
-    url = "http://test.exercism.io/user/submissions/#{@submission.id}"
+    assert_equal dispatch.name, submission.user.username
+    url = "http://test.exercism.io/user/submissions/#{submission.id}"
     assert_equal url, dispatch.submission_url
     assert /nitpick/ =~ dispatch.subject, "Expected subject to match /nitpick/"
   end
