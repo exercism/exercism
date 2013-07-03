dbefb84a164e55a384eab727d0dbdf6b8db40a38
Mike Gehard <mike.gehard@gmail.com>
2013-07-02 21:23:55 -0600

    Binary example in Go

---
+++ assignments/go/binary/binary_test.go
@@ -0,0 +1,23 @@
+package binary
+
+import "testing"
+
+type binaryTest struct {
+	binary        string
+	expected      int
+}
+
+var binaryTests = []binaryTest {
+	{"1", 1}, {"10", 2}, {"11", 3},
+	{"100", 4}, {"1001", 9}, {"10001101000", 1128},
+	{"12", 0},
+}
+
+func TestBinary(t *testing.T) {
+	for _, test := range binaryTests {
+		actual := ToDecimal(test.binary)
+		if actual != test.expected {
+			t.Errorf("ToDecimal(%d): expected %d, actual %d", test.binary, test.expected, actual)
+		}
+	}
+}
