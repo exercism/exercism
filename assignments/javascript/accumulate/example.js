99e3248d345547606478b24ff2dad229463bdc7d
Wil Moore III <wil.moore@wilmoore.com>
2013-07-05 05:53:56 -0600

    add guarded (feature detected) `map` polyfill

--- assignments/javascript/accumulate/example.js
+++ assignments/javascript/accumulate/example.js
@@ -1,3 +1,15 @@
 Array.prototype.accumulate = function (accumulator) {
-  return this.map(accumulator);
+  if (typeof Array.prototype.map == 'function') {
+    return this.map(accumulator);
+  }
+
+  var idx = 0;
+  var out = [];
+  var end = this.length;
+
+  for (; idx < end; ++idx) {
+    out.push(accumulator(this[idx]));
+  }
+
+  return out;
 };
