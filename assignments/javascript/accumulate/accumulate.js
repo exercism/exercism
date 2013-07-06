623d8355032379ddfb1d34c99ef8f2209f2efa02
Zach <briggszj@gmail.com>
2013-07-05 21:29:20 -0400

    Fix up Zach's messy rebase

--- assignments/javascript/accumulate/accumulate.js
+++
@@ -1,8 +0,0 @@
-Array.prototype.accumulate = function(modify){
-  var accumulator = [];
-
-  this.forEach(function(element){
-    accumulator.push(modify(element));
-  });
-  return accumulator;
-};
