8c953cdf8960804a2480646582c4c941e99b6182
Wil Moore III <wil.moore@wilmoore.com>
2013-07-06 21:48:30 -0600

    add JavaScript binary search tree assignment
    - add `example.js` and `binary-search-tree_test.spec.js`
    - JSHint is happy
    - all tests are passing
    - 2nd and subsequent tests skipped

---
+++ assignments/javascript/binary-search-tree/example.js
@@ -0,0 +1,43 @@
+function BinarySearchTree(data) {
+  this.data  = data;
+  this.left  = undefined;
+  this.right = undefined;
+}
+
+BinarySearchTree.prototype.insert = function(value) {
+  if (value <= this.data) {
+    this.insertLeft(value);
+  } else {
+    this.insertRight(value);
+  }
+
+  return this;
+};
+
+BinarySearchTree.prototype.insertLeft = function(value) {
+  if (!this.left) {
+    this.left = new BinarySearchTree(value);
+  } else {
+    this.left.insert(value);
+  }
+
+  return this;
+};
+
+BinarySearchTree.prototype.insertRight = function(value) {
+  if (!this.right) {
+    this.right = new BinarySearchTree(value);
+  } else {
+    this.right.insert(value);
+  }
+
+  return this;
+};
+
+BinarySearchTree.prototype.each = function(fn) {
+  if (this.left)  { this.left.each(fn);  }
+  fn.call(this, this.data);
+  if (this.right) { this.right.each(fn); }
+};
+
+module.exports = BinarySearchTree;
