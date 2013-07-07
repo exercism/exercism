696aedac3db5de4ccd9f78ff9f14d3c63a7a1b7c
Katrina Owen <katrina.owen@gmail.com>
2013-07-07 10:55:55 -0600

    Make 'how to nitpick' publicly available

--- lib/app/nitpick.rb
+++ lib/app/nitpick.rb
@@ -1,10 +1,6 @@
 class ExercismApp < Sinatra::Base
 
   get '/nitpick' do
-    unless current_user.nitpicker?
-      flash[:error] = "Finish at least one exercise, and we'll tell you all about nitpicking!"
-      redirect '/'
-    end
     erb :"about/nitpick"
   end
 
