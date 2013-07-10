ca4d52235d824228d60290335a9c099477f8e47b
keighty <k80.leonard@gmail.com>
2013-07-10 14:42:17 -0700

    skips all but first test

--- assignments/javascript/scrabble-score/scrabble-score_test.spec.js
+++ assignments/javascript/scrabble-score/scrabble-score_test.spec.js
@@ -6,27 +6,27 @@ describe('Scrabble', function() {
     expect(score).toEqual(0);
   });
 
-  it("scores a null as zero",function() {
+  xit("scores a null as zero",function() {
     var score = Scrabble.score(null);
     expect(score).toEqual(0);
   });
 
-  it("scores a very short word",function() {
+  xit("scores a very short word",function() {
     var score = Scrabble.score("a");
     expect(score).toEqual(1);
   });
 
-  it("scores the word by the number of letters",function() {
+  xit("scores the word by the number of letters",function() {
     var score = Scrabble.score("street");
     expect(score).toEqual(6);
   });
 
-  it("scores more complicated words with more",function() {
+  xit("scores more complicated words with more",function() {
     var score = Scrabble.score("quirky");
     expect(score).toEqual(22);
   });
 
-  it("scores case insensitive words",function() {
+  xit("scores case insensitive words",function() {
     var score = Scrabble.score("MULTIBILLIONAIRE");
     expect(score).toEqual(20);
   });
