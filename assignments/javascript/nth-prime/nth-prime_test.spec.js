4f6a0cfecf70509755dc9b71be9bf6d255e338a1
keighty <k80.leonard@gmail.com>
2013-07-10 14:37:12 -0700

    skips all but first test

--- assignments/javascript/nth-prime/nth-prime_test.spec.js
+++ assignments/javascript/nth-prime/nth-prime_test.spec.js
@@ -7,22 +7,22 @@ describe('Prime',function() {
     expect(prime).toEqual(2);
   });
 
-  it('second',function(){
+  xit('second',function(){
     var prime = Prime.nth(2);
     expect(prime).toEqual(3);
   });
 
-  it('sixth',function(){
+  xit('sixth',function(){
     var prime = Prime.nth(6);
     expect(prime).toEqual(13);
   });
 
-  it('big prime',function(){
+  xit('big prime',function(){
     var prime = Prime.nth(10001);
     expect(prime).toEqual(104743);
   });
 
-  it('weird case',function() {
+  xit('weird case',function() {
     try {
       Prime.nth(0);
     } catch(error) {
