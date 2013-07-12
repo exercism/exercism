0aee2bb8cfb9d069f564d76e59e1c16c52fd2716
Wil Moore III <wil.moore@wilmoore.com>
2013-07-11 22:14:49 -0600

    add Trinary JavaScript assignment
    - add `example.js` and `trinary_test.spec.js`
    - JSHint is happy
    - all tests passing
    - 2nd and subsequent tests skipped

---
+++ assignments/javascript/trinary/example.js
@@ -0,0 +1,14 @@
+module.exports = Trinary;
+
+function Trinary(decimal) {
+  this.digits = decimal.split('').reverse().map(Number);
+}
+
+Trinary.prototype.toDecimal = function() {
+  var decimal = this.digits.reduce(this.accumulator, 0);
+  return isNaN(decimal) ? 0 : decimal;
+};
+
+Trinary.prototype.accumulator = function(decimal, digit, index) {
+  return decimal += digit * Math.pow(3, index);
+};
