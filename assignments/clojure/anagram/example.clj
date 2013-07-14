0042837c3f2cf8b2bfb52fd3231889cf6048ba43
Solomon White <rubysolo@gmail.com>
2013-07-14 07:43:03 -0600

    add anagram assignment to clojure track

---
+++ assignments/clojure/anagram/example.clj
@@ -0,0 +1,14 @@
+(ns anagram
+  (:require [clojure.string :refer [split lower-case]]))
+
+(defn- canonicalize
+  [word]
+  (-> word
+      lower-case
+      (split #"")
+      sort))
+
+(defn anagrams-for
+  [word candidates]
+  (let [canonical (canonicalize word)]
+    (vec (filter (fn [c] (= canonical (canonicalize c))) candidates))))
