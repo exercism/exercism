3411799fe7651b9ff54f5fc38a4f72edcafdc81e
Peter Suschlik <peter@suschlik.de>
2013-07-17 21:34:12 +0200

    Test User.

--- test/exercism/user_test.rb
+++ test/exercism/user_test.rb
@@ -1,10 +1,12 @@
 require './test/mongo_helper'
 
 require 'exercism/user'
+require 'exercism/null_submission'
 require 'exercism/exercise'
 require 'exercism/locale'
 require 'exercism/trail'
 require 'exercism/submission'
+require 'exercism/nit'
 
 class UserTest < MiniTest::Unit::TestCase
 
@@ -20,6 +22,11 @@ class UserTest < MiniTest::Unit::TestCase
     user1 = User.new(attributes)
     user2 = User.new(attributes)
     assert_equal user1, user2
+  end
+
+  def test_user_create_key
+    user = User.new
+    assert_match %r{\A[a-z0-9]{40}\z}, user.key
   end
 
   def test_user_on_a_single_trail
@@ -89,6 +96,130 @@ class UserTest < MiniTest::Unit::TestCase
     two = Exercise.new('nong', 'two')
     assert !user.may_nitpick?(one)
     assert !user.may_nitpick?(two)
+  end
+
+  def test_user_not_a_guest
+    user = User.new
+    refute user.guest?
+  end
+
+  def test_user_current_languages
+    user = User.new(current: {'nong' => 'one', 'femp' => 'two'})
+    assert_equal %w(nong femp).sort, user.current_languages.sort
+  end
+
+  def test_user_is?
+    user = User.new(username: 'burtlo')
+    assert user.is?('burtlo')
+    refute user.is?('alice')
+  end
+
+  def test_user_without_submissions_is_new
+    user = User.new
+    assert user.new?
+  end
+
+  def test_user_with_submissions_isnt_new
+    user = User.new
+    def user.submissions; [:some]; end
+    refute user.new?
+  end
+
+  def test_admin_isnt_new
+    admin = User.new(is_admin: true)
+    refute admin.new?
+  end
+
+  def test_create_user_from_github
+    user = User.from_github(23, 'alice', 'alice@example.com', 'avatar_url')
+    assert_equal 1, User.count
+    assert_equal 23, user.github_id
+    assert_equal 'alice', user.username
+    assert_equal 'alice@example.com', user.email
+    assert_equal 'avatar_url', user.avatar_url
+  end
+
+  def test_sets_avatar_url_unless_present
+    User.create(github_id: 23)
+    user = User.from_github(23, nil, nil, 'new?1234').reload
+    assert_equal 'new', user.avatar_url
+  end
+
+  def test_does_not_overwrite_avatar_url_if_present
+    User.create(github_id: 23, avatar_url: 'old')
+    user = User.from_github(23, nil, nil, 'new?1234').reload
+    assert_equal 'old', user.avatar_url
+  end
+
+  def test_admin_is_nitpicker
+    admin = User.new(is_admin: true)
+    assert admin.nitpicker?
+  end
+
+  def test_user_with_completed_exercises_is_nitpicker
+    user = User.new
+    def user.completed; [:some]; end
+    assert user.nitpicker?
+  end
+
+  def test_user_no_ongoing_without_exercises
+    user = User.new
+    assert_equal [], user.ongoing
+  end
+
+  def test_user_ongoing_without_submission
+    user = User.new(current: {'nong' => 'one'})
+    assert_equal [false], user.ongoing.map(&:submitted?)
+  end
+
+  def test_user_ongoing_with_submissions
+    user = User.create(current: {'nong' => 'one'})
+    exercise = Exercise.new('nong', 'one')
+
+    user.submissions << create_submission(exercise, :code => "s1")
+    user.submissions << create_submission(exercise, :code => "s2")
+    user.save
+
+    assert_equal [true], user.ongoing.map(&:submitted?)
+    assert_equal ["s2"], user.ongoing.map(&:code)
+  end
+
+  def test_user_none_done_without_exercises
+    user = User.new
+    assert_equal [], user.done
+  end
+
+  def test_user_done_without_submissions
+    user = User.create(current: {'nong' => 'one'}, completed: {'nong' => ['one']})
+    assert_equal [false], user.done.map(&:submitted?)
+  end
+
+  def test_user_done_with_submissions
+    user = User.create(current: {'nong' => 'one'}, completed: {'nong' => ['one']})
+    exercise = Exercise.new('nong', 'one')
+
+    user.submissions << create_submission(exercise, :code => "s1")
+    user.submissions << create_submission(exercise, :code => "s2")
+    user.save
+
+    assert_equal [true], user.done.map(&:submitted?)
+    assert_equal ["s2"], user.done.map(&:code)
+  end
+
+  def test_user_do!
+    user = User.new
+    exercise = Exercise.new('nong', 'one')
+
+    user.do!(exercise)
+    assert_equal({'nong' => 'one'}, user.reload.current)
+  end
+
+  private
+
+  def create_submission(exercise, attributes={})
+    submission = Submission.on(exercise)
+    attributes.each { |key, value| submission[key] = value }
+    submission
   end
 
 end
