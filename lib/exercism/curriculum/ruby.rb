a3b25c71258ebc7fbc2e76fc2b05018ce207a923
Katrina Owen <katrina.owen@gmail.com>
2013-07-16 23:03:18 -0600

    Move rna-transcription to 2nd place in ruby

--- lib/exercism/curriculum/ruby.rb
+++ lib/exercism/curriculum/ruby.rb
@@ -2,8 +2,8 @@ class Exercism
   class RubyCurriculum
     def slugs
       %w(
-        bob word-count anagram beer-song nucleotide-count
-        rna-transcription point-mutations phone-number
+        bob rna-transcription word-count anagram beer-song
+        nucleotide-count point-mutations phone-number
         grade-school robot-name leap etl meetup space-age grains
         gigasecond triangle scrabble-score roman-numerals
         binary prime-factors raindrops allergies strain
