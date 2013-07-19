f290ec752d65997f1dabfa1125082e65d9b46238
Wil Moore III <wil.moore@wilmoore.com>
2013-07-19 00:51:36 -0600

    add JavaScript `wordy` assignment
    - add `example.js` and `wordy_test.spec.js`
    - all tests passing
    - 2nd and subsequent tests skipped

---
+++ assignments/javascript/wordy/example.js
@@ -0,0 +1,66 @@
+exports.WordProblem   = WordProblem;
+exports.ArgumentError = ArgumentError;
+
+var BINARY_OPERATORS = {
+  'plus':          function(l, r) { return l + r; },
+  'minus':         function(l, r) { return l - r; },
+  'multiplied by': function(l, r) { return l * r; },
+  'divided by':    function(l, r) { return l / r; }
+};
+
+function operators() {
+  return Object.keys(BINARY_OPERATORS);
+}
+
+function pattern() {
+  var expression = '';
+  var operations = ' (' + operators().join('|') + ') ';
+
+  expression += '(?:what is ([-+]?[\\d]+)';
+  expression += operations;
+  expression += '([-+]?[\\d]+)(?:';
+  expression += operations;
+  expression += '([-+]?[\\d]+))?)';
+
+  return new RegExp(expression, 'i');
+}
+
+function WordProblem(question) {
+  this.question = question || '';
+  this.matches  = this.question.match(pattern());
+}
+
+WordProblem.prototype.tooComplicated = function() {
+  return this.matches === null;
+};
+
+WordProblem.prototype.answer = function() {
+  if (this.tooComplicated()) {
+    throw new ArgumentError("I don't understand the question");
+  }
+
+  return this.evaluate();
+};
+
+WordProblem.prototype.evaluate = function() {
+  var out = 0;
+  var m   = this.matches;
+
+  if (m[1] !== undefined && m[2] !== undefined && m[3] !== undefined) {
+    out = this.operate(m[2], m[1], m[3]);
+  }
+
+  if (m[4] !== undefined && m[5] !== undefined) {
+    out = this.operate(m[4], out, m[5]);
+  }
+
+  return out;
+};
+
+WordProblem.prototype.operate = function(operation, l, r) {
+  var fn = BINARY_OPERATORS[operation] || function() { return 0; };
+  return fn(Number(l), Number(r));
+};
+
+function ArgumentError() {}
+ArgumentError.prototype = new Error();
---
+++ assignments/javascript/wordy/wordy_test.spec.js
@@ -0,0 +1,90 @@
+var WordProblem   = require('./example').WordProblem;
+var ArgumentError = require('./example').ArgumentError;
+
+describe('Word Problem', function() {
+
+  it('add 1', function() {
+    var question = 'What is 1 plus 1?';
+    expect(2).toEqual(new WordProblem(question).answer());
+  });
+
+  xit('add 2', function() {
+    var question = 'What is 53 plus 2?';
+    expect(55).toEqual(new WordProblem(question).answer());
+  });
+
+  xit('add negative numbers', function() {
+    var question = 'What is -1 plus -10?';
+    expect(-11).toEqual(new WordProblem(question).answer());
+  });
+
+  xit('add more digits', function() {
+    var question = 'What is 123 plus 45678?';
+    expect(45801).toEqual(new WordProblem(question).answer());
+  });
+
+  xit('subtract', function() {
+    var question = 'What is 4 minus -12?';
+    expect(16).toEqual(new WordProblem(question).answer());
+  });
+
+  xit('multiply', function() {
+    var question = 'What is -3 multiplied by 25?';
+    expect(-75).toEqual(new WordProblem(question).answer());
+  });
+
+  xit('divide', function() {
+    var question = 'What is 33 divided by -3?';
+    expect(-11).toEqual(new WordProblem(question).answer());
+  });
+
+  xit('add twice', function() {
+    var question = 'What is 1 plus 1 plus 1?';
+    expect(3).toEqual(new WordProblem(question).answer());
+  });
+
+  xit('add then subtract', function() {
+    var question = 'What is 1 plus 5 minus -2?';
+    expect(8).toEqual(new WordProblem(question).answer());
+  });
+
+  xit('subtract twice', function() {
+    var question = 'What is 20 minus 4 minus 13?';
+    expect(3).toEqual(new WordProblem(question).answer());
+  });
+
+  xit('subtract then add', function() {
+    var question = 'What is 17 minus 6 plus 3?';
+    expect(14).toEqual(new WordProblem(question).answer());
+  });
+
+  xit('multiply twice', function() {
+    var question = 'What is 2 multiplied by -2 multiplied by 3?';
+    expect(-12).toEqual(new WordProblem(question).answer());
+  });
+
+  xit('add then multiply', function() {
+    var question = 'What is -3 plus 7 multiplied by -2?';
+    expect(-8).toEqual(new WordProblem(question).answer());
+  });
+
+  xit('divide twice', function() {
+    var question = 'What is -12 divided by 2 divided by -3?';
+    expect(2).toEqual(new WordProblem(question).answer());
+  });
+
+  xit('too advanced', function() {
+    var question = 'What is 53 cubed?';
+    var problem  = new WordProblem(question);
+
+    expect(problem.answer.bind(problem)).toThrow(new ArgumentError());
+  });
+
+  xit('irrelevant', function() {
+    var question = 'Who is the president of the United States?';
+    var problem  = new WordProblem(question);
+
+    expect(problem.answer.bind(problem)).toThrow(new ArgumentError());
+  });
+
+});
