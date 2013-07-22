0554cbef1a76ba0ff641eb1575a4381d72c86592
Solomon White <rubysolo@gmail.com>
2013-07-21 18:08:42 -0600

    add clojure/grade-school

--- lib/exercism/curriculum/clojure.rb
+++ lib/exercism/curriculum/clojure.rb
@@ -3,7 +3,7 @@ class Exercism
     def slugs
       %w(
         bob word-count anagram beer-song nucleotide-count rna-transcription
-        point-mutations phone-number
+        point-mutations phone-number grade-school
         robot-name leap triangle scrabble-score
         crypto-square kindergarden-garden robot-simulator queen-attack
       )
