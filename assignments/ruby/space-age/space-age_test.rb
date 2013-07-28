2015676352a6e87ded2c2a2f5a5371e15f1100e6
Katrina Owen <katrina.owen@gmail.com>
2013-07-27 21:53:37 -0600

    Make ruby:space-age tests pending

--- assignments/ruby/space-age/space-age_test.rb
+++ assignments/ruby/space-age/space-age_test.rb
@@ -9,47 +9,55 @@ class SpaceAgeTest < MiniTest::Unit::TestCase
   end
 
   def test_age_in_earth_years
+    skip
     age = SpaceAge.new(1_000_000_000)
     assert_equal 31.69, age.on_earth
   end
 
   def test_age_in_mercury_years
+    skip
     age = SpaceAge.new(2_134_835_688)
     assert_equal 67.65, age.on_earth
     assert_equal 280.88, age.on_mercury
   end
 
   def test_age_in_venus_years
+    skip
     age = SpaceAge.new(189_839_836)
     assert_equal 6.02, age.on_earth
     assert_equal 9.78, age.on_venus
   end
 
   def test_age_on_mars
+    skip
     age = SpaceAge.new(2_329_871_239)
     assert_equal 73.83, age.on_earth
     assert_equal 39.25, age.on_mars
   end
 
   def test_age_on_jupiter
+    skip
     age = SpaceAge.new(901_876_382)
     assert_equal 28.58, age.on_earth
     assert_equal 2.41, age.on_jupiter
   end
 
   def test_age_on_saturn
+    skip
     age = SpaceAge.new(3_000_000_000)
     assert_equal 95.06, age.on_earth
     assert_equal 3.23, age.on_saturn
   end
 
   def test_age_on_uranus
+    skip
     age = SpaceAge.new(3_210_123_456)
     assert_equal 101.72, age.on_earth
     assert_equal 1.21, age.on_uranus
   end
 
   def test_age_on_neptune
+    skip
     age = SpaceAge.new(8_210_123_456)
     assert_equal 260.16, age.on_earth
     assert_equal 1.58, age.on_neptune
