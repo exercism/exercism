054c36adf9630b73d3f3753924c5631b933f2abf
Mike Gehard <mike.gehard@gmail.com>
2013-07-27 20:58:33 -0600

    Gofmt

--- assignments/go/rna-transcription/example.go
+++ assignments/go/rna-transcription/example.go
@@ -2,37 +2,6 @@ package rnatranscription
 
 import "strings"
 
-func ToRna(dna string) string{
-  return strings.Replace(dna, "T", "U", -1)
+func ToRna(dna string) string {
+	return strings.Replace(dna, "T", "U", -1)
 }
-
-/*
-
-# In general, inheriting from Ruby core classes
-# is dangerous and confusing.
-# http://words.steveklabnik.com/beware-subclassing-ruby-core-classes
-# In this case it works out with no surprises.
-class DNA < String
-  THYMIDINE = 'T'
-  URACIL = 'U'
-
-  def to_rna
-    tr THYMIDINE, URACIL
-  end
-end
-
-class DeoxyribonucleicAcid
-  THYMIDINE = 'T'
-  URACIL = 'U'
-
-  attr_reader :strand
-  def initialize(strand)
-    @strand = strand
-  end
-
-  def to_rna
-    strand.tr THYMIDINE, URACIL
-  end
-end
-
-*/
