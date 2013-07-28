260c24b4f34afbef09f38f044061d4d8195749e0
Katrina Owen <katrina.owen@gmail.com>
2013-07-27 21:53:54 -0600

    Refactor ruby:space-age

--- assignments/ruby/space-age/example.rb
+++ assignments/ruby/space-age/example.rb
@@ -1,53 +1,27 @@
 class SpaceAge
-
   attr_reader :seconds
+
   def initialize(seconds)
-    @seconds = seconds
+    @seconds = seconds.to_f
   end
 
-  def on_earth
-    earth_year.round(2)
-  end
+  EARTH_YEAR = 31_557_600
 
-  def on_mercury
-    (earth_year / 0.2408467).round(2)
-  end
+  {
+    "mercury" => EARTH_YEAR * 0.2408467,
+    "earth"   => EARTH_YEAR,
+    "venus"   => EARTH_YEAR * 0.61519726,
+    "mars"    => EARTH_YEAR * 1.8808158,
+    "jupiter" => EARTH_YEAR * 11.862615,
+    "saturn"  => EARTH_YEAR * 29.447498,
+    "uranus"  => EARTH_YEAR * 84.016846,
+    "neptune" => EARTH_YEAR * 164.79132
+  }.each do |planet, seconds_per_year|
 
-  def on_venus
-    (earth_year / 0.61519726).round(2)
-  end
+    define_method("on_#{planet}") do
+      (seconds / seconds_per_year).round(2)
+    end
 
-  def on_mars
-    (earth_year / 1.8808158).round(2)
-  end
-
-  def on_jupiter
-   (earth_year / 11.862615).round(2)
-  end
-
-  def on_saturn
-   (earth_year / 29.447498).round(2)
-  end
-
-  def on_uranus
-   (earth_year / 84.016846).round(2)
-  end
-
-  def on_neptune
-   (earth_year / 164.79132).round(2)
-  end
-
-  private
-
-  def earth_year
-    seconds / seconds_per_earth_year
-  end
-
-  def seconds_per_day
-    60 * 60 * 24
-  end
-
-  def seconds_per_earth_year
-    31557600.0
   end
 end
+
