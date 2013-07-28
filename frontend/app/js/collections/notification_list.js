23433cc52610cbada4b0370f8277e839916e0e89
Zach <briggszj@gmail.com>
2013-07-28 14:40:07 -0400

    Notifications handle case where user is not logged in

--- frontend/app/js/collections/notification_list.js
+++ frontend/app/js/collections/notification_list.js
@@ -2,11 +2,12 @@ window.exercism.collections.NotificationList = Backbone.Collection.extend({
   url: '/api/v1/notifications',
 
   initialize: function(){
+    this.newCount = 0;
     this.listenTo(this, "all", this.updateNewCount);
   },
 
   updateNewCount: function() {
-    var previousNewCount = this.newCount || 0;
+    var previousNewCount = this.newCount;
     this.newCount = this.countNew();
     if (this.newCount !== previousNewCount) {
       this.trigger("notification");
@@ -16,10 +17,10 @@ window.exercism.collections.NotificationList = Backbone.Collection.extend({
 
   countNew: function() {
     return this.reduce(function(memo, model) {
-      if (model.attributes.read) {
-        return memo + 0;
-      } else {
+      if (model.attributes.read === false) {
         return memo + 1;
+      } else {
+        return memo + 0;
       }
     }, 0);
   },
