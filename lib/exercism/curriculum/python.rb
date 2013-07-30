21bf8ae7d305281ee36db2e6a465748c3375f6e8
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-07-30 09:26:37 -0500

    add python curriculum

---
+++ lib/exercism/curriculum/python.rb
@@ -0,0 +1,11 @@
+class Exercism
+  class PythonCurriculum
+    def slugs
+      %w(bob rna-transcription word-count)
+    end
+
+    def locale
+      Locale.new('python', 'py', 'py')
+    end
+  end
+end
