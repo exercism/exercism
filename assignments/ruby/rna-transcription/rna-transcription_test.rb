a0cce9b1df15e8d091a4b0778642154595e0fa0b
Paul Kunysch <kunysch@me.com>
2013-08-02 14:53:11 +0200

    rna: Extend tests. Use long classnames.

--- assignments/ruby/rna-transcription/rna-transcription_test.rb
+++ assignments/ruby/rna-transcription/rna-transcription_test.rb
@@ -1,30 +1,71 @@
 require 'minitest/autorun'
 require_relative 'dna'
 
-class DNATest < MiniTest::Unit::TestCase
+class RibonucleicAcidTest < MiniTest::Unit::TestCase
+  def setup
+    @cga = RibonucleicAcid.new('CGA')
+    @agc = RibonucleicAcid.new('AGC')
+  end
+
+  def test_acid_equals_acid
+    assert_equal @agc, @agc
+    refute_equal @agc, @cga
+  end
+
+  def test_acid_equals_string
+    skip
+    assert_equal @agc, 'AGC'
+    refute_equal @agc, 'CGA'
+  end
+
+  def test_string_equals_acid
+    skip
+    assert_equal 'AGC', @agc
+    refute_equal 'CGA', @agc
+  end
+end
+
+class DeoxyribonucleicAcidTest < RibonucleicAcidTest
+  def setup
+    skip
+    @cga = DeoxyribonucleicAcid.new('CGA')
+    @agc = DeoxyribonucleicAcid.new('AGC')
+  end
+
+  def test_similarity_of_acid_classes
+    # skipped in setup()
+    assert_equal DeoxyribonucleicAcid <= String,
+                 RibonucleicAcid <= String,
+                 "Acid classes not similar: one is a String, the other isn't"
+  end
+
+  def test_rna_conversion_returns_ribonucleic_acid_instance
+    skip
+    assert_instance_of RibonucleicAcid, DeoxyribonucleicAcid.new('C').to_rna
+  end
 
   def test_transcribes_cytidine_unchanged
-    assert_equal 'C', DNA.new("C").to_rna
+    skip
+    assert_equal 'C', DeoxyribonucleicAcid.new('C').to_rna
   end
 
   def test_transcribes_guanosine_unchanged
     skip
-    assert_equal 'G', DNA.new("G").to_rna
+    assert_equal 'G', DeoxyribonucleicAcid.new('G').to_rna
   end
 
   def test_transcribes_adenosine_unchanged
     skip
-    assert_equal 'A', DNA.new("A").to_rna
+    assert_equal 'A', DeoxyribonucleicAcid.new('A').to_rna
   end
 
   def test_it_transcribes_thymidine_to_uracil
     skip
-    assert_equal 'U', DNA.new("T").to_rna
+    assert_equal 'U', DeoxyribonucleicAcid.new('T').to_rna
   end
 
   def test_it_transcribes_all_occurrences_of_thymidine_to_uracil
     skip
-    assert_equal 'ACGUGGUCUUAA', DNA.new('ACGTGGTCTTAA').to_rna
+    assert_equal 'ACGUGGUCUUAA', DeoxyribonucleicAcid.new('ACGTGGTCTTAA').to_rna
   end
-
 end
