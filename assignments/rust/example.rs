561eaf45f318e27693e60112dc4f31077363b67c
Markus Prinz <markus.prinz@nuclearsquid.com>
2013-08-05 13:51:59 +0200

    Rust: add example implementation for bob

--- assignments/rust/example.rs
+++ assignments/rust/example.rs
@@ -1,3 +1,20 @@
-pub fn reply(drivel: &str) -> &str { 
-    "world"
+pub fn reply(message: &str) -> &str {
+    if is_silence(message) { "Fine. Be that way." }
+    else if is_yelling(message) { "Woah, chill out!" }
+    else if is_question(message) { "Sure." }
+    else  { "Whatever." }
+}
+
+fn is_silence(message: &str) -> bool {
+    message == ""
+}
+
+fn is_yelling(message: &str) -> bool {
+    let upcase_message: &[Ascii] = message.to_ascii().to_upper();
+    let ascii_message: &[Ascii] = message.to_ascii();
+    upcase_message == ascii_message
+}
+
+fn is_question(message: &str) -> bool {
+    message.ends_with("?")
 }
