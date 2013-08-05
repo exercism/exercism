c7b22756a6c6a7fa8abcf26bbd7990282e7756cf
Markus Prinz <markus.prinz@nuclearsquid.com>
2013-08-05 17:04:08 +0200

    Fix whitespace nitpick.

--- assignments/rust/example.rs
+++ assignments/rust/example.rs
@@ -2,7 +2,7 @@ pub fn reply(message: &str) -> &str {
     if is_silence(message) { "Fine. Be that way." }
     else if is_yelling(message) { "Woah, chill out!" }
     else if is_question(message) { "Sure." }
-    else  { "Whatever." }
+    else { "Whatever." }
 }
 
 fn is_silence(message: &str) -> bool {
