9894146dca41b9b72c455e6bbfaf57525d361d50
Jason Duffy <jasonsduffy@gmail.com>
2013-08-07 10:42:12 -0400

    rename exercise file name and skip all but the first test

--- assignments/javascript/simple-cipher/simple-cipher_test.spec.js
+++ assignments/javascript/simple-cipher/simple-cipher_test.spec.js
@@ -1,4 +1,4 @@
-var Cipher = require('./example');
+var Cipher = require('./cipher');
 
 describe('Random key cipher', function () {
   var cipher = new Cipher();
@@ -10,15 +10,15 @@ describe('Random key cipher', function () {
   // Here we take advantage of the fact that plaintext of "aaa..."
   // outputs the key. This is a critical problem with shift ciphers, some
   // characters will always output the key verbatim.  
-  it('can encode', function () {
+  xit('can encode', function () {
     expect(cipher.encode('aaaaaaaaaa')).toEqual(cipher.key.substr(0,10));
   });
 
-  it('can decode', function () {
+  xit('can decode', function () {
     expect(cipher.decode(cipher.key.substr(0,10))).toEqual('aaaaaaaaaa');
   });
 
-  it('is reversible', function () {
+  xit('is reversible', function () {
     var plaintext = 'abcdefghij';
     expect(cipher.decode(cipher.encode(plaintext))).toEqual(plaintext);
   });
@@ -27,19 +27,19 @@ describe('Random key cipher', function () {
 
 describe('Incorrect key cipher', function () {
 
-  it('throws an error with an all caps key', function () {
+  xit('throws an error with an all caps key', function () {
     expect( function () {
       new Cipher("ABCDEF");
     }).toThrow(new Error("Bad key"));
   });
 
-  it('throws an error with a numeric key', function () {
+  xit('throws an error with a numeric key', function () {
     expect( function () {
       new Cipher("12345");
     }).toThrow(new Error("Bad key"));
   });
 
-  it('throws an error with an empty key', function () {
+  xit('throws an error with an empty key', function () {
     expect( function () {
       new Cipher("");
     }).toThrow(new Error("Bad key"));
@@ -51,28 +51,28 @@ describe('Substitution cipher', function () {
   var key = 'abcdefghij';
   var cipher = new Cipher(key);
 
-  it('keeps the submitted key', function () {
+  xit('keeps the submitted key', function () {
     expect(cipher.key).toEqual(key);
   });
 
-  it('can encode', function () {
+  xit('can encode', function () {
     expect(cipher.encode('aaaaaaaaaa')).toEqual('abcdefghij');
   });
 
-  it('can decode', function () {
+  xit('can decode', function () {
     expect(cipher.decode('abcdefghij')).toEqual('aaaaaaaaaa');
   });
 
-  it('is reversible', function () {
+  xit('is reversible', function () {
     expect(cipher.decode(cipher.encode('abcdefghij'))).toEqual('abcdefghij');
   });
 
-  it(': double shift encode', function () {
+  xit(': double shift encode', function () {
     expect(new Cipher('iamapandabear').encode('iamapandabear'))
       .toEqual('qayaeaagaciai');
   });
 
-  it('can wrap', function () {
+  xit('can wrap', function () {
     expect(cipher.encode('zzzzzzzzzz')).toEqual('zabcdefghi');
   });
 
