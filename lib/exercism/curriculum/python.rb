41aeda14c68a42b1e970b68d74cc05f2dcf58841
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-08-08 16:58:55 -0500

    add raindrops assignment

--- lib/exercism/curriculum/python.rb
+++ lib/exercism/curriculum/python.rb
@@ -5,7 +5,7 @@ class Exercism
           bob rna-transcription word-count anagram beer-song nucleotide-count
           point-mutations phone-number grade-school robot-name etl leap
           space-age grains gigasecond triangle scrabble-score roman-numerals
-          binary prime-factors
+          binary prime-factors raindrops
       )
     end
 
