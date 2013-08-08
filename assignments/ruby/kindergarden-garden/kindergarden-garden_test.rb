9a12ec7f47f7e8b3d7543e90e10bc1a16eee668e
Katrina Owen <katrina.owen@gmail.com>
2013-08-08 09:34:23 -0700

    Add more tests to ruby:kindergarden-garden

--- assignments/ruby/kindergarden-garden/kindergarden-garden_test.rb
+++ assignments/ruby/kindergarden-garden/kindergarden-garden_test.rb
@@ -133,3 +133,25 @@ class SurpriseTest < MiniTest::Unit::TestCase
   end
 
 end
+
+class TwoGardensDifferentStudents < MiniTest::Unit::TestCase
+ def diagram
+   "VCRRGVRG\nRVGCCGCV"
+ end
+
+ def garden_1
+   @garden_1 ||= Garden.new(diagram, ["Alice", "Bob", "Charlie", "Dan"])
+ end
+
+ def garden_2
+   @garden_2 ||= Garden.new(diagram, ["Bob", "Charlie", "Dan", "Erin"])
+ end
+
+ def test_bob_and_charlie_per_garden
+   skip
+   assert_equal [:radishes, :radishes, :grass, :clover], garden_1.bob
+   assert_equal [:violets, :clover, :radishes, :violets], garden_2.bob
+   assert_equal [:grass, :violets, :clover, :grass], garden_1.charlie
+   assert_equal [:radishes, :radishes, :grass, :clover], garden_2.charlie
+ end
+end
