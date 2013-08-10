28f17fa679e17d2876a2b6409febbe70fd106581
Katrina Owen <katrina.owen@gmail.com>
2013-08-09 18:58:26 -0700

    Refactor exercise class

--- test/exercism/exercise_test.rb
+++ test/exercism/exercise_test.rb
@@ -1,26 +1,27 @@
 require './test/test_helper'
-
 require 'exercism/exercise'
 
 class ExerciseTest < Minitest::Test
-
   def test_exercise_attributes
-    exercise = Exercise.new('nong', 'one')
+    exercise = Exercise.new('go', 'one')
 
-    assert_equal 'nong', exercise.language
+    assert_equal 'go', exercise.language
     assert_equal 'one', exercise.slug
   end
 
   def test_exercise_name
-    exercise = Exercise.new('nong', 'one-and-two')
+    exercise = Exercise.new('go', 'one')
+    assert_equal 'One', exercise.name
+  end
 
+  def test_compound_exercise_name
+    exercise = Exercise.new('go', 'one-and-two')
     assert_equal 'One And Two', exercise.name
   end
 
   def test_exercise_to_s
-    exercise = Exercise.new('nong', 'one')
-
-    assert_equal '#<Exercise nong:one>', exercise.to_s
+    exercise = Exercise.new('go', 'one')
+    assert_equal 'Exercise: one (Go)', exercise.to_s
   end
-
 end
+
