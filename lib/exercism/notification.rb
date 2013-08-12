bbf79afd11c708393033f375c5dd7f41e4e83eaf
Katrina Owen <katrina.owen@gmail.com>
2013-08-11 18:25:08 -0600

    Rework notifications to remove duplication. See #445.
    This moves view logic out of a presenter and into a petroglyph view,
    adds more attributes to the notifications, rewords the notifications in
    the template, and updates the backbone collection and model to handle
    namespaced results from the API.

--- lib/exercism/notification.rb
+++ lib/exercism/notification.rb
@@ -1,22 +1,63 @@
+require 'forwardable'
 class Notification
   include Mongoid::Document
+  extend Forwardable
 
   field :re, as: :regarding, type: String
   field :r, as: :read, type: Boolean, default: false
-  field :l, as: :link, type: String
-  field :f, as: :from, type: String
   field :at, type: Time, default: ->{ Time.now.utc }
+  field :c, as: :count, type: Integer, default: 0
 
   belongs_to :user
+  belongs_to :submission
+
+  def_delegators :submission, :language, :slug
 
   def self.recent_for_user(user)
     where(user: user).limit(100).descending(:at)
+  end
+
+  def self.on(submission, options)
+    data = {
+      submission: submission,
+      user: options.fetch(:to),
+      regarding: options[:regarding]
+    }
+    notification = where(data.merge(read: false)).first || new(data)
+    notification.increment
+    notification.save
+    notification
   end
 
   def self.mark_read(user, id)
     notification = where(user: user).and(_id: id).first
     notification.update_attributes(read: true)
     notification
+  end
+
+  def nitpick?
+    regarding == 'nitpick'
+  end
+
+  def done?
+    regarding == 'done'
+  end
+
+  def recipient
+    user
+  end
+
+  def username
+    submission.user.username
+  end
+
+  def increment
+    self.count += 1
+  end
+
+  def read!
+    self.read = true
+    save
   end
 end
 
