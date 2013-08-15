24801419440ba06d4193c082542a2ad501c6014b
Katrina Owen <katrina.owen@gmail.com>
2013-08-15 07:31:38 -0600

    Fix 'please login' path

--- lib/app/dashboard.rb
+++ lib/app/dashboard.rb
@@ -2,7 +2,7 @@ class ExercismApp < Sinatra::Base
 
 ['/dashboard/:language/:exercise/?', '/dashboard/:language/?'].each do |route|
   get route do
-    please_login route
+    please_login request.path
 
     language, exercise = params[:language], params[:exercise]
     dashboard = Gallery.new(current_user, Submission.pending_for(language, exercise))
--- lib/app/gallery.rb
+++ lib/app/gallery.rb
@@ -2,7 +2,7 @@ class ExercismApp < Sinatra::Base
 
 ['/gallery/:language/:exercise/?', '/gallery/:language/?'].each do |route|
   get route do
-    please_login route
+    please_login request.path
     language, exercise = params[:language], params[:exercise]
     dashboard = Dashboard.new(current_user,
                               exercise ? Submission.approved_for(language, exercise).limit(100) : [] )
