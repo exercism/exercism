8f44dd5ab61cfd873ecd288a1ab2249c18a95ec1
Katrina Owen <katrina.owen@gmail.com>
2013-08-14 19:16:33 -0600

    Tweak gallery/dashboard
    There's so much duplication here.

--- lib/app/dashboard.rb
+++ lib/app/dashboard.rb
@@ -1,22 +1,21 @@
 require 'app/presenters/dashboard'
 class ExercismApp < Sinatra::Base
 
-['/dashboard/:language/:exercise/', '/dashboard/:language/?'].each do |route|
+['/dashboard/:language/:exercise/?', '/dashboard/:language/?'].each do |route|
   get route do
-    language, exercise = params[:language], params[:exercise]
-    if current_user.guest?
-      erb :index
-    else
-      dashboard = Dashboard.new(current_user, Submission.pending_for(language, exercise))
+    please_login route
 
-      locals = {
-        submissions: dashboard.submissions,
-        language: language,
-        exercise: exercise,
-        breakdown: Breakdown.of(language)
-      }
-      erb :dashboard, locals: locals
-    end
+    language, exercise = params[:language], params[:exercise]
+    dashboard = Dashboard.new(current_user, Submission.pending_for(language, exercise))
+
+    locals = {
+      submissions: dashboard.submissions,
+      language: language,
+      exercise: exercise,
+      exercises: exercises_available_for(language),
+      breakdown: Breakdown.of(language)
+    }
+    erb :dashboard, locals: locals
   end
 end
 
--- lib/app/gallery.rb
+++ lib/app/gallery.rb
@@ -1,21 +1,19 @@
 require 'app/presenters/dashboard'
 class ExercismApp < Sinatra::Base
 
-['/gallery/:language/:exercise/', '/gallery/:language/?'].each do |route|
+['/gallery/:language/:exercise/?', '/gallery/:language/?'].each do |route|
   get route do
+    please_login route
     language, exercise = params[:language], params[:exercise]
-    if current_user.guest?
-      erb :index
-    else
-      dashboard = Dashboard.new(current_user, Submission.approved_for(language, exercise))
+    dashboard = Dashboard.new(current_user, Submission.approved_for(language, exercise))
 
-      locals = {
-        submissions: dashboard.submissions,
-        language: language,
-        exercise: exercise
-      }
-      erb :gallery, locals: locals
-    end
+    locals = {
+      submissions: dashboard.submissions,
+      language: language,
+      exercise: exercise,
+      exercises: exercises_available_for(language)
+    }
+    erb :gallery, locals: locals
   end
 end
 
