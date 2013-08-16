34bc702341860c3bdb9f4408d3db6d736b6b46d8
Solomon White <rubysolo@gmail.com>
2013-08-15 21:02:34 -0600

    add elixir/raindrops

--- lib/exercism/curriculum/elixir.rb
+++ lib/exercism/curriculum/elixir.rb
@@ -6,7 +6,7 @@ class Exercism
         rna-transcription point-mutations phone-number
         grade-school leap etl meetup space-age grains
         gigasecond triangle scrabble-score roman-numerals
-        binary prime-factors
+        binary prime-factors raindrops
       )
     end
 
