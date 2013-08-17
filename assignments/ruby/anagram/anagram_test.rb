b01184871e0a8cc57df2bf058bc5a5e507fe0f7c
Henrik N <henrik@nyh.se>
2013-08-17 18:17:43 +0200

    ruby/anagram prefix test checks both ways.
    Realized that [the submission that inspired this][1] actually checked for
    the candidate as a prefix of the subject, not the other way around.
    
    [1]: http://exercism.io/submissions/520ea215d908bd42b70005d0

--- assignments/ruby/anagram/anagram_test.rb
+++ assignments/ruby/anagram/anagram_test.rb
@@ -40,11 +40,11 @@ class AnagramTest < MiniTest::Unit::TestCase
     detector = Anagram.new('mass')
     assert_equal [], detector.match(['last'])
   end
-  
+
   def test_eliminate_anagram_subsets
     skip
-    detector = Anagram.new('dog')
-    assert_equal [], detector.match(['good'])
+    detector = Anagram.new('good')
+    assert_equal [], detector.match(['dog', 'goody'])
   end
 
   def test_detect_anagram
