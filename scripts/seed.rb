79ad9b9710f75acfce488dddd8f42e6602557085
Katrina Owen <katrina.owen@gmail.com>
2013-08-19 08:26:49 -0600

    Fix the seed data
    I forgot to update the seed data to match the new "submit old exercises"
    feature.
    
    This needs some serious refactoring, it's getting out of hand.

--- scripts/seed.rb
+++ scripts/seed.rb
@@ -47,22 +47,21 @@ alice_data = {
 }
 alice = User.create(alice_data)
 
-languages = {
-  "ruby" => "rb",
-  "clojure" => "clj",
-  "javascript" => "js",
-  "elixir" => "exs",
-}
-
 users = []
 
-languages.each do |language,exe|
+Exercism.current_curriculum.trails.each do |_, trail|
   (1..60).each do |n|
-    index = "#{language}#{n}"
-    user = User.create(username: index, github_id: index, email: "#{index}_coder@example.com", current: { language => "bob" })
-    users << user
+    locale = trail.locale
+    language = locale.language
+    username = "#{locale.language}#{n}"
+    user = User.create(username: username, email: "#{username}_coder@example.com")
+    exercise = trail.first
+    user.do! exercise
+    users << user.reload
+
     rand(1..5).times do |i|
-      attempt = Attempt.new(user, "class Bob\nend", "bob.#{exe}").save
+      exercise = user.current_in(language)
+      attempt = Attempt.new(user.reload, "class Code\nend", "#{exercise.slug}/code.#{locale.code_extension}").save
       created_on = Date.today - rand(30)
       attempt.submission.at = created_on.to_time - n.to_i * rand(3600)
       submission = attempt.submission
@@ -74,22 +73,22 @@ languages.each do |language,exe|
       end
       if rand(2) == 0
         locksmith = nil
-        if master.unlocks?(submission.exercise)
+        if master.unlocks?(exercise)
           locksmith = master
-        elsif journeyman.unlocks?(submission.exercise)
+        elsif journeyman.unlocks?(exercise)
           locksmith = journeyman
-        elsif apprentice.unlocks?(submission.exercise)
+        elsif apprentice.unlocks?(exercise)
           locksmith = apprentice
         end
         if locksmith
           Approval.new(submission.id, locksmith, 'very nice').save
-          Attempt.new(user.reload, "class Code \nend", "code.#{exe}").save
+          Attempt.new(user.reload, "class Code \nend", "#{user.current_in(language).slug}/code.#{locale.code_extension}").save
 
           Approval.new(submission.id, locksmith, 'expressive').save
-          Attempt.new(user.reload, "class Code \nend", "code.#{exe}").save
+          Attempt.new(user.reload, "class Code \nend", "#{user.current_in(language).slug}/code.#{locale.code_extension}").save
 
           Approval.new(submission.id, locksmith, 'short and sweet').save
-          Attempt.new(user.reload, "class Code \nend", "code.#{exe}").save
+          Attempt.new(user.reload, "class Code \nend", "#{user.current_in(language).slug}/code.#{locale.code_extension}").save
         end
       end
 
@@ -103,45 +102,46 @@ languages.each do |language,exe|
 
       submission.save
     end
-    print exe, " "
+    print locale.code_extension, " "
   end
 
-  alice.do! Exercism.current_curriculum.in(language).first
+  exercise = trail.first
+  alice.do! exercise
 end
-attempt = Attempt.new(alice, "class Bob\nend", "bob.rb").save
+attempt = Attempt.new(alice.reload, "class Code \nend", "#{alice.current_in('ruby').slug}/code.rb").save
 attempt.submission.at = Time.now.utc - 10
 attempt.submission.save
 
 Nitpick.new(attempt.submission.id, master, "It is missing `hey`.").save
 Notify.everyone(attempt.submission, 'nitpick')
-attempt = Attempt.new(alice, "class Bob\ndef hey\nend\nend", "bob.rb").save
+attempt = Attempt.new(alice.reload, "class Code \nend", "#{alice.current_in('ruby').slug}/code.rb").save
 Notify.everyone(attempt.previous_submission, 'code', except: alice)
 attempt.submission.at = Time.now.utc - 8
 attempt.submission.save
 Nitpick.new(attempt.submission.id, master, "The formatting is off. Reindent.").save
 Notify.everyone(attempt.submission, 'nitpick', except: master)
-attempt = Attempt.new(alice, "class Bob\n  def  hey\n  end\nend", "bob.rb").save
+attempt = Attempt.new(alice.reload, "class Code \nend", "#{alice.current_in('ruby').slug}/code.rb").save
 attempt.submission.at = Time.now.utc - 6
 attempt.submission.save
 Approval.new(attempt.submission.id, master, 'very nice').save
 alice.reload
 
-attempt = Attempt.new(alice, "class Words\nend", "words.rb").save
+attempt = Attempt.new(alice.reload, "class Code \nend", "#{alice.current_in('ruby').slug}/code.rb").save
 attempt.submission.at = Time.now.utc - 4
 attempt.submission.save
 Nitpick.new(attempt.submission.id, master, "Initialize takes an argument").save
 Notify.everyone(attempt.submission, 'nitpick', except: master)
-attempt = Attempt.new(alice, "class Words\n  def initialize(words)\n  end\nend", "words.rb").save
+attempt = Attempt.new(alice.reload, "class Code \nend", "#{alice.current_in('ruby').slug}/code.rb").save
 attempt.submission.at = Time.now.utc - 2
 attempt.submission.save
 Nitpick.new(attempt.submission.id, master, "`words.words` is so echo-y.").save
 Notify.everyone(attempt.submission, 'nitpick', except: master)
-attempt = Attempt.new(alice, "class Words\n  def initialize(phrase)\n  end\nend", "words.rb").save
+attempt = Attempt.new(alice.reload, "class Code \nend", "#{alice.current_in('ruby').slug}/code.rb").save
 Approval.new(attempt.submission.id, master, 'better').save
 Notify.source(attempt.submission, 'done')
 alice.reload
 
-attempt = Attempt.new(alice, "class Anagram\nend", "anagram.rb").save
+attempt = Attempt.new(alice.reload, "class Code \nend", "#{alice.current_in('ruby').slug}/code.rb").save
 long_comment = "I really like that you thought to check for nil. I also like that you put all of your answers in the same place and I like that you're hiding the implementation details of your if statement behind those methods that end with a question mark. \r\n\r\nI would rather see a .fetch to access your hash so we don't return nil by mistake. Alternatively, you could use a struct or a very small class to store your answers if you want the more concise dot syntax. @answers.fetch(:fine) vs @answers.fine\r\n\r\nLet's explore that nil check. It is currently in the fine? method which suggests that receiving a nil is a natural state of the system. I would rather see that nil check in a guard method at the top of hey rather than buried in fine?. I would also just blow up and raise an exception because if the Bob object receives nil then something has gone wrong. Zooming out even farther, if I were building this as an app I would put that nil check wherever we first got the input so that way the rest of the system doesn't need to check for it. From that perspective we don't even need a nil check in Bob at all.\r\n\r\nSummary: My suggestions are to either .fetch from your hash or create a struct or object instead. I would also not check for nil at all, assuming that it gets handled at the system input. If I did need to check for nil, I would do it at the top of hey and I would throw an error."
 Nitpick.new(attempt.submission.id, master, long_comment).save
 Notify.everyone(attempt.submission, 'nitpick', except: master)
@@ -154,7 +154,7 @@ bob_data = {
 }
 bob = User.create(bob_data)
 
-attempt = Attempt.new(bob, "def bark_along_little_doggie_when_the_moon_is_full_in_the_sky\n  \"woof\"\nend", 'bob.rb').save
+attempt = Attempt.new(bob, "def bark_along_little_doggie_when_the_moon_is_full_in_the_sky\n  \"woof\"\nend", 'bob/bob.rb').save
 Nitpick.new(attempt.submission.id, master, 'Not enough code.').save
 Nitpick.new(attempt.submission.id, master, 'I like the method name.').save
 
