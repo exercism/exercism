41be0743e8f1bdd39cda7611b1f43f9674b2b753
Bob Ippolito <bob@redivi.com>
2013-08-19 16:27:44 -0700

    haskell meetup

--- lib/exercism/curriculum/haskell.rb
+++ lib/exercism/curriculum/haskell.rb
@@ -4,7 +4,7 @@ class Exercism
       %w(
         bob rna-transcription word-count anagram beer-song
         nucleotide-count point-mutations phone-number
-        grade-school robot-name leap etl
+        grade-school robot-name leap etl meetup
       )
     end
 
