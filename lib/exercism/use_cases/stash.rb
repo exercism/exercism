bc46bd6ff761e6ccf72f7a6ad8861446c81158f2
Katrina Owen <katrina.owen@gmail.com>
2013-08-24 14:34:12 -0600

    Fix build broken in e94fe1d

--- lib/exercism/use_cases/stash.rb
+++ lib/exercism/use_cases/stash.rb
@@ -1,31 +1,38 @@
-class Stash 
+class Stash
 
-  attr_reader :user, :code, :filename, :language
+  attr_reader :user, :code
   def initialize(user, code, filename, curriculum = Exercism.current_curriculum)
     @user = user
     @code = code
-    @filename = filename
-    @language = curriculum.identify_language(@filename)
+    @file = Code.new(filename, curriculum.locales)
+  end
+
+  def filename
+    @file.filename
+  end
+
+  def language
+    @file.language
   end
 
   def submission
-  	@submission ||= Submission.new
+    @submission ||= Submission.new
   end
 
   def save
     user.submissions.each do |sub|
-      sub.supersede! if sub.stash_name == self.filename
+      sub.supersede! if sub.stash_name == filename
     end
     submission.state = 'stashed'
     submission.code = code
     submission.stash_name = filename
-  	user.submissions << submission
+    user.submissions << submission
     user.save
-  	self
+    self
   end
 
   def find
-    user.stashed_submissions.select{ |sub| sub.stash_name == self.filename}.first
+    user.stashed_submissions.select{ |sub| sub.stash_name == filename}.first
   end
 
 end
