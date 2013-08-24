92e83cd3ff14649d20413b59b11480370c977085
Katrina Owen <katrina.owen@gmail.com>
2013-08-24 15:30:29 -0600

    Make mute button more obvious

--- lib/app/helpers/submissions_helper.rb
+++ lib/app/helpers/submissions_helper.rb
@@ -8,13 +8,5 @@ module Sinatra
       action = submission.muted_by?(user) ? 'unmute' : 'mute'
       "/submissions/#{submission.id}/#{action}"
     end
-
-    def mute_button_for(submission, user)
-      if submission.muted_by?(user)
-        '<button type="submit" class="btn unmute-btn"><i class="icon-microphone"></i></button>'
-      else
-        '<button type="submit" class="btn mute-btn"><i class="icon-microphone-off"></i></button>'
-      end
-    end
   end
 end
