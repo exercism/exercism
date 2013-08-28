79c5978b8cf276adb22b1e24c841e1eee2e33057
Ben Lee <benl33@yahoo.com>
2013-08-28 12:05:50 -0400

    A word is not an anagram of itself
    Fix anagram/example.clj so it passes all of the tests.  In particular
    it was failing the test called word-is-not-own-anagram.  This could
    also be done with (and (not= word %)… inside the filter function, but I
    went with remove.

--- assignments/clojure/anagram/example.clj
+++ assignments/clojure/anagram/example.clj
@@ -11,4 +11,4 @@
 (defn anagrams-for
   [word candidates]
   (let [canonical (canonicalize word)]
-    (vec (filter #(= canonical (canonicalize %)) candidates))))
+    (remove #(= word %) (filter #(= canonical (canonicalize %)) candidates))))
