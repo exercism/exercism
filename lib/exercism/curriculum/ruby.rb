83eee326d87df222ffc396ff10960b617bb009f8
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2013-08-28 01:40:02 -0400

    renames kindergarden-garden to kindergarten-garden to correct spelling
    [#663]

--- lib/exercism/curriculum/ruby.rb
+++ lib/exercism/curriculum/ruby.rb
@@ -10,7 +10,7 @@ class Exercism
         atbash-cipher accumulate crypto-square trinary rna-transcription
         sieve simple-cipher octal luhn pig-latin pythagorean-triplet
         series difference-of-squares secret-handshake linked-list wordy
-        hexadecimal largest-series-product kindergarden-garden
+        hexadecimal largest-series-product kindergarten-garden
         binary-search-tree matrix robot-simulator nth-prime
         palindrome-products pascals-triangle say beer-song
         sum-of-multiples queen-attack saddle-points ocr-numbers
