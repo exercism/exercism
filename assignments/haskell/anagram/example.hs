211290995aa461deba01c41fb676b09bc9568c88
Stuart Childs <childss@codecavern.org>
2013-08-30 12:06:00 -0400

    Add word is not its own anagram test to Haskell.

--- assignments/haskell/anagram/example.hs
+++ assignments/haskell/anagram/example.hs
@@ -1,11 +1,14 @@
 module Anagram (anagramsFor) where
+import Control.Applicative (liftA2)
 import Data.Char (toLower)
 import Data.Map (Map, fromListWith)
 
 anagramsFor :: String -> [String] -> [String]
-anagramsFor word = filter ((canonical ==) . canonicalize)
+anagramsFor word = filter (liftA2 (&&) different anagram)
   where
+    anagram = ((canonical ==) . canonicalize)
     canonical = canonicalize word
     canonicalize :: String -> Map Char Int
     canonicalize = fromListWith (+) . map key
     key char = (toLower char, 1)
+    different = (word /=)
