e5acadd15a4f4466ad47953b3bd4d33b5f85b019
Kori Roys <kori@koriroys.com>
2013-09-01 18:38:26 -0500

    updating bob test error code help test to be more accurate

--- assignments/ruby/bob/bob_test.rb
+++ assignments/ruby/bob/bob_test.rb
@@ -5,7 +5,7 @@ begin
 rescue LoadError => e
   eval("\"#{DATA.read}\n\"").split("\n.\n").each_with_index do |s,i|
     if i > 0
-      puts "\t--- press a key to continue ---"
+      puts "\t--- press enter to continue ---"
       gets
     end
     puts "\n\n", s, "\n\n\n"
