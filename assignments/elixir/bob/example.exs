7f8cc197963c485408a19246211236da334b0e65
seeflanigan <seeflanigan@gmail.com>
2013-09-06 13:16:15 -0600

    Add alternate Bob example in Elixir

--- assignments/elixir/bob/example.exs
+++ assignments/elixir/bob/example.exs
@@ -30,3 +30,24 @@ defmodule Teenager do
   defp shouting?(input), do: input == String.upcase(input)
   defp question?(input), do: String.ends_with?(input, "?")
 end
+
+# Another approach which abstracts knowing about string categories 
+# away from Teenager and into a single responsibility module
+defmodule Message do
+  def silent?(input),   do: "" == String.strip(input)
+  def shouting?(input), do: input == String.upcase(input)
+  def question?(input), do: String.ends_with?(input, "?")
+end
+
+defmodule Teenager do
+  import Message, only: [silent?: 1, shouting?: 1, question?: 1]
+
+  def hey(input) do
+    cond do
+      silent?(input)   -> "Fine. Be that way!"
+      shouting?(input) -> "Woah, chill out!"
+      question?(input) -> "Sure."
+      true             -> "Whatever."
+    end
+  end
+end
