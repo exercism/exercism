275dde01a1313fa1dffa2e5915dd4dad5e94d903
Bob Ippolito <bob@redivi.com>
2013-09-13 18:29:35 -0700

    haskell secret-handshake

--- lib/exercism/curriculum/haskell.rb
+++ lib/exercism/curriculum/haskell.rb
@@ -13,6 +13,7 @@ class Exercism
         largest-series-product luhn matrix ocr-numbers octal trinary
         wordy simple-linked-list linked-list nth-prime palindrome-products
         pascals-triangle pig-latin pythagorean-triplet saddle-points say
+        secret-handshake
       )
     end
 
