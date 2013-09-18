860e03f076e6cf3f61aa9936a813f4e047e634c8
Katrina Owen <katrina.owen@gmail.com>
2013-09-18 10:53:59 -0600

    Fix error message

--- lib/app/auth.rb
+++ lib/app/auth.rb
@@ -33,7 +33,7 @@ class ExercismApp < Sinatra::Base
   if ENV['RACK_ENV'] == 'development'
     get '/backdoor' do
       if User.count == 0
-        flash[:error] = "You'll want to run the seed script: `ruby scripts/seed.rb``"
+        flash[:error] = "You'll want to run the seed script: `rake db:seed`"
         redirect '/'
       end
 
