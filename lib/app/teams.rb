c313d0b1f8c6d5a74fc326dbbd45ba8805f385cf
Katrina Owen <katrina.owen@gmail.com>
2013-09-22 12:36:30 -0600

    Organize team templates

--- lib/app/teams.rb
+++ lib/app/teams.rb
@@ -5,7 +5,7 @@ class ExercismApp < Sinatra::Base
 
     team = Team.where(slug: slug).first
     if team
-      erb :team, locals: {team: team, members: team.members_with_pending_submissions}
+      erb :"teams/show", locals: {team: team, members: team.members_with_pending_submissions}
     else
       flash[:error] = "We don't know anything about team '#{slug}'"
       redirect '/'
