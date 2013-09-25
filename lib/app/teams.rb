0c9d6f595abf93f1d3dbed07a055c3cd1dc42846
Martndemus <martenschilstra@gmail.com>
2013-09-25 10:49:44 +0200

    add basic edit function for teams

--- lib/app/teams.rb
+++ lib/app/teams.rb
@@ -37,4 +37,37 @@ class ExercismApp < Sinatra::Base
     end
   end
 
+  get '/teams/:slug/edit' do |slug|
+    please_login
+
+    team = Team.where(slug: slug).first
+
+    if team
+      unless team.includes?(current_user)
+        flash[:error] = "You may only edit team pages for teams that you are on."
+        redirect "/"
+      end
+      erb :"teams/edit", locals: {team: team, members: team.members.sort_by {|m| m.username.downcase}}
+    end
+  end
+
+  post '/teams/:slug/?' do |slug|
+    please_login
+
+    team = Team.where(slug: slug).first.defined_with(params[:team])
+
+    if team
+      unless team.includes?(current_user)
+        flash[:error] = "You may only edit team pages for teams that you are on."
+        redirect "/"
+      end
+
+      if team.valid?
+        team.save
+        redirect "/teams/#{team.slug}"
+      else
+        erb :"teams/edit", locals: {team: team, members: team.members.sort_by {|m| m.username.downcase}}
+      end
+    end
+  end
 end
