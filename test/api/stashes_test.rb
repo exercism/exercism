f613ff5026d9e183225797b23a8587b6c1906fc4
Katrina Owen <katrina.owen@gmail.com>
2013-09-29 08:37:12 -0600

    Refactor stash api tests

--- test/api/stashes_test.rb
+++ test/api/stashes_test.rb
@@ -9,43 +9,46 @@ class StashesApiTest < Minitest::Test
 
   attr_reader :alice
   def setup
-    @alice = User.create(username: 'alice', github_id: 1, current: {'ruby' => 'word-count', 'javascript' => 'anagram'})
+    @alice = User.create(current: {'ruby' => 'word-count'})
   end
 
   def teardown
     Mongoid.reset
   end
 
-  def test_api_accepts_stash_submission_and_returns_stash_file
-    post '/user/assignments/stash', {key: alice.key, code: 'THE CODE', filename: 'code.rb'}.to_json
-    assert_equal 201, last_response.status
-
-    get '/user/assignments/stash', {key: alice.key, filename: 'code.rb'}
-    assert_equal 200, last_response.status
-    assert_equal({"code" => 'THE CODE', "filename" => "code.rb"}, JSON::parse(last_response.body))
-  end
-
-  def test_api_requires_user_key_on_get_stash_file
+  def test_only_user_may_see_their_own_list_of_stashed_files
     get '/user/assignments/stash'
     assert_equal 401, last_response.status
   end
 
-  def test_api_requires_key_on_stash_submission
+  def test_only_user_may_stash_an_assignment
     post '/user/assignments/stash', {code: 'THE CODE', filename: 'code.rb'}.to_json
     assert_equal 401, last_response.status
   end
 
+  def test_stashing_and_retrieving_code
+    stashed_data = {'code' => 'THE CODE', 'filename' => 'code.rb'}
+    post '/user/assignments/stash', {key: alice.key}.merge(stashed_data).to_json
+    assert_equal 201, last_response.status
+
+    get '/user/assignments/stash', {key: alice.key, filename: 'code.rb'}
+    assert_equal 200, last_response.status
+    assert_equal stashed_data, JSON::parse(last_response.body)
+  end
+
   def test_api_returns_stash_list
-    post '/user/assignments/stash', {key: alice.key, code: 'THE CODE', filename: 'code.rb'}.to_json
-    get '/user/assignments/stash/list', {key: alice.key}
+    stashed_data = {'code' => 'THE CODE', 'filename' => 'code.rb'}
+    post '/user/assignments/stash', {key: alice.key}.merge(stashed_data).to_json
+    get '/user/assignments/stash/list', key: alice.key
     assert_equal({"list" => ["code.rb"]}, JSON::parse(last_response.body))
     assert_equal 200, last_response.status
   end
 
   def test_stash_deleted_on_submission_attempt
-    post '/user/assignments/stash', {key: alice.key, code: 'THE CODE', filename: 'code.rb'}.to_json
+    stashed_data = {'code' => 'THE CODE', 'filename' => 'code.rb'}
+    post '/user/assignments/stash', {key: alice.key}.merge(stashed_data).to_json
     post '/user/assignments', {key: alice.key, code: 'THE CODE', path: 'code.rb'}.to_json
-    get '/user/assignments/stash/list', {key: alice.key}
+    get '/user/assignments/stash/list', key: alice.key
     assert_equal({"list" => []}, JSON::parse(last_response.body))
   end
 end
