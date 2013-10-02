30ce66029008c810f88d1f5d26a7dfc73531e018
Robin Hilliard <robin@rocketboots.com>
2013-10-02 19:55:28 +1000

    All tests passing.

---
+++ assignments/erlang/beer-song/beer_song.erl
@@ -0,0 +1,41 @@
+-module(beer_song).
+-export([verse/1, sing/1, sing/2]).
+
+
+
+nBottles(0) ->
+  "no more bottles";
+
+nBottles(1) ->
+  "1 bottle";
+
+nBottles(N) ->
+  io_lib:format("~B bottles", [N]).
+
+
+
+verse(0) ->
+   "No more bottles of beer on the wall, no more bottles of beer.\n"
+   "Go to the store and buy some more, 99 bottles of beer on the wall.\n";
+
+verse(N) ->
+  io_lib:format(
+    "~s of beer on the wall, ~s of beer.\n"
+    "Take one down and pass it around, ~s of beer on the wall.\n",
+    [nBottles(N), nBottles(N), nBottles(N - 1)]).
+
+
+
+sing(N) ->
+  sing(N, 0).
+
+
+
+sing(N, N) ->
+  verse(N);
+
+sing(N, M) ->
+  io_lib:format("~s\n~s", [verse(N), sing(N-1, M)]).
+
+
+
---
+++ assignments/erlang/beer-song/beer_song_test.erl
@@ -0,0 +1,51 @@
+-module(beer_song_test).
+-include_lib("eunit/include/eunit.hrl").
+
+verse_test() ->
+  compareNestedLists(beer_song:verse(8),
+    "8 bottles of beer on the wall, 8 bottles of beer.\n"
+    "Take one down and pass it around, 7 bottles of beer on the wall.\n").
+
+verse_0_test() ->
+  compareNestedLists(beer_song:verse(0),
+    "No more bottles of beer on the wall, no more bottles of beer.\n"
+    "Go to the store and buy some more, 99 bottles of beer on the wall.\n").
+
+verse_1_test() ->
+  compareNestedLists(beer_song:verse(1),
+     "1 bottle of beer on the wall, 1 bottle of beer.\n"
+     "Take one down and pass it around, no more bottles of beer on the wall.\n").
+
+verse_2_test() ->
+  compareNestedLists(beer_song:verse(2),
+    "2 bottles of beer on the wall, 2 bottles of beer.\n"
+    "Take one down and pass it around, 1 bottle of beer on the wall.\n").
+
+singing_several_verses_test() ->
+  compareNestedLists(beer_song:sing(8, 6),
+    "8 bottles of beer on the wall, 8 bottles of beer.\n"
+    "Take one down and pass it around, 7 bottles of beer on the wall.\n\n"
+
+    "7 bottles of beer on the wall, 7 bottles of beer.\n"
+    "Take one down and pass it around, 6 bottles of beer on the wall.\n\n"
+
+    "6 bottles of beer on the wall, 6 bottles of beer.\n"
+    "Take one down and pass it around, 5 bottles of beer on the wall.\n").
+
+sing_all_the_rest_of_the_verses_test() ->
+  compareNestedLists(beer_song:sing(3),
+    "3 bottles of beer on the wall, 3 bottles of beer.\n"
+    "Take one down and pass it around, 2 bottles of beer on the wall.\n\n"
+
+    "2 bottles of beer on the wall, 2 bottles of beer.\n"
+    "Take one down and pass it around, 1 bottle of beer on the wall.\n\n"
+
+    "1 bottle of beer on the wall, 1 bottle of beer.\n"
+    "Take one down and pass it around, no more bottles of beer on the wall.\n\n"
+
+    "No more bottles of beer on the wall, no more bottles of beer.\n"
+    "Go to the store and buy some more, 99 bottles of beer on the wall.\n").
+
+compareNestedLists(Response, Expected) ->
+  ?assertEqual(lists:flatten(Expected), lists:flatten(Response)).
+
