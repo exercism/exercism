aceb85671fcd2d5be8d9945b38e719fe2367d53e
Katrina Owen <katrina.owen@gmail.com>
2013-10-06 13:32:51 -0600

    Add error handling to data migration.
    There are some orphan records lying around in production, and also
    people change their github username, which screws up the likes, mutes,
    and views.

--- lib/db/data_migration.rb
+++ lib/db/data_migration.rb
@@ -21,7 +21,11 @@ class DataMigration
   def self.migrate_users
     timeframes.each do |timeframe|
       User.unmigrated_in(timeframe).each do |user|
-        PGUser.create(user.pg_attributes)
+        begin
+          PGUser.create(user.pg_attributes)
+        rescue => e
+          puts "Failed to create user #{user.username}. #{e.message}"
+        end
       end
     end
     puts "MongoDB.Users: #{User.count}, PostgreSQL.Users: #{PGUser.count}"
@@ -30,10 +34,14 @@ class DataMigration
   def self.migrate_submissions
     timeframes.each do |timeframe|
       Submission.unmigrated_in(timeframe).each do |submission|
-        pg_submission = PGSubmission.create(submission.pg_attributes)
-        migrate_submission_viewers(submission, pg_submission)
-        migrate_muted_submissions(submission, pg_submission)
-        migrate_likes(submission, pg_submission)
+        begin
+          pg_submission = PGSubmission.create(submission.pg_attributes)
+          migrate_submission_viewers(submission, pg_submission)
+          migrate_muted_submissions(submission, pg_submission)
+          migrate_likes(submission, pg_submission)
+        rescue => e
+          puts "Failed to create submission #{submission.id}. #{e.message}"
+        end
       end
     end
     puts "MongoDB.Submissions: #{Submission.count}, PostgreSQL.Submission: #{PGSubmission.count}"
@@ -42,21 +50,33 @@ class DataMigration
   def self.migrate_submission_viewers(submission, pg_submission)
     submission.viewers.each do |username|
       viewer = PGUser.find_by_username(username)
-      SubmissionViewer.create(viewer_id: viewer.id, submission_id: pg_submission.id)
+      if viewer
+        SubmissionViewer.create(viewer_id: viewer.id, submission_id: pg_submission.id)
+      else
+        puts "Could not find user #{username} who viewed submission #{submission.id}."
+      end
     end
   end
 
   def self.migrate_muted_submissions(submission, pg_submission)
     submission.muted_by.each do |username|
       user = PGUser.find_by_username(username)
-      MutedSubmission.create(user_id: user.id, submission_id: pg_submission.id)
+      if user
+        MutedSubmission.create(user_id: user.id, submission_id: pg_submission.id)
+      else
+        puts "Could not find user #{username} who muted submission #{submission.id}."
+      end
     end
   end
 
   def self.migrate_likes(submission, pg_submission)
     submission.liked_by.each do |username|
       user = PGUser.find_by_username(username)
-      Like.create(user_id: user.id, submission_id: pg_submission.id)
+      if user
+        Like.create(user_id: user.id, submission_id: pg_submission.id)
+      else
+        puts "Unable to find user #{username} who liked submission #{submission.id}."
+      end
     end
   end
 
@@ -70,14 +90,22 @@ class DataMigration
 
   def self.migrate_team_memberships(team, pg_team)
     team.members.each do |member|
-      TeamMembership.create(user_id: member.pg_user.id, team_id: pg_team.id)
+      begin
+        TeamMembership.create(user_id: member.pg_user.id, team_id: pg_team.id)
+      rescue => e
+        puts "Failed to add #{member.username} to team #{team.slug}."
+      end
     end
   end
 
   def self.migrate_comments
     timeframes.each do |timeframe|
       Comment.unmigrated_in(timeframe).each do |comment|
-        PGComment.create(comment.pg_attributes)
+        begin
+          PGComment.create(comment.pg_attributes)
+        rescue => e
+          puts "Failed to create comment #{comment.id}. #{e.message}"
+        end
       end
     end
     puts "MongoDB.Comments: #{Comment.count}, PostgreSQL.Comments: #{PGComment.count}"
@@ -89,7 +117,11 @@ class DataMigration
 
     timeframes.each do |timeframe|
       Notification.unmigrated_in(timeframe).each do |notification|
-        PGNotification.create(notification.pg_attributes)
+        begin
+          PGNotification.create(notification.pg_attributes)
+        rescue => e
+          puts "Failed to create notification #{notification.id}. #{e.message}"
+        end
       end
     end
     puts "MongoDB.Notifications: #{Notification.count}, PostgreSQL.Notifications: #{PGNotification.count}"
