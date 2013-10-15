645b543ca34bf7e9e0ed829fbfe385c4a151a731
Katrina Owen <katrina.owen@gmail.com>
2013-10-15 07:34:59 -0600

    Prefer recent exercises in random work
    It's more likely that there are fewer people nitpicking on the recent
    exercises.
    
    This does not loop through multiple languages, so there's a chance that
    you will end up with no suggestion. This will be fixed in a later
    commit.
    
    Also, this is a hack. There are no tests. This will also be fixed in
    said later commit.

--- lib/exercism/user.rb
+++ lib/exercism/user.rb
@@ -40,9 +40,9 @@ class User
   end
 
   def random_work
-    completed.keys.shuffle.each do |language|
-      exercise = completed_exercises[language].sample
-      work = Submission.pending_for(language, exercise.slug).unmuted_for(username).asc(:nc)
+    language, slugs = completed.max_by {|lang, slugs| slugs.count}
+    slugs.reverse.each do |slug|
+      work = Submission.pending_for(language, slug).unmuted_for(username).asc(:nc)
       if work.count > 0
         return work.limit(10).to_a.sample
       end
