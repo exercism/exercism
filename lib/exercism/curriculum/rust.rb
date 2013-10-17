76edc3cdacf2de9cef43100bab3b722ba635c49c
Bob Ippolito <bob@redivi.com>
2013-10-17 15:30:13 +0200

    add a RustCurriculum class

---
+++ lib/exercism/curriculum/rust.rb
@@ -0,0 +1,13 @@
+class Exercism
+  class RustCurriculum
+    def slugs
+      %w(
+        bob rna-transcription word-count anagram
+      )
+    end
+
+    def locale
+      Locale.new('rust', 'rs', 'rs')
+    end
+  end
+end
