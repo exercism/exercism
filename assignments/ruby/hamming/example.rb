efc85d2f4949562e5a1f46b2fd49240e662cbb3c
Katrina Owen <katrina.owen@gmail.com>
2013-10-17 15:14:07 -0600

    Tweak hamming example

--- assignments/ruby/hamming/example.rb
+++ assignments/ruby/hamming/example.rb
@@ -1,11 +1,39 @@
 class Hamming
   def self.compute(strand1, strand2)
-    strand1.chars.zip(strand2.chars).select do |a, b|
-      mutation?(a, b)
-    end.count
+    new(strand1, strand2).distance
   end
 
-  def self.mutation?(a, b)
-    (a && b) && (a != b)
+  attr_reader :strand1, :strand2
+  def initialize(strand1, strand2)
+    @strand1 = strand1
+    @strand2 = strand2
+  end
+
+  def distance
+    common_pairs.count do |pair|
+      mutation?(*pair)
+    end
+  end
+
+  private
+
+  def common_pairs
+    sequence1.chars.zip(sequence2.chars)
+  end
+
+  def sequence1
+    strand1[0..common_length]
+  end
+
+  def sequence2
+    strand2[0..common_length]
+  end
+
+  def common_length
+    [@strand1.length, @strand2.length].min - 1
+  end
+
+  def mutation?(a, b)
+    a != b
   end
 end
