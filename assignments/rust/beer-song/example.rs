4b4c66e3e201ec05496be284fb44b113212e4efb
Bob Ippolito <bob@redivi.com>
2013-10-20 15:49:09 -0700

    rust - add whitespace to beer-song to ensure logic is in the first 80 cols and strings wrap before 100

--- assignments/rust/beer-song/example.rs
+++ assignments/rust/beer-song/example.rs
@@ -3,13 +3,20 @@ use std::iter;
 pub fn verse(n: int) -> ~str {
     match n {
         0 =>
-            ~"No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n",
+            ~"No more bottles of beer on the wall, no more bottles of beer.\n\
+              Go to the store and buy some more, 99 bottles of beer on the wall.\n",
         1 =>
-            ~"1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n",
+            ~"1 bottle of beer on the wall, 1 bottle of beer.\n\
+              Take it down and pass it around, no more bottles of beer on the wall.\n",
         2 =>
-            ~"2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n",
+            ~"2 bottles of beer on the wall, 2 bottles of beer.\n\
+              Take one down and pass it around, 1 bottle of beer on the wall.\n",
         n if n > 2 && n <= 99 =>
-            format!("{n} bottles of beer on the wall, {n} bottles of beer.\nTake one down and pass it around, {n_minus_1} bottles of beer on the wall.\n", n=n, n_minus_1=n - 1),
+            format!(
+                "{n} bottles of beer on the wall, {n} bottles of beer.\n\
+                 Take one down and pass it around, {n_minus_1} bottles of beer on the wall.\n",
+                n=n,
+                n_minus_1=n - 1),
         _ =>
             fail!(),
     }
