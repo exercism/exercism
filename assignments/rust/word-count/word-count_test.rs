1fd61dfed480e426c9214a77f28cea5d39b77510
Bob Ippolito <bob@redivi.com>
2013-10-20 10:21:06 -0700

    rust - refactor word_count tests

--- assignments/rust/word-count/word-count_test.rs
+++ assignments/rust/word-count/word-count_test.rs
@@ -5,84 +5,97 @@ use std::hashmap::HashMap;
 
 mod word_count;
 
+fn check_word_count(s: &str, pairs: &[(&str, uint)]) {
+    let mut m: HashMap<~str, uint> = word_count::word_count(s);
+    for &(k, v) in pairs.iter() {
+        assert_eq!((k, m.pop(&k.to_owned()).unwrap()), (k, v));
+    }
+    assert_eq!(m.iter().to_owned_vec(), ~[]);
+}
 
 #[test]
 #[ignore]
 fn test_no_words() {
-    assert_eq!(word_count::word_count(""), HashMap::new());
-    assert_eq!(word_count::word_count("-!@#$ $#() @@@"), HashMap::new());
+    check_word_count("", []);
+    check_word_count("-!@#$ $#() @@@", []);
 }
 
 #[test]
 #[ignore]
 fn test_count_one_word() {
-    let m = word_count::word_count("word");
-    assert_eq!(m.find(&~"word").unwrap(), &1);
+    check_word_count("word", [("word", 1)]);
 }
 
 #[test]
 #[ignore]
 fn test_count_one_of_each() {
-    let m = word_count::word_count("one of each");
-    assert_eq!(m.find(&~"one").unwrap(), &1);
-    assert_eq!(m.find(&~"of").unwrap(), &1);
-    assert_eq!(m.find(&~"each").unwrap(), &1);
+    check_word_count(
+        "one of each",
+        [("one", 1),
+         ("of", 1),
+         ("each", 1)]);
 }
 
 #[test]
 #[ignore]
 fn test_count_multiple_occurrences() {
-    let m = word_count::word_count("one fish two fish red fish blue fish");
-    assert_eq!(m.find(&~"one").unwrap(), &1);
-    assert_eq!(m.find(&~"fish").unwrap(), &4);
-    assert_eq!(m.find(&~"two").unwrap(), &1);
-    assert_eq!(m.find(&~"red").unwrap(), &1);
-    assert_eq!(m.find(&~"blue").unwrap(), &1);
+    check_word_count(
+        "one fish two fish red fish blue fish",
+        [("one", 1),
+         ("fish", 4),
+         ("two", 1),
+         ("red", 1),
+         ("blue", 1)]);
 }
 
 #[test]
 #[ignore]
 fn test_ignore_punctuation() {
-    let m = word_count::word_count("car : carpet as java : javascript!!&@$%^&");
-    assert_eq!(m.find(&~"car").unwrap(), &1);
-    assert_eq!(m.find(&~"carpet").unwrap(), &1);
-    assert_eq!(m.find(&~"as").unwrap(), &1);
-    assert_eq!(m.find(&~"java").unwrap(), &1);
-    assert_eq!(m.find(&~"javascript").unwrap(), &1);
+    check_word_count(
+        "car : carpet as java : javascript!!&@$%^&",
+        [("car", 1),
+         ("carpet", 1),
+         ("as", 1),
+         ("java", 1),
+         ("javascript", 1)]);
 }
 
 #[test]
 #[ignore]
 fn test_include_numbers() {
-    let m = word_count::word_count("testing, 1, 2 testing");
-    assert_eq!(m.find(&~"testing").unwrap(), &2);
-    assert_eq!(m.find(&~"1").unwrap(), &1);
-    assert_eq!(m.find(&~"2").unwrap(), &1);
+    check_word_count(
+        "testing, 1, 2 testing",
+        [("testing", 2),
+         ("1", 1),
+         ("2", 1)]);
 }
 
 #[test]
 #[ignore]
 fn test_normalize_case() {
-    let m = word_count::word_count("go Go GO");
-    assert_eq!(m.find(&~"go").unwrap(), &3);
+    check_word_count(
+        "go Go GO",
+        [("go", 3)]);
 }
 
 #[test]
 #[ignore]
 fn test_prefix_punctuation() {
-    let m = word_count::word_count("!%%#testing, 1, 2 testing");
-    assert_eq!(m.find(&~"testing").unwrap(), &2);
-    assert_eq!(m.find(&~"1").unwrap(), &1);
-    assert_eq!(m.find(&~"2").unwrap(), &1);
+    check_word_count(
+        "!%%#testing, 1, 2 testing",
+        [("testing", 2),
+         ("1", 1),
+         ("2", 1)]);
 }
 
 #[test]
 #[ignore]
 fn test_symbols_are_separators() {
-    let m = word_count::word_count("hey,my_spacebar_is_broken.");
-    assert_eq!(m.find(&~"hey").unwrap(), &1);
-    assert_eq!(m.find(&~"my").unwrap(), &1);
-    assert_eq!(m.find(&~"spacebar").unwrap(), &1);
-    assert_eq!(m.find(&~"is").unwrap(), &1);
-    assert_eq!(m.find(&~"broken").unwrap(), &1);
+    check_word_count(
+        "hey,my_spacebar_is_broken.",
+        [("hey", 1),
+         ("my", 1),
+         ("spacebar", 1),
+         ("is", 1),
+         ("broken", 1)]);
 }
