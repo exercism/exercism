8c262ffc50c2906173f33643c4fd46336a3aeab9
Atul Varma <varmaa@gmail.com>
2013-10-22 16:53:16 -0400

    First attempt at hyperlinking mentions inline (see #877).
    I'm doing the hyperlinking by parsing the generated Markdown HTML
    into a DOM tree using Nokogiri, and then modifying the DOM tree in-place
    by finding text nodes containing mentions, ignoring everything in
    code blocks. The DOM is then re-serialized to HTML and returned by the
    Markdown renderer in `lib/exercism/markdown.rb`.
    
    This is the only way I know of doing the hyperlinking that both gives
    us control over where the hyperlinking occurs, as well as preventing
    XSS vulnerabilities from creeping into our code. I also looked into the
    Redcarpet API, but it didn't seem to give us the same flexibility.
    
    Because of the extra re-serialization that occurs during the post-processing
    of the rendered Markdown, HTML output isn't *exactly* the same as it
    was before, though it still has the same basic DOM structure. See the
    change in the expected output of the `test_newlines` test in `MarkdownTest`
    for an example of this.
    
    I'm guessing that since the rendering now effectively enumerates all
    the mentions, we might want to have it return some kind of object
    containing both the rendered HTML as well as the list of mentions,
    obviating the need for the `ExtractsMentionsFromMarkdown` class.
    
    Things that still need to be done:
    
      * Right now we don't ensure that there are only spaces/punctuation
        around the mention, meaning e.g. our code would think `hi@toolness.com`
        contains a mention of `@toolness`. However, I *think* the
        `ExtractsMentionsFromMarkdown` class has the same limitation
        (not sure though).
      * A class `mention` is given to the hyperlink but it's not actually
        given any special display via CSS.
      * The list of mentions are still enumerated after the comment, but
        should be removed as per the discussion in #877.
      * Probably other stuff I'm forgetting about!
    
    Also, as I am a complete Ruby noob, the code I've added here is almost
    certainly terrible.

--- lib/exercism/markdown.rb
+++ lib/exercism/markdown.rb
@@ -1,6 +1,38 @@
 require 'redcarpet'
+require 'nokogiri'
 require 'rouge'
 require 'rouge/plugins/redcarpet'
+
+# GitHub usernames can contain alphanumerics and dashes, but must not
+# start with a dash.
+USERNAME_REGEX = /(@[0-9a-zA-z][0-9a-zA-Z\-]*)/
+
+def hyperlink_mentions!(node)
+  node.children.each do |child|
+    if child.node_type == Nokogiri::XML::Node::ELEMENT_NODE and
+       !child.matches?('code,td[class=code]')
+      hyperlink_mentions! child
+    elsif child.node_type == Nokogiri::XML::Node::TEXT_NODE
+      set = []
+      remaining = child.content
+      while !remaining.empty?
+        head, match, remaining = remaining.partition(USERNAME_REGEX)
+        set << child.document.create_text_node(head)
+        if !match.empty?
+          link = child.document.create_element("a")
+          link.set_attribute('class', 'mention')
+          link.set_attribute('href', '/' + match[1..-1])
+          link.content = match
+          set << link
+        end
+      end
+      if set.length > 1
+        set = Nokogiri::XML::NodeSet.new(child.document, set)
+        child.replace(set)
+      end
+    end
+  end
+end
 
 class Markdown < Redcarpet::Render::XHTML
 
@@ -28,6 +60,16 @@ class Markdown < Redcarpet::Render::XHTML
       space_after_headers: true,
       xhtml: true
     }
+  end
+
+  def postprocess(html_content)
+    dom = Nokogiri::HTML(html_content)
+    body = dom.css("body").first
+    if body
+      hyperlink_mentions! body
+      return body.inner_html
+    end
+    ""
   end
 
   def block_code(code, language)
--- test/exercism/markdown_test.rb
+++ test/exercism/markdown_test.rb
@@ -2,10 +2,34 @@ require './test/test_helper'
 require 'exercism/markdown'
 
 class MarkdownTest < Minitest::Test
+  def test_mention
+    markdown = "u @goose."
+    expected = "<p>u <a class=\"mention\" href=\"/goose\">@goose</a>.</p>"
+    assert_equal expected, Markdown.render(markdown)
+  end
+
+  def test_mention_works_multiple_times
+    markdown = "u @goose of @doom."
+    expected = "<p>u <a class=\"mention\" href=\"/goose\">@goose</a> of " +
+               "<a class=\"mention\" href=\"/doom\">@doom</a>.</p>"
+    assert_equal expected, Markdown.render(markdown)
+  end
+
+  def test_mention_ignores_code_spans
+    markdown = "`@goose`"
+    expected = "<p><code>@goose</code></p>"
+    assert_equal expected, Markdown.render(markdown)
+  end
+
+  def test_mention_ignores_fenced_code_blocks
+    markdown = "```\n@goose\n```"
+    expected = "<p><code>@goose</code></p>"
+    assert_match "<pre>@goose", Markdown.render(markdown)
+  end
 
   def test_newlines
     markdown = "Foo\nBar"
-    expected = "<p>Foo<br/>\nBar</p>\n"
+    expected = "<p>Foo<br>\nBar</p>"
     assert_equal expected, Markdown.render(markdown)
   end
 end
