54da652079f2018d54ed4c7724dc8d98b23a9d81
Bob Ippolito <bob@redivi.com>
2013-10-23 23:58:21 -0700

    erlang - bring anagram up to date with other implementations

--- assignments/erlang/anagram/anagram_test.erl
+++ assignments/erlang/anagram/anagram_test.erl
@@ -1,8 +1,43 @@
 -module(anagram_test).
 -include_lib("eunit/include/eunit.hrl").
 
-find_test() ->
-  ?assertEqual("inlets", anagram:find("listen", "enlists google inlets banana")).
+no_matches_test() ->
+    ?assertEqual(
+       anagram:find("diaper", ["hello", "world", "zombies", "pants"]),
+       []).
 
-find_not_test() ->
-  ?assertEqual("", anagram:find("loosen", "enlists google inlets banana")).
+detect_simple_anagram_test() ->
+    ?assertEqual(
+       anagram:find("ant", ["tan", "stand", "at"]),
+       ["tan"]).
+
+does_not_confuse_different_duplicates_test() ->
+    ?assertEqual(
+       anagram:find("galea", ["eagle"]),
+       []).
+
+eliminate_anagram_subsets_test() ->
+    ?assertEqual(
+       anagram:find("good", ["dog", "goody"]),
+       []).
+
+detect_anagram_test() ->
+    ?assertEqual(
+       anagram:find("listen", ["enlists", "google", "inlets", "banana"]),
+       ["inlets"]).
+
+multiple_anagrams_test() ->
+    ?assertEqual(
+       anagram:find("allergy", ["gallery", "ballerina", "regally", "clergy",
+                                "largely", "leading"]),
+       ["gallery", "regally", "largely"]).
+
+case_insensitive_anagrams_test() ->
+    ?assertEqual(
+       anagram:find("Orchestra", ["cashregister", "Carthorse", "radishes"]),
+       ["Carthorse"]).
+
+does_not_detect_a_word_as_its_own_anagram_test() ->
+    ?assertEqual(
+       anagram:find("banana", ["banana"]),
+       []).
