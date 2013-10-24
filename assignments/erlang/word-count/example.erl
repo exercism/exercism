d88e69a526c21407968339ddddc592cbcb68c4b6
Bob Ippolito <bob@redivi.com>
2013-10-23 23:27:12 -0700

    erlang - word_count fixes, use dict module

--- assignments/erlang/word-count/example.erl
+++ assignments/erlang/word-count/example.erl
@@ -1,23 +1,24 @@
 -module(word_count).
 -export([count/1]).
 
+-spec count(string()) -> dict().
+count(S) ->
+    lists:foldl(fun (K, Acc) -> dict:update_counter(K, 1, Acc) end,
+                dict:new(),
+                tokenize(string:to_lower(S))).
 
+is_alnum(C) ->
+    (C >= $a andalso C =< $z) orelse (C >= $0 andalso C =< $9).
 
-count([Char|Chars]) when is_integer(Char) ->
-  count(string:tokens([Char|Chars], " "), []).
+is_sep(C) ->
+    not is_alnum(C).
 
-
-
-count([], Tally) when is_list(Tally) ->
-  lists:flatten([io_lib:format("~s: ~B\n", [Word, Count]) || {Word, Count} <-lists:reverse(Tally)]);
-
-count([Word|Words], []) when is_list(Word) ->
-  count(Words, [{Word, 1}]);
-
-count([Word|Words], Tally) when is_list(Word) ->
-  case lists:keyfind(Word, 1, Tally) of
-    {Word, Count} ->
-      count(Words, lists:keyreplace(Word, 1, Tally, {Word, Count + 1}));
-    false ->
-      count(Words, [{Word, 1} | Tally])
-  end.
+tokenize([]) ->
+    [];
+tokenize(S) ->
+    case lists:splitwith(fun is_alnum/1, lists:dropwhile(fun is_sep/1, S)) of
+        {[], Rest} ->
+            tokenize(Rest);
+        {Word, Rest} ->
+            [Word | tokenize(Rest)]
+    end.
--- assignments/erlang/word-count/word_count_test.erl
+++ assignments/erlang/word-count/word_count_test.erl
@@ -1,13 +1,66 @@
 -module(word_count_test).
 -include_lib("eunit/include/eunit.hrl").
 
-count_test() ->
-  compareNestedLists(word_count:count("one two two three three three"),
-    "one: 1\n"
-    "two: 2\n"
-    "three: 3\n").
+%% dict should be used to implement wound_count:count/1.
 
+count_one_word_test() ->
+    assert_count(
+      "word",
+      [{"word", 1}]).
 
-compareNestedLists(Response, Expected) ->
-  ?assertEqual(lists:flatten(Expected), lists:flatten(Response)).
+count_one_of_each_test() ->
+    assert_count(
+      "one of each",
+      [{"one", 1},
+       {"of", 1},
+       {"each", 1}]).
 
+count_multiple_occurrences_test() ->
+    assert_count(
+      "one fish two fish red fish blue fish",
+      [{"one", 1},
+       {"two", 1},
+       {"fish", 4},
+       {"red", 1},
+       {"blue", 1}]).
+
+ignore_punctuation_test() ->
+    assert_count(
+      "car : carpet as java : javascript!!&@$%^&",
+      [{"car", 1},
+       {"carpet", 1},
+       {"as", 1},
+       {"java", 1},
+       {"javascript", 1}]).
+
+include_numbers_test() ->
+    assert_count(
+      "testing, 1, 2 testing",
+      [{"testing", 2},
+       {"1", 1},
+       {"2", 1}]).
+
+normalize_case_test() ->
+    assert_count(
+      "go Go GO",
+      [{"go", 3}]).
+
+prefix_punctuation_test() ->
+    assert_count(
+      "!%%#testing, 1, 2 testing",
+      [{"testing", 2},
+       {"1", 1},
+       {"2", 1}]).
+
+symbols_are_separators_test() ->
+    assert_count(
+      "hey,my_spacebar_is_broken.",
+      [{"hey", 1},
+       {"my", 1},
+       {"spacebar", 1},
+       {"is", 1},
+       {"broken", 1}]).
+
+assert_count(S, Expect) ->
+    ?assertEqual(orddict:from_list(dict:to_list(word_count:count(S))),
+                 orddict:from_list(Expect)).
