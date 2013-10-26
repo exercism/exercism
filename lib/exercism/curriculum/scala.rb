3d8bd5f52ebf1454c9d27a69423aedda0b3cf833
Sean Griffin <sean@thoughtbot.com>
2013-10-26 17:09:11 -0600

    Add curriculum file

---
+++ lib/exercism/curriculum/scala.rb
@@ -0,0 +1,13 @@
+class Exercism
+  class ScalaCurriculum
+    def slugs
+      %w(
+        bob word-count-anagram
+      )
+    end
+
+    def locale
+      Lcoale.new('scala', 'scala', 'scala')
+    end
+  end
+end
