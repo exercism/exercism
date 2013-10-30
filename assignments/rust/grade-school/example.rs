fbc40746df76ea02e4f2a1de242b9f59f1201662
Bob Ippolito <bob@redivi.com>
2013-10-29 22:45:33 -0700

    rust grade-school - use a named field in School, clean up grade(uint)

--- assignments/rust/grade-school/example.rs
+++ assignments/rust/grade-school/example.rs
@@ -2,7 +2,9 @@ extern mod extra;
 use extra::sort;
 use std::hashmap::HashMap;
 
-struct School ( HashMap<uint, ~[~str]> );
+struct School {
+    priv grades: HashMap<uint, ~[~str]>
+}
 
 fn sorted<T: Clone + Ord>(array: &[T]) -> ~[T] {
     let mut res = array.iter().map(|v| v.clone()).to_owned_vec();
@@ -12,27 +14,26 @@ fn sorted<T: Clone + Ord>(array: &[T]) -> ~[T] {
 
 impl School {
     pub fn new() -> School {
-        School(HashMap::new())
+        School { grades: HashMap::new() }
     }
 
     pub fn add(self, grade: uint, student: &str) -> School {
-        let mut m = *self;
-        m.mangle(
+        let mut s = self;
+        s.grades.mangle(
             grade,
             student,
             |_, x| ~[x.into_owned()],
             |_, xs, x| xs.push(x.into_owned()));
-        School(m)
+        s
     }
 
     pub fn sorted(self) -> ~[(uint, ~[~str])] {
-        sorted((*self).iter().map(|(&grade, students)| {
+        sorted(self.grades.iter().map(|(&grade, students)| {
             (grade, sorted(students.clone()))
         }).to_owned_vec())
     }
 
     pub fn grade(self, grade: uint) -> ~[~str] {
-        let default: ~[~str] = ~[];
-        sorted(*(*self).find(&grade).unwrap_or(&default))
+        self.grades.find(&grade).map(|&v| sorted(v.to_owned())).unwrap_or(~[])
     }
 }
