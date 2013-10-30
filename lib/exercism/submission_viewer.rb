6b575d96b35af04a3de34149871966f50d46ab60
Katrina Owen <katrina.owen@gmail.com>
2013-10-30 16:19:16 -0600

    Attempt another fix for view count

--- lib/exercism/submission_viewer.rb
+++ lib/exercism/submission_viewer.rb
@@ -3,6 +3,6 @@ class SubmissionViewer < ActiveRecord::Base
   belongs_to :submission
   belongs_to :viewer, class_name: "User"
 
-  validates :viewer_id, uniqueness: {within: :submission_id}
+  validates :viewer_id, uniqueness: {scope: :submission_id}
 
 end
