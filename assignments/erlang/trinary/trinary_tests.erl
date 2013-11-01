8403fe3a5729ba5bff401fc8a8f955da911447c6
Wil Moore III <wil.moore@wilmoore.com>
2013-11-01 14:50:41 -0600

    refactor to snake case function names

--- assignments/erlang/trinary/trinary_tests.erl
+++ assignments/erlang/trinary/trinary_tests.erl
@@ -1,34 +1,34 @@
 -module(trinary_tests).
--import(example, [toDecimal/1]).
+-import(example, [to_decimal/1]).
 -include_lib("eunit/include/eunit.hrl").
 
 trinary_1_is_decimal_1_test() ->
-  ?assertEqual(1, example:toDecimal("1")).
+  ?assertEqual(1, example:to_decimal("1")).
 
 %% trinary_2_is_decimal_2_test() ->
-%%   ?assertEqual(2, example:toDecimal("2")).
+%%   ?assertEqual(2, example:to_decimal("2")).
 
 %% trinary_10_is_decimal_3_test() ->
-%%   ?assertEqual(3, example:toDecimal("10")).
+%%   ?assertEqual(3, example:to_decimal("10")).
 
 %% trinary_11_is_decimal_4_test() ->
-%%   ?assertEqual(4, example:toDecimal("11")).
+%%   ?assertEqual(4, example:to_decimal("11")).
 
 %% trinary_100_is_decimal_9_test() ->
-%%   ?assertEqual(9, example:toDecimal("100")).
+%%   ?assertEqual(9, example:to_decimal("100")).
 
 %% trinary_112_is_decimal_14_test() ->
-%%   ?assertEqual(14, example:toDecimal("112")).
+%%   ?assertEqual(14, example:to_decimal("112")).
 
 %% trinary_222_is_decimal_26_test() ->
-%%   ?assertEqual(26, example:toDecimal("222")).
+%%   ?assertEqual(26, example:to_decimal("222")).
 
 %% trinary_1120_is_decimal_42_test() ->
-%%   ?assertEqual(42, example:toDecimal("1120")).
+%%   ?assertEqual(42, example:to_decimal("1120")).
 
 %% trinary_1122000120_is_decimal_32091_test() ->
-%%   ?assertEqual(32091, example:toDecimal("1122000120")).
+%%   ?assertEqual(32091, example:to_decimal("1122000120")).
 
 %% invalid_trinary_is_decimal_0_test() ->
-%%   ?assertEqual(0, example:toDecimal("carrot")).
+%%   ?assertEqual(0, example:to_decimal("carrot")).
 
