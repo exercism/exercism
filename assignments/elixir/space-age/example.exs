b10fe4a32d133a4f956c89565d5a6c3747b76ff5
Peter Minten <peter.minten@online.nl>
2013-11-07 13:22:47 +0100

    Remove metaprogramming from elixir space-age

--- assignments/elixir/space-age/example.exs
+++ assignments/elixir/space-age/example.exs
@@ -1,10 +1,13 @@
 defmodule SpaceAge do
-  def earth_years(seconds),   do: seconds / 31557600.0
-  def mercury_years(seconds), do: earth_years(seconds) / 0.2408467
-  def venus_years(seconds),   do: earth_years(seconds) / 0.61519726
-  def mars_years(seconds),    do: earth_years(seconds) / 1.8808158
-  def jupiter_years(seconds), do: earth_years(seconds) / 11.862615
-  def saturn_years(seconds),  do: earth_years(seconds) / 29.447498
-  def uranus_years(seconds),  do: earth_years(seconds) / 84.016846
-  def neptune_years(seconds), do: earth_years(seconds) / 164.79132
+
+  def age_on(:earth, seconds), do: seconds / 31557600.0
+  def age_on(planet, seconds), do: age_on(:earth, seconds) / planet_rel_years(planet)
+
+  defp planet_rel_years(:mercury), do: 0.2408467
+  defp planet_rel_years(:venus),   do: 0.61519726
+  defp planet_rel_years(:mars),    do: 1.8808158
+  defp planet_rel_years(:jupiter), do: 11.862615
+  defp planet_rel_years(:saturn),  do: 29.447498
+  defp planet_rel_years(:uranus),  do: 84.016846
+  defp planet_rel_years(:neptune), do: 164.79132
 end
