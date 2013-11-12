9da2c1c247a39cb456ca68dc5ca4c3d28621de20
Mitchell C Kuppinger <dpneumo@gmail.com>
2013-11-11 22:12:03 -0600

    "octal" strings containing invalid characters should evaluate to 0.
    Have added tests to assert that"8", "9", "6789" & "abc1z" should each evaluate to 0. Also added tests that a leading 0 in the octal string is considered valid.
    
    The octal class code has been changed to pass the additional tests.

--- assignments/ruby/octal/example.rb
+++ assignments/ruby/octal/example.rb
@@ -4,14 +4,19 @@ class Octal
 
   attr_reader :digits
   def initialize(decimal)
-    @digits = decimal.reverse.chars.collect(&:to_i)
+    @digits = decimal.reverse.chars
   end
 
   def to_decimal
     decimal = 0
     digits.each_with_index do |digit, index|
-      decimal += digit * BASE**index
+      return 0 unless valid_chars.include? digit
+      decimal += digit.to_i * BASE**index
     end
     decimal
+  end
+
+  def valid_chars
+    [*('0'..(BASE-1).to_s)]
   end
 end
