72a8bdd6bd376755589da8d622e956a47a5d7b4d
Russell <russell@russelldunphy.com>
2013-11-13 15:30:16 +0000

    :ocean: Make recent submissions more likely to be picked
    * Make the likelihood of a submission being randomly chosen for
      nitpicking deteriorate over time.
    * Give a 30% chance that an exercise other than the last one you
      completed will be chosen as one for you to nitpick.
    * No longer take number of nits into account when selecting an exercise.
    
    This will close #965

--- test/exercism/work_test.rb
+++ test/exercism/work_test.rb
@@ -1,23 +1,59 @@
 require './test/integration_helper'
+require 'mocha/setup'
 
 class WorkTest < Minitest::Test
   include DBCleaner
 
   def test_work_where_alice_has_commented
-    alice = User.create(username: 'alice')
-    bob   = User.create(username: 'bob')
+    alice = User.create(username: 'alice', completed: { ruby: [:anything] })
+    bob = User.create(username: 'bob')
     submission = Submission.create(user: bob, language: 'ruby', slug: 'anything')
     Comment.create(user: bob, submission: submission, body: 'something')
     Comment.create(user: alice, submission: submission, body: 'something')
 
-    assert_equal [], Work.new(alice).work_for('ruby', 'anything').to_a
+    assert_nil Work.new(alice).random
   end
 
   def test_work_where_alice_has_not_commented
-    alice = User.create(username: 'alice')
-    bob   = User.create(username: 'bob')
+    alice = User.create(username: 'alice', completed: { ruby: [:anything] })
+    bob = User.create(username: 'bob')
     submission = Submission.create(user: bob, language: 'ruby', slug: 'anything')
 
-    assert_equal [submission], Work.new(alice).work_for('ruby', 'anything').to_a
+    assert_equal submission, Work.new(alice).random
   end
+
+  def test_no_completed_exercises
+    user = User.create(completed: {})
+    work = Work.new(user)
+    assert_nil work.random
+  end
+
+  def test_completed_exercise_latest_first_in_70_percent_of_cases
+    alice = User.create(username: 'alice')
+    bob = User.create(username: 'bob')
+    sub1 = Submission.create(state: 'pending', user: bob, language: 'python', slug: 'one')
+    sub2 = Submission.create(state: 'pending', user: alice, language: 'python', slug: 'two')
+
+    user = User.create(completed: { python: [:one, :two]})
+
+    work = Work.new(user)
+    work.stubs(:rand).returns(0.6)
+    assert_equal sub2, work.random
+  end
+
+  def test_completed_exercise_random_first_in_30_percent_of_cases
+    alice = User.create(username: 'alice')
+    bob = User.create(username: 'bob')
+
+    sub1 = Submission.create(state: 'pending', user: bob, language: 'python', slug: 'one')
+    sub2 = Submission.create(state: 'pending', user: alice, language: 'python', slug: 'two')
+
+    user = User.create(completed: { python: [:one, :two]})
+    user.completed[:python].expects(:shuffle).returns([:one, :two])
+
+    work = Work.new(user)
+    work.stubs(:rand).returns(0.8)
+    assert_equal work.random, sub1
+  end
+
 end
