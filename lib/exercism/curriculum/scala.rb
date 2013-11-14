bf802e701e2d554d5519e676bbdd6ca1dcd64303
Sean Griffin <sean@thoughtbot.com>
2013-11-14 14:15:43 -0500

    Add gigasecond in scala

--- lib/exercism/curriculum/scala.rb
+++ lib/exercism/curriculum/scala.rb
@@ -5,6 +5,7 @@ class Exercism
         bob hamming word-count anagram
         nucleotide-count phone-number
         grade-school robot-name leap etl meetup space-age grains
+        gigasecond
       )
     end
 
