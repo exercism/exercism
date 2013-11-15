28d2f3b1f5381310cabe73023dd8070bfaf91e68
Katrina Owen <katrina.owen@gmail.com>
2013-11-14 17:13:24 -0700

    Simplify ruby hamming example

--- assignments/ruby/hamming/example.rb
+++ assignments/ruby/hamming/example.rb
@@ -1,39 +1,7 @@
 class Hamming
   def self.compute(strand1, strand2)
-    new(strand1, strand2).distance
-  end
-
-  attr_reader :strand1, :strand2
-  def initialize(strand1, strand2)
-    @strand1 = strand1
-    @strand2 = strand2
-  end
-
-  def distance
-    common_pairs.count do |pair|
-      mutation?(*pair)
+    (0...[strand1.length, strand2.length].min).count do |i|
+      strand1[i] != strand2[i]
     end
-  end
-
-  private
-
-  def common_pairs
-    sequence1.chars.zip(sequence2.chars)
-  end
-
-  def sequence1
-    strand1[0..common_length]
-  end
-
-  def sequence2
-    strand2[0..common_length]
-  end
-
-  def common_length
-    [@strand1.length, @strand2.length].min - 1
-  end
-
-  def mutation?(a, b)
-    a != b
   end
 end
