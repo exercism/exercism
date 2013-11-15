efd2133153b769163ace9aa996ed475c57e67a8f
Katrina Owen <katrina.owen@gmail.com>
2013-11-15 14:25:42 -0700

    Shuffle ruby exercises around

--- lib/exercism/curriculum/ruby.rb
+++ lib/exercism/curriculum/ruby.rb
@@ -2,17 +2,16 @@ class Exercism
   class RubyCurriculum
     def slugs
       %w(
-        bob hamming word-count anagram
-        nucleotide-count phone-number
-        grade-school robot-name leap etl meetup space-age grains
-        gigasecond triangle scrabble-score roman-numerals
-        binary prime-factors raindrops allergies strain
+        bob leap grade-school hamming word-count anagram
+        etl space-age grains gigasecond triangle scrabble-score
+        roman-numerals prime-factors raindrops strain
+        beer-song nucleotide-count phone-number robot-name
         atbash-cipher accumulate crypto-square trinary rna-transcription
-        sieve simple-cipher octal luhn pig-latin pythagorean-triplet
+        sieve simple-cipher luhn pig-latin pythagorean-triplet allergies
         series difference-of-squares secret-handshake linked-list wordy
         hexadecimal largest-series-product kindergarten-garden
         binary-search-tree matrix robot-simulator nth-prime
-        palindrome-products pascals-triangle say beer-song
+        palindrome-products pascals-triangle say meetup
         sum-of-multiples queen-attack saddle-points ocr-numbers
         simple-linked-list linked-list point-mutations
       )
