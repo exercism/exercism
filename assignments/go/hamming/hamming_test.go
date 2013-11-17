5c82e1cc9dbc5686fa113649fa1fd34ba2f9d5de
Katrina Owen <katrina.owen@gmail.com>
2013-11-17 13:22:19 -0700

    Update hamming tests in Go

--- assignments/go/hamming/hamming_test.go
+++ assignments/go/hamming/hamming_test.go
@@ -9,13 +9,16 @@ var testCases = []struct {
 	strandA, strandB string
 	description      string
 }{
-	{0, "GGACTGA", "GGACTGA", "no difference between identical strands"},
-	{3, "ACT", "GGA", "complete hamming distance in small strand"},
-	{9, "GGACGGATTCTG", "AGGACGGATTCT", "distance in off by one strand"},
-	{1, "GGACG", "GGTCG", "small hamming distance in middle somewhere"},
-	{2, "ACCAGGG", "ACTATGG", "larger distance"},
-	{3, "AAACTAGGGG", "AGGCTAGCGGTAGGAC", "ignores extra length on other strand when longer"},
-	{5, "GACTACGGACAGGGTAGGGAAT", "GACATCGCACACC", "ignores extra length on original strand when longer"},
+	{0, "", "", "no difference between empty strands"},
+	{2, "AG", "CT", "complete hamming distance for small strand"},
+	{0, "A", "A", "no difference between identical strands"},
+	{1, "A", "G", "complete distance for single nucleotide strand"},
+	{1, "AT", "CT", "small hamming distance"},
+	{1, "GGACG", "GGTCG", "small hamming distance in longer strand"},
+	{0, "AAAG", "AAA", "ignores extra length on first strand when longer"},
+	{0, "AAA", "AAAG", "ignores extra length on second strand when longer"},
+	{4, "GATACA", "GCATAA", "large hamming distance"},
+	{9, "GGACGGATTCTG", "AGGACGGATTCT", "hamming distance in very long strand"},
 }
 
 func TestHamming(t *testing.T) {
