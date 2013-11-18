9a892a7bc28017737591bd327fe3bff5967c8626
Katrina Owen <katrina.owen@gmail.com>
2013-11-17 19:08:22 -0500

    Update octal in Go

--- assignments/go/octal/example.go
+++ assignments/go/octal/example.go
@@ -1,10 +1,23 @@
 package octal
 
 import (
+	"math"
 	"strconv"
+	"strings"
 )
 
-func ToDecimal(input string) (r int64) {
-	r, _ = strconv.ParseInt(input, 8, 0)
-	return
+// Easy way:
+// r, _ = strconv.ParseInt(input, 8, 0)
+//
+// Let's do it the hard way.
+
+func ToDecimal(s string) (r int64) {
+	digits := strings.Split(s, "")
+	length := len(digits)
+
+	for i := length; i > 0; i = i - 1 {
+		v, _ := strconv.ParseInt(digits[i-1], 10, 0)
+		r = r + (v * int64(math.Pow(8, float64(length-i))))
+	}
+	return r
 }
--- assignments/go/octal/octal_test.go
+++ assignments/go/octal/octal_test.go
@@ -1,13 +1,13 @@
 package octal
 
-import "testing"
+import (
+	"testing"
+)
 
-type testCase struct {
+var testCases = []struct {
 	input    string
 	expected int64
-}
-
-var testCases = []testCase{
+}{
 	{"1", 1},
 	{"10", 8},
 	{"1234567", 342391},
@@ -18,7 +18,7 @@ func TestToDecimal(t *testing.T) {
 	for _, test := range testCases {
 		actual := ToDecimal(test.input)
 		if actual != test.expected {
-			t.Errorf("ToDecimal(%s): expected[%d], actual [%d]", test.input, test.expected, actual)
+			t.Fatalf("ToDecimal(%s): expected[%d], actual [%d]", test.input, test.expected, actual)
 		}
 	}
 }
