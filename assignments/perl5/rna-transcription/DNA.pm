925a3bf1d3f0a0d0ff9924cdfbfaba1722a3439a
Gabor Szabo <gabor@szabgab.com>
2013-11-19 20:56:13 +0200

    add test cases

--- assignments/perl5/rna-transcription/DNA.pm
+++ assignments/perl5/rna-transcription/DNA.pm
@@ -1,6 +1,11 @@
 package DNA;
 
 sub to_rna {
+	my ($dna) = @_;
+    $dna =~ y/T/U/;
+    #$dna =~ tr/T/U/;   # y and tr are synonymes in perl 5
+    #$dna =~ s/T/U/g;   # regexes are not really the right tool for this
+	return $dna;
 }
 
 1;
