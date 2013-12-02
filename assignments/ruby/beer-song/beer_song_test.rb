e6c6477d2e553105dd309985d5f5668404a1a670
Katrina Owen <katrina.owen@gmail.com>
2013-12-02 05:30:50 -0800

    Tweak beer song in ruby

--- assignments/ruby/beer-song/beer_song_test.rb
+++ assignments/ruby/beer-song/beer_song_test.rb
@@ -17,31 +17,37 @@ class BeerSongTest < MiniTest::Unit::TestCase
   end
 
   def test_another_typical_verse
+    skip
     expected = "3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n"
     assert_equal expected, song.verse(3)
   end
 
   def test_verse_1
+    skip
     expected = "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
     assert_equal expected, song.verse(1)
   end
 
   def test_verse_2
+    skip
     expected = "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
     assert_equal expected, song.verse(2)
   end
 
   def test_verse_0
+    skip
     expected = "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
     assert_equal expected, song.verse(0)
   end
 
   def test_several_verses
+    skip
     expected = "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n\n7 bottles of beer on the wall, 7 bottles of beer.\nTake one down and pass it around, 6 bottles of beer on the wall.\n\n6 bottles of beer on the wall, 6 bottles of beer.\nTake one down and pass it around, 5 bottles of beer on the wall.\n\n"
     assert_equal expected, song.verses(8, 6)
   end
 
   def test_the_whole_song
+    skip
     assert_equal song.verses(99, 0), song.sing
   end
 end
