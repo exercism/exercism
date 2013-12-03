54b343947c2dd52212d267f8b2d4ae9179d57b15
petehuang <peteh22@gmail.com>
2013-12-03 01:09:36 -0600

    Add more stub files for elixir exercises

---
+++ assignments/elixir/gigasecond/gigasecond.exs
@@ -0,0 +1,10 @@
+defmodule Gigasecond do
+	@doc """
+	Calculate a date one billion seconds after an input date.
+	"""
+	@spec from({pos_integer, pos_integer, pos_integer}) :: :calendar.date
+
+	def from({year, month, day}) do
+
+	end
+end
