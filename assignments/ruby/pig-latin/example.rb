425adb4d5985c50766567c5b785cfe678d07bbac
Mitchell C Kuppinger <dpneumo@gmail.com>
2013-12-02 21:54:37 -0600

    Example passes the added tests

--- assignments/ruby/pig-latin/example.rb
+++ assignments/ruby/pig-latin/example.rb
@@ -1,14 +1,28 @@
 class PigLatin
-
   def self.translate(phrase)
-    phrase.split(" ").map do |word|
-      head, tail = word.scan(/\A([^aeiou]?qu|[^aeiou]*)(.*)/).first
-      tail + head + suffix
-    end.join " "
+    phrase.split(' ').map do |word|
+      PigLatin.new(word).translate
+    end.join(' ')
   end
 
-  def self.suffix
-    "ay"
+  def initialize(word)
+    @word = word.downcase.gsub(/[^a-z]/,'')
   end
 
+  def translate
+    return (word + "ay") if it_starts_with_vowel_sound?
+    start, remainder = parse_initial_consonant_sound_and_remainder
+    remainder + start + "ay"
+  end
+
+private
+  attr_reader :word
+
+  def it_starts_with_vowel_sound?
+    word.match /\A([aeiou]|y[^aeiou]|xr)/
+  end
+
+  def parse_initial_consonant_sound_and_remainder
+    word.scan(/\A([^aeiou]?qu|[^aeiou]+)(.*)/).first
+  end
 end
