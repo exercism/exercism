38447f3d73478f33c89a3a67c48d245b1a48445c
petehuang <peteh22@gmail.com>
2013-12-05 11:30:31 -0600

    Add strain exercise for elixir

---
+++ assignments/elixir/strain/example.exs
@@ -0,0 +1,29 @@
+defmodule Strain do
+
+  @doc """
+  Keep all entries in a collection that return true for a given function.
+
+  Using filter would work, but don't use it.
+  """
+  @spec keep(List, function) :: List
+  
+  def keep(collection, function) do
+    Enum.reduce(collection, [], fn(entry, kept) ->
+      if function.(entry), do: [entry|kept], else: kept
+    end) |> Enum.reverse
+  end
+  
+  @doc """
+  DIscard all entries in a collection that return true for a given function.
+
+  Using reject would work, but don't use it.
+  """
+  @spec discard(List, function) :: List
+
+  def discard(collection, function) do
+    Enum.reduce(collection, [], fn(entry, kept) ->
+      unless function.(entry), do: [entry|kept], else: kept
+    end) |> Enum.reverse
+  end
+
+end
