54d8fca7413bba617b6b5afca9c3c1c3b13c40df
petehuang <peteh22@gmail.com>
2013-12-08 14:19:54 -0600

    Fix difference of squares example

--- assignments/elixir/difference-of-squares/example.exs
+++ assignments/elixir/difference-of-squares/example.exs
@@ -7,37 +7,23 @@ defmodule Squares do
   Calculate sum of squares from 1 to a given end number.
   """
   @spec sum_of_squares(pos_integer) :: pos_integer
-
-  def sum_of_squares(1) do
-    1
-  end
-
   def sum_of_squares(number) do
-    (number * number) + sum_of_squares(number - 1)
+    Enum.reduce(1..number, 0, fn(x, acc) -> acc + (x * x) end)
   end
 
   @doc """
   Calculate square of sums from 1 to a given end number.
   """
   @spec square_of_sums(pos_integer) :: pos_integer
-
   def square_of_sums(number) do
-    sum_to(number) * sum_to(number)
-  end
-
-  defp sum_to(1) do
-    1
-  end
-
-  defp sum_to(number) do
-    number + sum_to(number - 1)
+    sum_to_number = Enum.reduce(1..number, fn(x, acc) -> x + acc end)
+    sum_to_number * sum_to_number
   end
 
   @doc """
   Calculate difference between sum of squares and square of sums from 1 to a given end number.
   """
   @spec difference(pos_integer) :: pos_integer
-
   def difference(number) do
     square_of_sums(number) - sum_of_squares(number)
   end
