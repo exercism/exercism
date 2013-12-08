5d731a4d890b3ef7e4a211658c27b817a4a19fa8
Wil Moore III <wil.moore@wilmoore.com>
2013-12-07 20:41:11 -0700

    fix phone-number module/import for test runner & refactor/cleanup

--- assignments/javascript/phone-number/example.js
+++ assignments/javascript/phone-number/example.js
@@ -1,42 +1,39 @@
-(function() {
-  'use strict';
+'use strict';
 
-  function Phone(number) {
-    this.rawNumber = number;
-    this.cleanedNumber = this.cleanNumber(number);
+var Phone = module.exports = function Phone(number) {
+  this.rawNumber = number;
+  this.cleanedNumber = this.cleanNumber(number);
+};
+
+Phone.prototype.cleanNumber = function(number) {
+  var num = number.replace(/\D/g,'');
+
+  if (num.length === 10) {
+    return num;
+  } else if (num.length === 11 && num[0] === "1") {
+    return num.substr(1,num.length);
+  } else {
+    return "0000000000";
   }
+};
 
-  Phone.prototype.cleanNumber = function(number) {
-    var num = number.replace(/\D/g,'');
+Phone.prototype.number = function() {
+  return this.cleanedNumber;
+};
 
-    if (num.length === 10) {
-      return num;
-    } else if (num.length === 11 && num[0] === "1") {
-      return num.substr(1,num.length);
-    } else {
-      return "0000000000";
-    }
-  };
+Phone.prototype.areaCode = function() {
+  return this.number().substr(0,3);
+};
 
-  Phone.prototype.number = function() {
-    return this.cleanedNumber;
-  };
+Phone.prototype.exchangeCode = function() {
+  return this.number().substr(3,3);
+};
 
-  Phone.prototype.areaCode = function() {
-    return this.number().substr(0,3);
-  };
+Phone.prototype.subscriberNumber = function() {
+  return this.number().substr(6,4);
+};
 
-  Phone.prototype.exchangeCode = function() {
-    return this.number().substr(3,3);
-  };
+Phone.prototype.toString = function() {
+  return "(" + this.areaCode() + ") " + this.exchangeCode() + "-" + this.subscriberNumber();
+};
 
-  Phone.prototype.subscriberNumber = function() {
-    return this.number().substr(6,4);
-  };
-
-  Phone.prototype.toString = function() {
-    return "(" + this.areaCode() + ") " + this.exchangeCode() + "-" + this.subscriberNumber();
-  };
-
-  module.exports = Phone;
-})();
