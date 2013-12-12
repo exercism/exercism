1ca5e6c5285ba420a6eb98fb3cec3034e3cbd484
Steven Meunier <steven@systemoutlaw.net>
2013-12-12 21:51:35 +0100

    DNA strands should only contain valid nucleotides
    Extra Clojure test case for rna-transcription

--- assignments/clojure/rna-transcription/example.clj
+++ assignments/clojure/rna-transcription/example.clj
@@ -1,8 +1,13 @@
 (ns dna)
 
+(def adenosine \A)
+(def cytadine  \C)
+(def guanosine \G)
 (def thymidine \T)
 (def uracil    \U)
 
 (defn to-rna
   [strand]
+  {:pre [(every? #{adenosine cytadine guanosine thymidine}
+                 (set strand))]}
   (clojure.string/replace strand thymidine uracil))
