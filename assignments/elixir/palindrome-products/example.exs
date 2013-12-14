3e02bf7455dc2a125a9fbb579613870ec669c901
petehuang <peteh22@gmail.com>
2013-12-14 15:38:34 -0600

    Refactor palindrome products example for elixir

--- assignments/elixir/palindrome-products/example.exs
+++ assignments/elixir/palindrome-products/example.exs
@@ -3,14 +3,12 @@ defmodule Palindromes do
   @doc """
   Generates all palindrome products from an optionally given min factor (or 1) to a given max factor.
   """
+  @spec generate(non_neg_integer) :: HashDict.t
   @spec generate(non_neg_integer, non_neg_integer) :: HashDict.t
   def generate(max_factor, min_factor // 1) do
     Enum.reduce(min_factor..max_factor, HashDict.new, fn(x, dict) ->
       Enum.reduce(x..max_factor, dict, fn(y, products) ->
-        cond do
-          palindrome?(x * y) -> add_factor(products, x, y)
-          true -> products
-        end
+        if palindrome?(x * y), do: add_factor(products, x, y), else: products
       end)
     end)
     |> Enum.sort
--- assignments/elixir/palindrome-products/palindrome_products_test.exs
+++ assignments/elixir/palindrome-products/palindrome_products_test.exs
@@ -6,32 +6,32 @@ defmodule PalindromeProductsTest do
 
   test "largest palindrome from single digit factors" do
     palindromes = Palindromes.generate(9)
-    assert 9 == List.last(Dict.keys(palindromes))
-    assert [[1, 9], [3, 3]] == Dict.fetch!(palindromes, 9)
+    assert 9 == palindromes |> Dict.keys |> Enum.sort |> List.last
+    assert [[1, 9], [3, 3]] == palindromes[9]
   end
 
   test "largest palindrome from double digit factors" do
     palindromes = Palindromes.generate(99, 10)
-    assert 9009 == List.last(Dict.keys(palindromes))
-    assert [[91, 99]] == Dict.fetch!(palindromes, 9009)
+    assert 9009 == palindromes |> Dict.keys |> Enum.sort |> List.last
+    assert [[91, 99]] == palindromes[9009]
   end
 
   test "smallest palindrome from double digit factors" do
     palindromes = Palindromes.generate(99, 10)
-    assert 121 == Enum.first(Dict.keys(palindromes))
-    assert [[11, 11]] == Dict.fetch!(palindromes, 121)
+    assert 121 == palindromes |> Dict.keys |> Enum.sort |> Enum.first
+    assert [[11, 11]] == palindromes[121]
   end
 
   test "largest palindrome from triple digit factors" do
     palindromes = Palindromes.generate(999, 100)
-    assert 906609 == List.last(Dict.keys(palindromes))
-    assert [[913, 993]] == Dict.fetch!(palindromes, 906609)
+    assert 906609 == palindromes |> Dict.keys |> Enum.sort |> List.last
+    assert [[913, 993]] == palindromes[906609]
   end
 
   test "smallest palindromes from triple digit factors" do
     palindromes = Palindromes.generate(999, 100)
-    assert 10201 == Enum.first(Dict.keys(palindromes))
-    assert [[101, 101]] == Dict.fetch!(palindromes, 10201)
+    assert 10201 == palindromes |> Dict.keys |> Enum.sort |> Enum.first
+    assert [[101, 101]] == palindromes[10201]
 
   end
 
