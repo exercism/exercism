828bbdf3f72dff185dc3919dbc2e99f66d167e3b
Katrina Owen <katrina.owen@gmail.com>
2013-12-20 16:56:52 -0700

    Fix admin section

--- lib/app/curriculum.rb
+++ lib/app/curriculum.rb
@@ -6,6 +6,12 @@ class ExercismApp < Sinatra::Base
       Submission.select('slug, state, count(id)').where(language: 'ruby').group(:slug, :state).each do |submission|
         summary[submission.slug][submission.state] = submission.count.to_i
       end
+      sql = "SELECT s.slug, COUNT(c.id) nits FROM comments c INNER JOIN submissions s ON c.submission_id=s.id WHERE s.language=? GROUP BY slug"
+      query = ActiveRecord::Base.send(:sanitize_sql_array, [sql, language])
+
+      ActiveRecord::Base.connection.execute(query).to_a.each do |submission|
+        summary[submission["slug"]]["nits"] = submission["nits"]
+      end
       summary
     end
 
@@ -29,7 +35,7 @@ class ExercismApp < Sinatra::Base
     end
 
     trail = Exercism.current_curriculum.trails[id.to_sym]
-    languages = Exercism.current_curriculum.trails.keys
+    languages = Exercism.current_curriculum.trails.map {|_, t| t.name}
     progress = progress(id)
 
     erb :curriculum, locals: { trail: trail, languages: languages, progress: progress }
