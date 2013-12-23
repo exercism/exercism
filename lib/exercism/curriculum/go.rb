1f42df984c0f000c0effdce6fb988084a0677fc2
Katrina Owen <katrina.owen@gmail.com>
2013-12-23 09:13:08 -0700

    Move point-mutations in go to the end of the track

--- lib/exercism/curriculum/go.rb
+++ lib/exercism/curriculum/go.rb
@@ -3,9 +3,9 @@ class Exercism
     def slugs
       %w(
         leap etl hamming anagram word-count binary accumulate
-        point-mutations allergies bob
+        allergies bob
         rna-transcription roman-numerals atbash-cipher
-        phone-number octal bottles nucleotide-count
+        phone-number octal bottles nucleotide-count point-mutations
       )
     end
 
