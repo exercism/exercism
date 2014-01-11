771578ab5bfa095f7c30694101c513967154338d
Katrina Owen <katrina.owen@gmail.com>
2014-01-10 21:30:52 -0700

    Specify minitest gem in test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ $:.unshift File.expand_path("../../lib", __FILE__)
 ENV['RACK_ENV'] = 'test'
 require 'simplecov' if ENV['COVERAGE']
 
+gem 'minitest', '=4.7.5'
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'pry'
