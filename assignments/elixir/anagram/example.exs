e2b3cd6682792a9c9f81df5830573f7482962db2
Katrina Owen <katrina.owen@gmail.com>
2014-01-12 09:06:58 -0700

    Update edge cases in elixir/anagram

--- assignments/elixir/anagram/example.exs
+++ assignments/elixir/anagram/example.exs
@@ -1,8 +1,23 @@
 defmodule Anagram do
-  def match(base, candidates) do
-    b = canonical base
-    Enum.filter candidates, fn(c) -> base != c && b == canonical c end
+  @doc """
+  Returns those words that are anagrams of the `w` word.
+
+  Comparison is case insensitive.
+  """
+  @spec match(String.t, [String.t]) :: [String.t]
+  def match(target, words) do
+    lc_target = String.downcase(target)
+    sorted_target = sort(lc_target)
+    Enum.filter(words, fn word ->
+      lc_word = String.downcase(word)
+      # `and` is shortcutting
+      lc_word != lc_target and sort(lc_word) == sorted_target
+    end)
   end
 
-  defp canonical(string), do: :lists.sort String.codepoints string
+  defp sort(s) do
+    # :lists.sort is specialized on lists, which saves the overhead of the
+    # reduce based Enum functions.
+    String.graphemes(s) |> :lists.sort()
+  end
 end
