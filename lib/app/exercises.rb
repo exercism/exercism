d401e83fc0a9fa8f0d95dc965b40e932f1c94755
Katrina Owen <katrina.owen@gmail.com>
2014-01-12 08:10:18 -0700

    Expose test suite. See #1135

--- lib/app/exercises.rb
+++ lib/app/exercises.rb
@@ -1,4 +1,9 @@
 class ExercismApp < Sinatra::Base
+  get '/exercises/:language/:slug' do |language, slug|
+    assignment = Assignment.new(language, slug, Exercism.curriculum.path)
+    erb :"exercises/test_suite", locals: {assignment: assignment}
+  end
+
   get '/exercises/:slug' do |slug|
     erb :"exercises/readme", locals: {readme: Readme.new(slug).text}
   end
