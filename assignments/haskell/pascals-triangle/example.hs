f4103ea12500f9feb011d5adcb47a0ac814213da
Bob Ippolito <bob@redivi.com>
2014-01-20 14:04:58 -0800

    haskell - use less naive algorithm for pascals triangle

--- assignments/haskell/pascals-triangle/example.hs
+++ assignments/haskell/pascals-triangle/example.hs
@@ -1,13 +1,12 @@
 module Triangle (triangle, row) where
-import Data.List (foldl')
 
 triangle :: Integral a => [[a]]
 triangle = map row [1..]
 
 row :: (Integral a) => a -> [a]
-row n = map (choose n0) [0..n0]
-  where n0 = n - 1
-
-choose :: (Integral a) => a -> a -> a
-choose n k = foldl' go 1 [1..k]
-  where go z i = z * (n - i + 1) `div` i
+row n = reflect [] $ scanl choices 1 [1 .. pred n `div` 2]
+  where
+    reflect acc [] | odd n     = tail acc
+                   | otherwise = acc
+    reflect acc (x:xs) = x : reflect (x:acc) xs
+    choices z i = z * (n - i) `div` i
