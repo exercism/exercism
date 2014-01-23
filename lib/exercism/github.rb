8e32273e7a4515275d6db6bf863ffce90d524e6d
Katrina Owen <katrina.owen@gmail.com>
2014-01-23 05:21:41 -0700

    Clean up GitHub helper

--- lib/exercism/github.rb
+++ lib/exercism/github.rb
@@ -1,6 +1,14 @@
 class Github
   def self.login_url
-    "https://github.com/login/oauth/authorize?client_id=#{ENV.fetch('EXERCISM_GITHUB_CLIENT_ID')}"
+    "https://github.com/login/oauth/authorize?client_id=#{client_id}"
+  end
+
+  def self.client_id
+    ENV.fetch('EXERCISM_GITHUB_CLIENT_ID')
+  end
+
+  def self.client_secret
+    ENV.fetch('EXERCISM_GITHUB_CLIENT_SECRET')
   end
 
   def self.authenticate(code)
@@ -9,24 +17,12 @@ class Github
       c.use Faraday::Adapter::NetHttp
     end
 
-    # Now that we know we're okay to connect, we have to request a bearer
-    # token which is needed to request details as a user.
-    options = {
-      client_id: ENV.fetch('EXERCISM_GITHUB_CLIENT_ID'),
-      client_secret: ENV.fetch('EXERCISM_GITHUB_CLIENT_SECRET'),
-      code: code
-    }
-
-    # FIXME: delete this when github can take application/json again
-    options = options.map {|k,v| "#{k}=#{v}"}.join('&')
-
     response = conn.post do |req|
       req.url '/login/oauth/access_token'
-    # req.headers['Content-Type'] = 'application/json'
+      req.headers['Content-Type'] = 'application/json'
       req.headers['Accept'] = 'application/json'
       req.headers['User-Agent'] = user_agent
-      req.body = options
-      #req.body = options.to_json
+      req.body = {client_id: client_id, client_secret: client_secret, code: code}.to_json
     end
     access_token = JSON.parse(response.body)['access_token']
 
@@ -35,9 +31,6 @@ class Github
       c.use Faraday::Adapter::NetHttp
     end
 
-    # Now that we have a code and a bearer token, we can finally ask for
-    # more details about the user. This will give us access to their
-    # github id, their username, and their email.
     response = conn.get do |req|
       req.url '/user'
       req.headers['User-Agent'] = user_agent
