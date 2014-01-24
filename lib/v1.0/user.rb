41375a2877c96c288edc2fc5de4152b8243806f8
Katrina Owen <katrina.owen@gmail.com>
2014-01-23 20:02:38 -0700

    Slot in temporary layout for logged in state (v1.0)

--- lib/v1.0/user.rb
+++ lib/v1.0/user.rb
@@ -13,6 +13,6 @@ class ExercismV1p0 < Sinatra::Base
     please_login
     user = User.find_by_username(username)
     exercise = user.exercises.find_by_key(key)
-    haml :"user/exercise", locals: {exercise: App::User::Exercise.new(exercise)}, layout: false
+    haml :"user/exercise", locals: {exercise: App::User::Exercise.new(exercise)}
   end
 end
