9e53388bf6281e6717df478e267ba8235c6283b2
Katrina Owen <katrina.owen@gmail.com>
2014-01-25 06:47:58 -0700

    Add profile page placeholder

--- lib/v1.0/user.rb
+++ lib/v1.0/user.rb
@@ -1,14 +1,21 @@
 class ExercismV1p0 < Sinatra::Base
+  get '/account' do
+    please_login
+
+    haml :"user/account", locals: {account: App::User::Account.new(current_user)}
+  end
+
+  get '/:username/profile' do |username|
+    please_login
+
+    user = User.find_by_username(username)
+    haml :"user/profile" #, locals: {profile: App::User::Profile.new(user)}
+  end
+
   get '/:username/:key' do |username, key|
     please_login
     user = User.find_by_username(username)
     exercise = user.exercises.find_by_key(key)
     haml :"user/exercise", locals: {exercise: App::User::Exercise.new(exercise)}
-  end
-
-  get '/account' do
-    please_login
-
-    haml :"user/account", locals: {account: App::User::Account.new(current_user)}
   end
 end
