72a86f2b97e8a3a82b8fb03fd4a7fbdcc8a25858
Wil Moore III <wil.moore@wilmoore.com>
2014-01-26 22:00:02 -0700

    PHP setup for Travis:
    - install a sane PHP 5.x for travis since there is none by default on a Ruby VM.
    - clean up PHPUnit-based tests for better portability.

--- assignments/php/trinary/trinary_test.php
+++ assignments/php/trinary/trinary_test.php
@@ -2,61 +2,60 @@
 
 require realpath(__DIR__ . '/trinary.php');
 
-class TrinaryTest extends PHPUnit_Framework_TestCase
-{
+class TrinaryTest extends PHPUnit_Framework_TestCase {
 
   /** @test */
-  public function _1_is_decimal_1()
-  {
-    $this->assertEquals(1, (new Trinary('1'))->toDecimal());
+  public function test_1_is_decimal_1() {
+    $trinary = new Trinary('1');
+    $this->assertEquals(1, $trinary->toDecimal());
   }
 
-  /** @skip */
-  public function _2_is_decimal_2()
-  {
-    $this->assertEquals(2, (new Trinary('2'))->toDecimal());
+  public function test_2_is_decimal_2() {
+    $this->markTestSkipped();
+    $trinary = new Trinary('2');
+    $this->assertEquals(2, $trinary->toDecimal());
   }
 
-  /** @skip */
-  public function _10_is_decimal_3()
-  {
-    $this->assertEquals(3, (new Trinary('10'))->toDecimal());
+  public function test_10_is_decimal_3() {
+    $this->markTestSkipped();
+    $trinary = new Trinary('10');
+    $this->assertEquals(3, $trinary->toDecimal());
   }
 
-  /** @skip */
-  public function _11_is_decimal_4()
-  {
-    $this->assertEquals(4, (new Trinary('11'))->toDecimal());
+  public function test_11_is_decimal_4() {
+    $this->markTestSkipped();
+    $trinary = new Trinary('11');
+    $this->assertEquals(4, $trinary->toDecimal());
   }
 
-  /** @skip */
-  public function _100_is_decimal_9()
-  {
-    $this->assertEquals(9, (new Trinary('100'))->toDecimal());
+  public function test_100_is_decimal_9() {
+    $this->markTestSkipped();
+    $trinary = new Trinary('100');
+    $this->assertEquals(9, $trinary->toDecimal());
   }
 
-  /** @skip */
-  public function _112_is_decimal_14()
-  {
-    $this->assertEquals(14, (new Trinary('112'))->toDecimal());
+  public function test_112_is_decimal_14() {
+    $this->markTestSkipped();
+    $trinary = new Trinary('112');
+    $this->assertEquals(14, $trinary->toDecimal());
   }
 
-  /** @skip */
-  public function _222_is_decimal_26()
-  {
-    $this->assertEquals(26, (new Trinary('222'))->toDecimal());
+  public function test_222_is_decimal_26() {
+    $this->markTestSkipped();
+    $trinary = new Trinary('222');
+    $this->assertEquals(26, $trinary->toDecimal());
   }
 
-  /** @skip */
-  public function _1122000120_is_decimal_32091()
-  {
-    $this->assertEquals(32091, (new Trinary('1122000120'))->toDecimal());
+  public function test_1122000120_is_decimal_32091() {
+    $this->markTestSkipped();
+    $trinary = new Trinary('1122000120');
+    $this->assertEquals(32091, $trinary->toDecimal());
   }
 
-  /** @skip */
-  public function _invalid_trinary_is_decimal_0()
-  {
-    $this->assertEquals(0, (new Trinary('carrot'))->toDecimal());
+  public function test_invalid_trinary_is_decimal_0() {
+    $this->markTestSkipped();
+    $trinary = new Trinary('carrot');
+    $this->assertEquals(0, $trinary->toDecimal());
   }
 
 }
--- assignments/php/wordy/wordy_test.php
+++ assignments/php/wordy/wordy_test.php
@@ -2,103 +2,102 @@
 
 require realpath(__DIR__ . '/wordy.php');
 
-class WordProblemTest extends PHPUnit_Framework_TestCase
-{
+class WordProblemTest extends PHPUnit_Framework_TestCase {
 
   /** @test */
-  public function add_1() {
+  public function test_add_1() {
     $problem = new WordProblem('What is 1 plus 1?');
     $this->assertEquals(2, $problem->answer());
   }
 
-  /** @skip */
-  public function add_2() {
+  public function test_add_2() {
+    $this->markTestSkipped();
     $problem = new WordProblem('What is 53 plus 2?');
     $this->assertEquals(55, $problem->answer());
   }
 
-  /** @skip */
-  public function add_negative_numbers() {
+  public function test_add_negative_numbers() {
+    $this->markTestSkipped();
     $problem = new WordProblem('What is -1 plus -10?');
     $this->assertEquals(-11, $problem->answer());
   }
 
-  /** @skip */
-  public function add_more_digits() {
+  public function test_add_more_digits() {
+    $this->markTestSkipped();
     $problem = new WordProblem('What is 123 plus 45678?');
     $this->assertEquals(45801, $problem->answer());
   }
 
-  /** @skip */
-  public function subtract() {
+  public function test_subtract() {
+    $this->markTestSkipped();
     $problem = new WordProblem('What is 4 minus -12?');
     $this->assertEquals(16, $problem->answer());
   }
 
-  /** @skip */
-  public function multiply() {
+  public function test_multiply() {
+    $this->markTestSkipped();
     $problem = new WordProblem('What is -3 multiplied by 25?');
     $this->assertEquals(-75, $problem->answer());
   }
 
-  /** @skip */
-  public function divide() {
+  public function test_divide() {
+    $this->markTestSkipped();
     $problem = new WordProblem('What is 33 divided by -3?');
     $this->assertEquals(-11, $problem->answer());
   }
 
-  /** @skip */
-  public function add_twice() {
+  public function test_add_twice() {
+    $this->markTestSkipped();
     $problem = new WordProblem('What is 1 plus 1 plus 1?');
     $this->assertEquals(3, $problem->answer());
   }
 
-  /** @skip */
-  public function add_then_subtract() {
+  public function test_add_then_subtract() {
+    $this->markTestSkipped();
     $problem = new WordProblem('What is 1 plus 5 minus -2?');
     $this->assertEquals(8, $problem->answer());
   }
 
-  /** @skip */
-  public function subtract_twice() {
+  public function test_subtract_twice() {
+    $this->markTestSkipped();
     $problem = new WordProblem('What is 20 minus 4 minus 13?');
     $this->assertEquals(3, $problem->answer());
   }
 
-  /** @skip */
-  public function subtract_then_add() {
+  public function test_subtract_then_add() {
+    $this->markTestSkipped();
     $problem = new WordProblem('What is 17 minus 6 plus 3?');
     $this->assertEquals(14, $problem->answer());
   }
 
-  /** @skip */
-  public function multiply_twice() {
+  public function test_multiply_twice() {
+    $this->markTestSkipped();
     $problem = new WordProblem('What is 2 multiplied by -2 multiplied by 3?');
     $this->assertEquals(-12, $problem->answer());
   }
 
-  /** @skip */
-  public function add_then_multiply() {
+  public function test_add_then_multiply() {
+    $this->markTestSkipped();
     $problem = new WordProblem('What is -3 plus 7 multiplied by -2?');
     $this->assertEquals(-8, $problem->answer());
   }
 
-  /** @skip */
-  public function divide_twice() {
+  public function test_divide_twice() {
+    $this->markTestSkipped();
     $problem = new WordProblem('What is -12 divided by 2 divided by -3?');
     $this->assertEquals(2, $problem->answer());
   }
 
-  /** @skip */
-  public function too_advanced() {
+  public function test_too_advanced() {
+    $this->markTestSkipped();
     $this->setExpectedException('ArgumentError');
 
     $problem = new WordProblem('What is 53 cubed?');
     $problem->answer();
   }
 
-  /** @skip */
-  public function irrelevant() {
+  public function test_irrelevant() {
+    $this->markTestSkipped();
     $this->setExpectedException('ArgumentError');
 
     $problem = new WordProblem('Who is the president of the United States?');
