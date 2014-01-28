da72b4825ccddd95dec504ea8a265bbd12c83201
David Rueck <drueck@gmail.com>
2014-01-28 14:47:40 -0800

    Update js grade school tests to make hiding private data possible

--- assignments/javascript/grade-school/grade-school_test.spec.js
+++ assignments/javascript/grade-school/grade-school_test.spec.js
@@ -4,48 +4,47 @@ describe("School", function() {
   var school;
 
   beforeEach(function() {
-    school = new School("Haleakala Hippy School");
+    school = new School();
   });
 
-  it("an empty school", function() {
-    expect(school.db).toEqual({});
+  it("a new school has an empty roster", function() {
+    expect(school.roster()).toEqual({});
   });
 
-  xit("add a student", function() {
-    school.add("Aimee",2);
+  xit("adding a student adds them to the roster for the given grade", function() {
+    school.add("Aimee", 2);
     var expectedDb = { 2 : [ "Aimee" ] };
-    expect(school.db).toEqual(expectedDb);
+    expect(school.roster()).toEqual(expectedDb);
   });
 
-  xit("add mores students in the same class", function() {
-    school.add("James",2);
+  xit("adding more students to the same grade adds them to the roster", function() {
     school.add("Blair",2);
+    school.add("James",2);
     school.add("Paul",2);
-    var expectedDb = { 2 : [ "James", "Blair", "Paul" ] };
-    expect(school.db).toEqual(expectedDb);
+    var expectedDb = { 2 : [ "Blair", "James", "Paul" ] };
+    expect(school.roster()).toEqual(expectedDb);
   });
 
-  xit("add students to different grades", function() {
+  xit("adding students to different grades adds them to the roster", function() {
     school.add("Chelsea",3);
     school.add("Logan",7);
     var expectedDb = { 3 : [ "Chelsea" ], 7 : [ "Logan"] };
-    expect(school.db).toEqual(expectedDb);
+    expect(school.roster()).toEqual(expectedDb);
   });
 
-  xit("get students in a grade", function() {
+  xit("grade returns the students in that grade in alphabetical order", function() {
     school.add("Franklin",5);
     school.add("Bradley",5);
     school.add("Jeff",1);
-    var expectedStudents = [ "Franklin", "Bradley" ];
+    var expectedStudents = [ "Bradley", "Franklin" ];
     expect(school.grade(5)).toEqual(expectedStudents);
   });
 
-  xit("get students in a non-existant grade", function() {
+  xit("grade returns an empty array if there are no students in that grade", function() {
     expect(school.grade(1)).toEqual([]);
   });
 
-
-  xit("sort school", function() {
+  xit("the students names in each grade in the roster are sorted", function() {
     school.add("Jennifer", 4);
     school.add("Kareem", 6);
     school.add("Christopher", 4);
@@ -55,7 +54,7 @@ describe("School", function() {
       4 : ["Christopher", "Jennifer"],
       6 : ["Kareem"]
     };
-    expect(school.sort()).toEqual(sorted);
+    expect(school.roster()).toEqual(sorted);
   });
 
 });
