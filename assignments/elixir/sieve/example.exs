e930e1d7d5cb2d299b39e71a1024272bc698cf9d
David Rueck <drueck@gmail.com>
2014-01-30 10:50:34 -0800

    Update Elixir Sieve to conform to the README
    Update the solution template to remove the private method stub
    Update the example to use the Sieve of Eratosthenes
    
    The README expects the implementation to use the Sieve of Eratosthenes,
    but the previous solution template encouraged a different algorithm by
    providing a stub for private method with a particular interface.

--- assignments/elixir/sieve/example.exs
+++ assignments/elixir/sieve/example.exs
@@ -5,15 +5,13 @@ defmodule Sieve do
   """
   @spec primes_to(non_neg_integer) :: [non_neg_integer]
   def primes_to(limit) do
-    Enum.reduce(2..limit, [], fn(number, primes) ->
-      if is_prime?(number, primes), do: [number|primes], else: primes end)
-      |> Enum.reverse
+    Enum.to_list(2..limit) |> do_primes([]) |> Enum.reverse
   end
 
-  # Checks if a given number is prime based on a given list of known primes.
-  @spec is_prime?(non_neg_integer, [non_neg_integer]) :: boolean
-
-  defp is_prime?(number, primes) do
-    Enum.all?(primes, &(rem(number, &1) != 0))
+  defp do_primes([], primes), do: primes
+  defp do_primes([ candidate | rest ], primes) do
+    candidates = Enum.reject(rest, &(rem(&1, candidate) == 0))
+    do_primes(candidates, [ candidate | primes ])
   end
+
 end
--- assignments/elixir/sieve/sieve.exs
+++ assignments/elixir/sieve/sieve.exs
@@ -5,14 +5,7 @@ defmodule Sieve do
   """
   @spec primes_to(non_neg_integer) :: [non_neg_integer]
   def primes_to(limit) do
-   
+
   end
 
-  @doc """
-  Checks if a given number is prime based on a given list of known primes.
-  """
-  @spec is_prime?(non_neg_integer, [non_neg_integer]) :: boolean
-  defp is_prime?(number, primes) do
-  
-  end
 end
