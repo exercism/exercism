744f500b8bae7b05da043189671622a44215e079
Magnus Hogberg <magnus.h@gmail.com>
2014-02-15 22:19:02 +0100

    Update tests and example to match information in readme.

--- assignments/python/rna-transcription/rna_transcription_test.py
+++ assignments/python/rna-transcription/rna_transcription_test.py
@@ -7,20 +7,20 @@ import unittest
 
 class DNATests(unittest.TestCase):
     def test_transcribes_cytidine_unchanged(self):
-        self.assertEqual('C', dna.DNA('C').to_rna())
+        self.assertEqual('C', dna.DNA('G').to_rna())
 
     def test_transcribes_guanosine_unchanged(self):
-        self.assertEqual('G', dna.DNA('G').to_rna())
+        self.assertEqual('G', dna.DNA('C').to_rna())
 
     def test_transcribes_adenosine_unchanged(self):
-        self.assertEqual('A', dna.DNA('A').to_rna())
+        self.assertEqual('A', dna.DNA('T').to_rna())
 
     def test_transcribes_thymidine_to_uracil(self):
-        self.assertEqual('U', dna.DNA('T').to_rna())
+        self.assertEqual('U', dna.DNA('A').to_rna())
 
     def test_transcribes_all_occurences(self):
         self.assertEqual(
-            'ACGUGGUCUUAA',
+            'UGCACCAGAAUU',
             dna.DNA('ACGTGGTCTTAA').to_rna()
         )
 
