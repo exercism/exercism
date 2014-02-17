b89aefbaeb85152148fa6f1d3abcb87df024e8e7
Katrina Owen <katrina.owen@gmail.com>
2014-02-17 07:58:44 -0700

    Deprecate accumulate in coffeescript

--- lib/exercism/curriculum/coffeescript.rb
+++ lib/exercism/curriculum/coffeescript.rb
@@ -1,10 +1,12 @@
 class Exercism
   class CoffeescriptCurriculum
     def slugs
+      # accumulate is deprecated
       %w(
-      bob word-count binary anagram accumulate trinary
+      bob word-count binary anagram trinary
       hexadecimal luhn binary-search-tree nth-prime
       palindrome-products queen-attack wordy beer-song
+      accumulate
       )
     end
 
