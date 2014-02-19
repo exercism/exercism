138d8a02c88a31cd8e80e67524cfb4cea88ceaaf
ScottDalessandro <scott@howdy-tech.com>
2014-02-19 13:43:32 -0500

    reversed the test to make it more readable and similar to the previous tests

--- assignments/javascript/binary/binary_test.spec.js
+++ assignments/javascript/binary/binary_test.spec.js
@@ -3,35 +3,35 @@ var Binary = require('./binary');
 describe('binary', function() {
 
   it('1 is decimal 1', function() {
-    expect(1).toEqual(new Binary('1').toDecimal());
+    expect(new Binary('1').toDecimal()).toEqual(1);
   });
 
   xit('10 is decimal 2', function() {
-    expect(2).toEqual(new Binary('10').toDecimal());
+    expect(new Binary('10').toDecimal()).toEqual(2);
   });
 
   xit('11 is decimal 3', function() {
-    expect(3).toEqual(new Binary('11').toDecimal());
+    expect(new Binary('11').toDecimal()).toEqual(3);
   });
 
   xit('100 is decimal 4', function() {
-    expect(4).toEqual(new Binary('100').toDecimal());
+    expect(new Binary('100').toDecimal()).toEqual(4);
   });
 
   xit('1001 is decimal 9', function() {
-    expect(9).toEqual(new Binary('1001').toDecimal());
+    expect(new Binary('1001').toDecimal()).toEqual(9);
   });
 
   xit('11010 is decimal 26', function() {
-    expect(26).toEqual(new Binary('11010').toDecimal());
+    expect(new Binary('11010').toDecimal()).toEqual(26);
   });
 
   xit('10001101000 is decimal 1128', function() {
-    expect(1128).toEqual(new Binary('10001101000').toDecimal());
+    expect(new Binary('10001101000').toDecimal()).toEqual(1128);
   });
 
   xit('carrot is decimal 0', function() {
-    expect(0).toEqual(new Binary('carrot').toDecimal());
+    expect(new Binary('carrot').toDecimal()).toEqual(0);
   });
 
 });
