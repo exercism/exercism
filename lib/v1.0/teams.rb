058ce0f143ddca4cfee81821e89a60a3227d170b
Katrina Owen <katrina.owen@gmail.com>
2014-02-21 23:56:26 -0700

    Add basic v1.0 data to team page

--- lib/v1.0/teams.rb
+++ lib/v1.0/teams.rb
@@ -2,6 +2,7 @@ class ExercismV1p0 < Sinatra::Base
   get '/teams/:slug' do |slug|
     please_login
 
-    haml :"teams/show"
+    team = Team.find_by_slug(slug)
+    haml :"teams/show", locals: {team: team}
   end
 end
