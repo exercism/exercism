190f8d027122f1a9eedddfd5fe7412ef3c287d0c
Katrina Owen <katrina.owen@gmail.com>
2014-03-01 12:49:20 -0700

    Set appropriate content-type on exercises response

--- lib/api/exercises.rb
+++ lib/api/exercises.rb
@@ -1,6 +1,7 @@
 class ExercismAPI < Sinatra::Base
   get '/exercises' do
     require_user
+    content_type 'application/json', :charset => 'utf-8'
     sql = "SELECT language, slug FROM user_exercises WHERE user_id = %s" % current_user.id.to_s
     exercises = Hash.new {|exercises, key| exercises[key] = []}
     UserExercise.connection.execute(sql).each_with_object(exercises) {|row, exercises|
