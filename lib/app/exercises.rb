8f2726f5402371c6bba47538d16dc97959c368fc
Katrina Owen <katrina.owen@gmail.com>
2014-03-01 16:56:47 -0700

    Get readme and test suite from x-api

--- lib/app/exercises.rb
+++ lib/app/exercises.rb
@@ -1,11 +1,20 @@
+require 'api/assignments/xapi'
+
 class ExercismApp < Sinatra::Base
   get '/exercises/:language/:slug' do |language, slug|
-    assignment = Assignment.new(language, slug, Exercism.curriculum.path)
-    erb :"exercises/test_suite", locals: {assignment: assignment}
+    response = Xapi.get("exercises", language, slug)
+    exercise = JSON.parse(response)["assignments"].first
+    language = exercise["track"]
+    slug = exercise["slug"]
+    text = exercise["files"].find {|filename, code| filename =~ /test/i}.last
+    erb :"exercises/test_suite", locals: {language: language, slug: slug, text: text}
   end
 
-  get '/exercises/:slug' do |slug|
-    erb :"exercises/readme", locals: {readme: Readme.new(slug).text}
+  get '/exercises/:language/:slug/readme' do |language, slug|
+    response = Xapi.get("exercises", language, slug)
+    exercise = JSON.parse(response)["assignments"].first
+    text = exercise["files"].find {|key, value| key == "README.md"}.last
+    erb :"exercises/readme", locals: {text: text}
   end
 
   post '/exercises/:language/:slug' do |language, slug|
