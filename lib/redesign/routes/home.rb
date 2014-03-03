d8b0b968bc5365dc3af97f52f2bad6eebc57fed0
Katrina Owen <katrina.owen@gmail.com>
2014-03-03 13:36:36 -0700

    Add placeholders in redesign

--- lib/redesign/routes/home.rb
+++ lib/redesign/routes/home.rb
@@ -2,7 +2,7 @@ module ExercismIO
   module Routes
     class Home < Core
       get '/' do
-        haml :"home/index"
+        haml :"home/index", locals: {email: '_@kytrinyx.com'}
       end
     end
   end
