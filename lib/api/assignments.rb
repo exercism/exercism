ed63ce9929cb3dd1253e6dd18d5fd38d1da891d9
Katrina Owen <katrina.owen@gmail.com>
2014-03-09 14:28:17 -0600

    Tweak error message in API

--- lib/api/assignments.rb
+++ lib/api/assignments.rb
@@ -43,15 +43,8 @@ class ExercismAPI < Sinatra::Base
     end
     unless user
       message = <<-MESSAGE
-      Beloved user,
-
-      We have changed your API key (and everyone else's).
-
-      You will need to log out from the exercism command line client
-      (`exercism logout`) and log back in using the new API key in
-      your account on the website.
-
-      Sorry about the inconvenience!
+      Please double-check the API key in your exercism account page and
+      ensure you have logged into the CLI using the correct key.
       MESSAGE
       halt 401, {:error => message}.to_json
     end
