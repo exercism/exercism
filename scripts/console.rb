21b1aa6d0e6fcfe37125027dd9ac9afd44773e99
Katrina Owen <katrina.owen@gmail.com>
2014-03-19 19:12:53 -0400

    add console method helpers

---
+++ scripts/console.rb
@@ -0,0 +1,19 @@
+$:.unshift File.expand_path("./../../lib", __FILE__)
+require 'bundler'
+Bundler.require
+require 'exercism'
+require 'services'
+
+ENV['RACK_ENV'] ||= 'development'
+
+def user(username)
+  User.find_by_username(username)
+end
+
+def submission(key)
+  Submission.find_by_key(key)
+end
+
+def exercise(key)
+  UserExercise.find_by_key(key)
+end
