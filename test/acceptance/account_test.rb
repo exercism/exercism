8b1aee85b4a8e93430d72a5405479007d5434022
Jeff Taggart <jeff@jetaggart.com>
2014-04-27 21:23:52 -0600

    Add test around creating a team
    This also changes logging for sending emails to invited users. Since
    it's not super critical if it fails (it seems), don't log in the test environment.

--- test/acceptance/account_test.rb
+++ test/acceptance/account_test.rb
@@ -26,4 +26,25 @@ class AccountTest < AcceptanceTestCase
       assert_equal 'some@email.com', find('[name=email]').value
     end
   end
+
+  def test_creating_a_team
+    create_user(username: 'one_username', github_id: 12345)
+    create_user(username: 'two_username', github_id: 4567)
+
+    with_login(@user) do
+      click_on 'Account'
+      click_on 'new team'
+
+      fill_in 'Slug', with: 'gocowboys'
+      fill_in 'Name', with: 'Go Cowboys'
+      fill_in 'Usernames', with: 'one_username, two_username'
+
+      click_on 'Save'
+
+      assert_equal '/teams/gocowboys', current_path
+      assert_content 'Team Go Cowboys'
+      assert_content 'one_username'
+      assert_content 'two_username'
+    end
+  end
 end
