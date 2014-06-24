2b7cd56734d8c79cad7b465e6dcfcb941333c3cd
Jacob Richardson <jacob.ninja.dev@gmail.com>
2014-06-24 15:47:04 -0600

    Add Golang as a supported language

--- lib/exercism/analysis.rb
+++ lib/exercism/analysis.rb
@@ -4,4 +4,5 @@ ANALYZER_CONFIG = {
     'ruby' => AnalyzerConfig.new(Exercism::Adapters::Ruby, [Exercism::Analyzers::Roodi]),
     'python' => AnalyzerConfig.new(Exercism::Adapters::Python, [Exercism::Analyzers::Pylint]),
     'javascript' => AnalyzerConfig.new(Exercism::Adapters::Javascript, [Exercism::Analyzers::Jslint]),
+    'go' => AnalyzerConfig.new(Exercism::Adapters::Go, [Exercism::Analyzers::Golint]),
 }
