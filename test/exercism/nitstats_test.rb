4760e08ac2f9285399d8ddaf0cfbec08c6d5bc88
Katrina Owen <katrina.owen@gmail.com>
2014-07-06 17:34:09 +0200

    Fix timezone issue in test

--- test/exercism/nitstats_test.rb
+++ test/exercism/nitstats_test.rb
@@ -15,7 +15,7 @@ class NitstatsTest < MiniTest::Test
 
   def test_labels_for_each_day_of_last_30_days
     Date.stubs(:today).returns(Date.new(2013, 11, 30))
-    Time.stubs(:now).returns(Time.new(2013, 11, 30))
+    Time.stubs(:now).returns(Time.utc(2013, 11, 30))
 
     expected = ["Nov 1", "Nov 2", "Nov 3", "Nov 4", "Nov 5", "Nov 6", "Nov 7", "Nov 8", "Nov 9", "Nov 10", "Nov 11", "Nov 12", "Nov 13", "Nov 14", "Nov 15", "Nov 16", "Nov 17", "Nov 18", "Nov 19", "Nov 20", "Nov 21", "Nov 22", "Nov 23", "Nov 24", "Nov 25", "Nov 26", "Nov 27", "Nov 28", "Nov 29", "Nov 30"]
 
