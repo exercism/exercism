abe7074fb31c0b0e8fd1cac67390c7cf892fef10
Matt Rogers <mattrogers@sbcglobal.net>
2014-09-07 15:26:01 -0500

    Remove unused variable from AssumableUser::Identity
    The `sql` variable wasn't being used, so I removed it.

--- lib/exercism/assumable_user.rb
+++ lib/exercism/assumable_user.rb
@@ -18,7 +18,6 @@ class AssumableUser
   end
 
   def self.all
-    sql = "SELECT username, github_id FROM users u INNER JOIN lifecycle_events e ON u.id=e.user_id GROUP BY u.username, u.github_id, u.created_at ORDER BY u.created_at DESC LIMIT 100"
     ::User.order('created_at DESC').limit(100).map {|user|
       Identity.new(user.username, user.github_id, user.onboarding_steps)
     }.sort_by(&:progress).reverse
