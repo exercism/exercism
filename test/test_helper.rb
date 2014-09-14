08a94e48c09547a59bb2f215782ee5fbb8556c52
Katrina Owen <katrina.owen@gmail.com>
2014-09-14 10:52:01 -0700

    Upgrade activerecord and friends

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,7 @@ $:.unshift File.expand_path("../../lib", __FILE__)
 ENV['RACK_ENV'] = 'test'
 require 'simplecov' if ENV['COVERAGE']
 
-gem 'minitest', '=5.3.5'
+gem 'minitest', '=5.4.1'
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'pry'
