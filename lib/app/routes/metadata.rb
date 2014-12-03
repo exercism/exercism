bc06638c0946e6bda9cd72eeaa6a2d5d0832b516
Katrina Owen <katrina.owen@gmail.com>
2014-12-03 07:10:41 -0800

    Handle 404s from xapi metadata endpoints

--- lib/app/routes/metadata.rb
+++ lib/app/routes/metadata.rb
@@ -2,16 +2,26 @@ module ExercismWeb
   module Routes
     class Metadata < Core
       get '/exercises/:track_id/:slug' do |track_id, slug|
-        _, response = Xapi.get("assignments", track_id, slug)
-        data = JSON.parse(response)["assignments"].first
+        status, response = Xapi.get("assignments", track_id, slug)
+        data = JSON.parse(response)
+        if status == 404
+          flash[:notice] = data['error']
+          redirect '/'
+        end
+        data = data["assignments"].first
         problem = Problem.new(data['track'], data['slug'])
         text = data["files"].find {|filename, code| filename =~ /test/i || filename =~ /\.t$/}.last
         erb :"exercises/test_suite", locals: {problem: problem, text: text}
       end
 
       get '/exercises/:track_id/:slug/readme' do |track_id, slug|
-        _, response = Xapi.get("assignments", track_id, slug)
-        data = JSON.parse(response)["assignments"].first
+        status, response = Xapi.get("assignments", track_id, slug)
+        data = JSON.parse(response)
+        if status == 404
+          flash[:notice] = data['error']
+          redirect '/'
+        end
+        data = data["assignments"].first
         problem = Problem.new(data['track'], data['slug'])
         text = data["files"].find {|key, value| key == "README.md"}.last
         erb :"exercises/readme", locals: {problem: problem, text: text}
