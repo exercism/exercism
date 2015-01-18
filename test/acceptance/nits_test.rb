fa13892e974930dc615963727f0934186d587fc1
Chris D'Ambrosio <cdambrosio@gmail.com>
2015-01-18 12:39:00 -0800

    Pass submissions to 'submissions/comment' erb

--- test/acceptance/nits_test.rb
+++ test/acceptance/nits_test.rb
@@ -83,4 +83,13 @@ class NitsTest < AcceptanceTestCase
       assert_content "You haven't received any nitpicks yet"
     end
   end
+
+  def test_navigate_to_edit_nit_page
+    nit = submission.comments.create(user: @user, body: "Test nit given body")
+
+    with_login(@user) do
+      visit "/submissions/#{submission.key}/nits/#{nit.id}/edit"
+      assert_content "Edit Nitpick"
+    end
+  end
 end
