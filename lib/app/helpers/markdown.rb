97b1014c6c48162ceb54386f4e0399e4bb2c181e
LNA <LAllen@8thLight.com>
2015-01-22 22:29:36 -0600

    Allows md to find language

--- lib/app/helpers/markdown.rb
+++ lib/app/helpers/markdown.rb
@@ -4,7 +4,10 @@ require 'loofah'
 module ExercismWeb
   module Helpers
     module Markdown
-      def md(text, language = nil)
+     def md(text, language = nil)
+         language = language.split('.')[1] if language.include?('.')
+         #needs a test
+
         ConvertsMarkdownToHTML.convert(language ? "```#{language}\n#{text}\n```" : text)
       end
     end
