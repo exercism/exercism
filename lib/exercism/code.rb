5896f5bf4bd4fab4a804b73115b3853e96dd9f61
Katrina Owen <katrina.owen@gmail.com>
2015-01-30 07:09:47 -0800

    Remove unused code

--- lib/exercism/code.rb
+++ lib/exercism/code.rb
@@ -3,34 +3,6 @@ class Exercism
 end
 
 class Code
-  LANGUAGES = {
-    'clj' => 'clojure',
-    'coffee' => 'coffeescript',
-    'cpp' => 'cpp',
-    'cs' => 'csharp',
-    'd' => 'dlang',
-    'erl' => 'erlang',
-    'exs' => 'elixir',
-    'fs' => 'fsharp',
-    'go' => 'go',
-    'hs' => 'haskell',
-    'java' => 'java',
-    'js' => 'javascript',
-    'lisp' => 'lisp',
-    'lua' => 'lua',
-    'm' => 'objective-c',
-    'ml' => 'ocaml',
-    'nim' => 'nimrod',
-    'php' => 'php',
-    'pl' => 'perl5', # Usually .pl for scripts, .pm for modules, .t for test files
-    'pm' => 'perl5',
-    'py' => 'python',
-    'rb' => 'ruby',
-    'rs' => 'rust',
-    'scala' => 'scala',
-    'swift' => 'swift'
-  }
-
   attr_reader :path
   def initialize(path)
     @path = path
