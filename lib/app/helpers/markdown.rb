6571ef1a972e8d22abf8d23f07171bbbbfaaddc4
Katrina Owen <katrina.owen@gmail.com>
2015-02-08 09:13:44 -0800

    Don't assume markdown is code. Fixes #2204

--- lib/app/helpers/markdown.rb
+++ lib/app/helpers/markdown.rb
@@ -4,10 +4,7 @@ require 'loofah'
 module ExercismWeb
   module Helpers
     module Markdown
-         #needs a test
-      def md(text, language = "")
-        language = language.split('.')[1] if language.include?('.')
-
+      def md(text, language=nil)
         ConvertsMarkdownToHTML.convert(language ? "```#{language}\n#{text}\n```" : text)
       end
     end
