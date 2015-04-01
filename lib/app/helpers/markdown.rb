11254a0f56df24aa27f538495dff7c297a3d19c7
mtakac <takacmarek1221@gmail.com>
2015-04-01 06:10:23 +0200

    Fixed syntax highlighting for Objective-C
    Bla|

--- lib/app/helpers/markdown.rb
+++ lib/app/helpers/markdown.rb
@@ -5,6 +5,10 @@ module ExercismWeb
   module Helpers
     module Markdown
       def md(text, language=nil)
+        # HACK: Rouge needs Objective C get in form 'objective_c' for correct
+        # syntax highlighting
+        language = (language && language == 'objective-c') ? 'objective_c' : language
+
         ConvertsMarkdownToHTML.convert(language ? "```#{language}\n#{text}\n```" : text)
       end
     end
