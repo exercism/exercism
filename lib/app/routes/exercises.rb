832483835afc1236043e7e7ff3a0f4a14c851e47
SaiVardhan <saimca03@gmail.com>
2015-04-02 16:05:24 +0530

    hound fixes

--- lib/app/routes/exercises.rb
+++ lib/app/routes/exercises.rb
@@ -139,12 +139,14 @@ module ExercismWeb
         content_type :json
         submission = Submission.find_by_key(params[:key])
         begin
-          blob = Octokit.blob("#{submission.user.username}/#{submission.slug}", params[:sha])
+          blob = Octokit.blob("#{submission.user.username}/#{submission.slug}", 
+                               params[:sha])
           src_classifer = SourceClassifier.new(File.join(File.dirname(__FILE__), "../../../bin/", 
                                                "trainer.bin"))
           result =  Base64.decode64(blob.content)
           source_language = src_classifer.identify(result)
-          marked_content = ConvertsMarkdownToHTML.convert("```#{source_language.downcase}\n#{result}\n```")
+          marked_content = ConvertsMarkdownToHTML.convert("```#{source_language.downcase}\n
+                                                            #{result}\n```")
           content = { data: marked_content } 
           content.to_json
         rescue Exception => e
