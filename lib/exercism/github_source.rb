c797a0ae3bbdbc8ff5b09f551ca99ecaa4c92fc3
code-R <vamsi.skrishna@gmail.com>
2015-04-07 12:06:17 +0530

    minor refactoring

--- lib/exercism/github_source.rb
+++ lib/exercism/github_source.rb
@@ -13,11 +13,7 @@ class GithubSource
       c.login = 'SaiPramati'
       c.password = 'pramati123'
     end
-    git_tree_source = Octokit.tree("#{submission.user.username}/#{submission.slug}",
-                                    submission.solution.values.first, recursive: true)
     result = []
-    sorted_blobs = git_tree_source.tree.select{ |node| node.type == "blob" }
-    sorted_trees = git_tree_source.tree.select{ |node| node.type == "tree" }
 
     (sorted_trees + sorted_blobs).each do |node|
       node_contents = {}
@@ -37,4 +33,18 @@ class GithubSource
     end
     result.to_json
   end
+  private
+    def sorted_blobs
+      git_tree_source.tree.select{ |node| node.type == "blob" }
+    end
+
+    def sorted_trees
+      git_tree_source.tree.select{ |node| node.type == "tree" }
+    end
+
+    def git_tree_source
+      repo_info = "#{submission.user.username}/#{submission.slug}"
+      commit_id = submission.solution.values.first
+      @git_tree_source ||= Octokit.tree(repo_info, commit_id, recursive: true)
+    end
 end
