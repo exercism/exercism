efaf404ae86078f81cabafcb1e980da5b836f828
Katrina Owen <katrina.owen@gmail.com>
2015-04-26 12:10:28 +0200

    Silence bugsnag in test environment

--- config/bugsnag.rb
+++ config/bugsnag.rb
@@ -1,4 +1,5 @@
 Bugsnag.configure do |config|
+  config.release_stage = ENV["RACK_ENV"]
   config.notify_release_stages = ["production", "development"]
   config.api_key = ENV['BUGSNAG_API_KEY']
   config.project_root = File.expand_path("../..", __FILE__)
