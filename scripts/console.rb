64816598ba3b9650b96a377a29cdfc17bf4fc33b
Katrina Owen <katrina.owen@gmail.com>
2015-05-02 12:16:51 -0700

    Delete stray requires for email stuff
    I missed these the first time around

--- scripts/console.rb
+++ scripts/console.rb
@@ -2,7 +2,6 @@ $:.unshift File.expand_path("./../../lib", __FILE__)
 require 'bundler'
 Bundler.require
 require 'exercism'
-require 'services'
 
 ENV['RACK_ENV'] ||= 'development'
 
--- scripts/env.rb
+++ scripts/env.rb
@@ -2,6 +2,5 @@ $:.unshift File.expand_path("./../../lib", __FILE__)
 require 'bundler'
 Bundler.require
 require 'exercism'
-require 'services'
 
 ENV['RACK_ENV'] ||= 'development'
