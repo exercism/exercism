23f5776ab6c13619d151c506566cb06ca5876511
Michael Batchelor <batchelor@nationbuilder.com>
2015-05-07 17:13:21 -0700

    persist url to everlasting exercises link, that way the username can change

--- lib/app/routes/exercises.rb
+++ lib/app/routes/exercises.rb
@@ -16,6 +16,21 @@ module ExercismWeb
         erb :"nitpick/index", locals: locals
       end
 
+      get '/exercises/:key' do |key|
+        exercise = UserExercise.find_by_key(key)
+        if exercise.nil?
+          flash[:notice] = "Couldn't find that exercise."
+          redirect '/'
+        end
+
+        if exercise.submissions.empty?
+          # We have orphan exercises at the moment.
+          flash[:notice] = "That submission no longer exists."
+          redirect '/'
+        end
+        redirect "/submissions/%s" % exercise.submissions.last.key
+      end
+
       get '/submissions/:key' do |key|
         submission = Submission.includes(:user, comments: :user).find_by_key(key)
         unless submission
