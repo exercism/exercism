3e4fca15831d327f38b52d3bfc383ba1e2ea52fc
SaiVardhan <saimca03@gmail.com>
2015-06-04 11:18:46 +0530

    cleared recently viewed page issue

--- frontend/app/js/tree_source.js
+++ frontend/app/js/tree_source.js
@@ -1,22 +1,25 @@
-$('#git-tree-content').on('select_node.jstree', function(e, data){
-  var gitNode = data.node.data;
-  var sha = gitNode.sha;
-  if(gitNode.type === 'file'){
-  $.ajax({
-    type: 'get',
-    dataType: 'json',
-    url: '/submissions/'+key+'/blobs/'+sha,
-    success : function(response) {
-      $('#error-message').html('');
-      $('#git-code').html(response.data);
-    },
-    error: function(xhr, status, error) {
-      var errorResponse = JSON.parse(xhr.responseText);
-      $('#error-message').html(errorResponse.message);
-    }
-  });
+if (typeof treeContent === 'undefined') {
+  treeContent = '';
 }
-}).jstree({ 'core' : {
-  'data' : treeContent 
-   } 
-});
+$('#git-tree-content').on('select_node.jstree', function(e, data){
+        var gitNode = data.node.data;
+        var sha = gitNode.sha;
+        if(gitNode.type === 'file'){
+        $.ajax({
+          type: 'get',
+          dataType: 'json',
+          url: '/submissions/'+key+'/blobs/'+sha,
+          success : function(response) {
+            $('#error-message').html('');
+            $('#git-code').html(response.data);
+          },
+          error: function(xhr, status, error) {
+            var errorResponse = JSON.parse(xhr.responseText);
+            $('#error-message').html(errorResponse.message);
+          }
+        });
+      }
+      }).jstree({ 'core' : {
+        'data' : treeContent 
+         } 
+      });
