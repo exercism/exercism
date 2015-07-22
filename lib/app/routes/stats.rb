5b475907e8b56c4c01a80228c122ced3d1772f13
Katrina Owen <katrina.owen@gmail.com>
2015-07-22 11:33:02 -0600

    Fix bug in stats route
    We missed a spot when we refactored some of the track helper methods.

--- lib/app/routes/stats.rb
+++ lib/app/routes/stats.rb
@@ -2,18 +2,24 @@ module ExercismWeb
   module Routes
     class Stats < Core
       get '/stats' do
-        redirect "/stats/#{tracks.active.first.track_id}"
+        redirect "/stats/#{active_tracks.first.slug}"
       end
 
       get '/stats/:track_id' do |track_id|
-        track = tracks.find(track_id)
+        track = tracks.find {|t| t.id == track_id}
         if track.nil?
           status 404
           erb :"errors/not_found"
         else
-          progress = ExercismWeb::Presenters::Progress.new(track.id, track.language)
-          erb :"stats/show", locals: {tracks: tracks.active, progress: progress}
+          progress = ExercismWeb::Presenters::Progress.new(track.slug, track.language)
+          erb :"stats/show", locals: {tracks: active_tracks, progress: progress}
         end
+      end
+
+      private
+
+      def active_tracks
+        @active_tracks ||= tracks.select(&:active?)
       end
     end
   end
