2dbde988ece102e02bbc6d5e0951a8b09e77f63f
Katrina Owen <katrina.owen@gmail.com>
2015-09-01 22:08:31 -0600

    Use archive flag in profile and account
    The archive flag replaces the state field. Instead of showing tables for current, hibernating,
    and completed, we show only tables for current and archived.
    
    The profile and account pages now link to the exercise rather than the submission.
    
    This removes the comment count, since that is a bit ambiguous when we're talking about an
    exercise (is this all the comments on all the submissions? just the comments on the latest iteration?).

--- lib/app/presenters/profile.rb
+++ lib/app/presenters/profile.rb
@@ -9,16 +9,24 @@ class Profile
     user.username
   end
 
-  def has_current_submissions?
-    user.submissions.pending.count > 0
+  def exercises
+    @exercises ||= user.exercises.where(archived: false)
   end
 
-  def has_completed_submissions?
-    user.submissions.done.count > 0
+  def archived_exercises
+    @archived_exercises ||= user.exercises.where(archived: true)
   end
 
-  def has_hibernating_submissions?
-    user.submissions.hibernating.count > 0
+  def can_access?(exercise)
+    narcissistic? || manager? || current_user.nitpicker_on?(exercise.problem)
+  end
+
+  def has_current_exercises?
+    exercises.count > 0
+  end
+
+  def has_archived_exercises?
+    archived_exercises.count > 0
   end
 
   def teams
@@ -29,24 +37,12 @@ class Profile
     teams.any?
   end
 
-  def current
-    user.active_submissions
-  end
-
-  def hibernating_submissions
-    user.submissions.hibernating
-  end
-
-  def completed_in(track_id)
-    user.completed_submissions_in(track_id).reverse
+  def archived_in(track_id)
+    archived_exercises.where(language: track_id).reverse
   end
 
   def track_ids
-    @track_ids ||= user.submissions.done.pluck('language').uniq
-  end
-
-  def github_link
-    %{<a target="_blank" href="https://github.com/#{user.username}">#{user.username}</a>}
+    @track_ids ||= archived_exercises.pluck('language').uniq
   end
 
   def submission_link(submission)
@@ -54,22 +50,6 @@ class Profile
      %{<a href="/submissions/#{submission.key}">#{submission.name}&nbsp;<i class="fa fa-star"></i></a>}
     else
      %{<a href="/exercises/#{submission.track_id}/#{submission.slug}">#{submission.name}</a>}
-    end
-  end
-
-  def no_current_submissions_message
-    if narcissistic?
-      "You have not submitted any exercises lately."
-    else
-      "#{username} has not submitted any exercises lately."
-    end
-  end
-
-  def no_completed_submissions_message
-    if narcissistic?
-      "You have not completed any exercises yet."
-    else
-      "#{username} has not completed any exercises yet."
     end
   end
 
@@ -82,5 +62,4 @@ class Profile
   end
 
   attr_reader :user, :current_user
-
 end
