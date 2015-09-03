e78a1a8bebc3efbbf79eaa8f615d69582713d747
Katrina Owen <katrina.owen@gmail.com>
2015-09-03 10:53:49 -0600

    Stop wrangling state on unsubmit

--- lib/exercism/use_cases/unsubmit.rb
+++ lib/exercism/use_cases/unsubmit.rb
@@ -16,7 +16,6 @@ class Unsubmit
 
     raise NothingToUnsubmit.new  if submission.nil?
     raise SubmissionHasNits.new  if submission.nit_count > 0
-    raise SubmissionDone.new     if submission.done?
     raise SubmissionTooOld.new   if submission.older_than?(TIMEOUT)
 
     options = {
@@ -27,10 +26,6 @@ class Unsubmit
     }
     previous_submission = Submission.reversed.where(options).limit(1).first
 
-    unless previous_submission.nil?
-      previous_submission.state = 'pending'
-      previous_submission.save
-    end
     submission.destroy
     Hack::UpdatesUserExercise.new(submission.user_id, submission.track_id, submission.slug).update
   end
