ed78ebc8c32fe9121cb66498114eb1aa08fc8f87
Micah Frost <micah.frost@gmail.com>
2015-11-23 11:06:26 -0700

    Remove langauge option from #md helper
    Now, the `#md` helper only accepts Markdown. We no longer try to
    fence off input with a supplied language. The last remaining place
    the argument was used has been converted to `#syntax`.
    
    The markdown helper test has been removed as the functionality
    is now tested by the new syntax helper as well as the
    Markdown to HTML converter.

--- lib/app/helpers/markdown.rb
+++ lib/app/helpers/markdown.rb
@@ -4,18 +4,8 @@ require 'loofah'
 module ExercismWeb
   module Helpers
     module Markdown
-      ROUGE_LANG = {
-        'objective-c' => 'objective_c',
-        'elisp' => 'common_lisp',
-        'plsql' => 'sql',
-        'ecmascript' => 'javascript',
-      }
-
-      def md(text, language=nil)
-        # HACK: Some languages have different names in Rouge
-        language = ROUGUE_LANG[language] || language
-
-        ConvertsMarkdownToHTML.convert(language ? "```#{language}\n#{text}\n```" : text)
+      def md(text)
+        ConvertsMarkdownToHTML.convert(text)
       end
     end
   end
