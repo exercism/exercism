720be48f036a03e6e7c2e1866188ed55657ad91c
Micah Frost <micah.frost@gmail.com>
2015-11-23 11:35:08 -0700

    Simplify and extend #syntax helper specs

--- test/app/helpers/syntax_test.rb
+++ test/app/helpers/syntax_test.rb
@@ -10,34 +10,30 @@ class SyntaxHelperTest < Minitest::Test
   end
 
   def test_inline_markdown
-  code = <<CODE
+    code = <<CODE
 """
-```Leap year
-Most years are not leap years.
-Is a leap year if every year that is evenly divisible by 4.
-  Is not a leaper if it is evenly divisible by 100.
-    Is a leap year if evenly divisible by 400.
-Test driven development forced us to care about most of of the cases right away.
-The examples in the test made the read me much clearer.
-Python error about module meant to suggest a particular file name.
-Pythons logical and operator simpler than Javascript's ampersands.(&)
-Exercism readme's structured as nonrepeating text was unclear.
-Acceptance criteria for even the most basic cases can be unclear.
-Exercism's domain specific language takes some getting used to each time.
+Method usage:
+```
+foo.bar()
 ```
 """
 
-def is_leap_year(year):
-    if year % 4 != 0:
-        return False
-    elif year % 4 == 0  and year % 400 == 0:
-        return True
-    elif year % 4 == 0  and year % 100 == 0:
-        return False
+def bar():
     return True
 CODE
 
     output = helper.syntax(code, "python")
     refute_match('<p>', output)
+    assert_match('<div class="highlight python">', output)
+  end
+
+  def test_language_option
+    code = <<CODE
+def bar():
+    return True
+CODE
+
+    output = helper.syntax(code, "python")
+    assert_match('<div class="highlight python">', output)
   end
 end
