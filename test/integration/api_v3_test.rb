7cfdb91105ad304e65c81ead15607763e9069f81
Ben Biddington <ben.biddington@gmail.com>
2015-12-01 15:56:06 +1300

    Delete experiment

--- test/integration/api_v3_test.rb
+++ test/integration/api_v3_test.rb
@@ -2,18 +2,6 @@ require "./test/test_helper"
 require "./test/x_helper"
 require 'faraday'
 
-class SampleClient
-  class << self
-    @@client = X::Xapi
-    
-    def get(url)
-      reply = @@client.request url
-      
-      JSON.parse(reply.body)  
-    end
-  end
-end
-
 # https://github.com/exercism/exercism.io/issues/2631
 
 class ApiV3Examples < Minitest::Test
