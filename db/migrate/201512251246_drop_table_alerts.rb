c81aa11be983bcef299a5a2bd8297d21a8660f1b
Katrina Owen <katrina.owen@gmail.com>
2015-12-25 12:53:35 -0700

    Drop table alerts

---
+++ db/migrate/201512251246_drop_table_alerts.rb
@@ -0,0 +1,18 @@
+class DropTableAlerts < ActiveRecord::Migration
+  def up
+    drop_table :alerts
+  end
+
+  def down
+    create_table :alerts do |t|
+      t.integer   :user_id,       null: false
+      t.text      :text
+      t.string    :url
+      t.string    :link_text
+      t.boolean   :read
+      t.timestamps                null: false
+    end
+
+    add_index :alerts, :user_id
+  end
+end
