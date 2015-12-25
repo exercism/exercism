672acfee44bf858fde50500a61dc54cf259ab76a
Katrina Owen <katrina.owen@gmail.com>
2015-12-25 13:05:14 -0700

    Drop unused completion field from user exercise

---
+++ db/migrate/201512251301_drop_completed_at_field_from_user_exercises.rb
@@ -0,0 +1,9 @@
+class DeleteOldSolutionColumnsOnUserExercise < ActiveRecord::Migration
+  def up
+    remove_column :user_exercises, :completed_at
+  end
+
+  def down
+    add_column :user_exercises, :completed_at, :timestamp
+  end
+end
