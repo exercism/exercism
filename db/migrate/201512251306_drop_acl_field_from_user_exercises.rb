81d91f65c51df43ecea1de1b03598fbbbe89402a
Katrina Owen <katrina.owen@gmail.com>
2015-12-25 13:08:04 -0700

    drop is_nitpicker column from user exercises

---
+++ db/migrate/201512251306_drop_acl_field_from_user_exercises.rb
@@ -0,0 +1,9 @@
+class DropAclFieldFromUserExercises < ActiveRecord::Migration
+  def up
+    remove_column :user_exercises, :is_nitpicker
+  end
+
+  def down
+    add_column :user_exercises, :is_nitpicker, :boolean, :default => false
+  end
+end
