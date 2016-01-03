c8ee625c113cea22b7817a12f47e11c187044852
Alex Wheeler <afwheeler92@gmail.com>
2016-01-03 01:02:32 -0500

    changes UserExercise#current to order results by language and id ascending -this makes the results more understandable since language exercises are grouped together

--- test/exercism/user_exercise_test.rb
+++ test/exercism/user_exercise_test.rb
@@ -31,5 +31,15 @@ class UserExerciseTest < Minitest::Test
     exercise.reload
     assert_equal 12, exercise.nit_count
   end
+
+  def test_current
+    alice = User.create(username: 'alice')
+    closure_1 = UserExercise.create(user: alice, archived: false, language: 'closure', iteration_count: 1)
+    ruby_1 = UserExercise.create(user: alice, archived: false, language: 'ruby', iteration_count: 1)
+    closure_2 = UserExercise.create(user: alice, archived: false, language: 'closure', iteration_count: 1)
+    ruby_2 = UserExercise.create(user: alice, archived: false, language: 'ruby', iteration_count: 1)
+    archived_ruby = UserExercise.create(user: alice, archived: true, language: 'ruby', iteration_count: 1)
+    assert_equal UserExercise.current, [closure_1, closure_2, ruby_1, ruby_2]
+  end
 end
 
