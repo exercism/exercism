d519266cc3b95e0295e80bd1b12d71367ecd6217
Katrina Owen <katrina.owen@gmail.com>
2016-01-22 11:28:31 -0700

    Add sidekiq queue for experimental codereview stuff

---
+++ lib/jobs/codereview.rb
@@ -0,0 +1,15 @@
+require 'sidekiq'
+
+module Jobs
+  class CodeReview
+    include Sidekiq::Worker
+    sidekiq_options :queue => :codereview
+
+    def perform(_)
+      # This exists solely to get the job scheduled.
+      # There is a stand-alone worker that processes
+      # the jobs.
+      # See https://github.com/exercism/rikki
+    end
+  end
+end
