64a8cdca65df9618327ed6c062afb87c80d7097e
Katrina Owen <katrina.owen@gmail.com>
2016-01-22 16:29:40 -0700

    Delete leftover onboarding stuff

--- lib/exercism/assumable_user.rb
+++ lib/exercism/assumable_user.rb
@@ -1,25 +1,16 @@
 class AssumableUser
   class Identity
 
-    attr_reader :username, :github_id, :steps
-    def initialize(username, github_id, steps)
+    attr_reader :username, :github_id
+    def initialize(username, github_id)
       @username = username
       @github_id = github_id
-      @steps = steps
-    end
-
-    def progress
-      @progress ||= Onboarding.step(steps)
-    end
-
-    def status
-      @status ||= Onboarding.status(steps)
     end
   end
 
   def self.all
     ::User.order('created_at DESC').limit(100).map {|user|
-      Identity.new(user.username, user.github_id, user.onboarding_steps)
-    }.sort_by(&:progress).reverse
+      Identity.new(user.username, user.github_id)
+    }
   end
 end
