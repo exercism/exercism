ac75b803f8a12772635125dc26da99517270688e
Katrina Owen <katrina.owen@gmail.com>
2016-01-22 17:47:17 -0700

    Leave relative paths in solution

--- lib/exercism/iteration.rb
+++ lib/exercism/iteration.rb
@@ -1,33 +1,15 @@
 class Iteration
-  attr_reader :solution, :paths, :comment
+  attr_reader :solution, :comment, :track_id, :slug
 
-  def initialize(solution, track_id=nil, slug=nil, comment: nil)
+  def initialize(solution, track_id, slug, comment: nil)
     @track_id = track_id
     @slug = slug
     @comment = comment
-    @paths = solution.keys.map { |path| Code.new(path) }
 
     @solution = {}
     solution.each do |path, contents|
-      @solution[filename(path)] = contents.strip
+      filename = path.split(/\\|\//).join('/').gsub(/^#{track_id}\/#{slug}\//, "")
+      @solution[filename] = contents.strip
     end
-  end
-
-  def track_id
-    @track_id || max_occurrences(paths.map(&:track))
-  end
-
-  def slug
-    @slug || max_occurrences(paths.map(&:slug))
-  end
-
-  private
-
-  def max_occurrences(ary)
-    ary.group_by(&:itself).values.max_by(&:size).first
-  end
-
-  def filename(path)
-    Code.new(path).filename
   end
 end
