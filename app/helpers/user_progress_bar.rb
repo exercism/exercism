9c1f645533b5a8452d96a2ce559f55bbc6417682
Katrina Owen <katrina.owen@gmail.com>
2016-01-26 16:17:53 -0700

    Fix infinity error on profile page. Closes #2740

--- app/helpers/user_progress_bar.rb
+++ app/helpers/user_progress_bar.rb
@@ -2,6 +2,7 @@ module ExercismWeb
   module Helpers
     module UserProgressBar
       def percent progress_values
+        return "0" if progress_values[1].zero?
         (progress_values[0]/progress_values[1].to_f * 100).to_i.to_s
       end
 
