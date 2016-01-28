6e350e83d8aa0335a44f7ec84a92a1eee3ca00f2
Katrina Owen <katrina.owen@gmail.com>
2016-01-28 13:26:52 -0700

    Fix broken profiles due to mangled submissions
    Right now there's a bug where older clients submit code that is not recognized as
    belonging to a specific track. This causes the progress bars on the profile page
    to blow up.

--- lib/exercism/user_progression.rb
+++ lib/exercism/user_progression.rb
@@ -3,14 +3,13 @@ class UserProgression
 
   def self.user_progress(user)
     track_complete_exercises = Hash.new 0
-    user.exercises.where('iteration_count > 0').each{|exe| track_complete_exercises[exe.problem.language] += 1}
-    tracks = track_count
+    user.exercises.where("language<>''").where('iteration_count > 0').each{|exe| track_complete_exercises[exe.problem.language] += 1}
+    tracks = track_counts
     track_complete_exercises.each{|k, v| track_complete_exercises[k] = [v, tracks[k]]}
     track_complete_exercises
   end
 
-  private
-  def self.track_count
+  def self.track_counts
     Hash[X::Track.all.collect{|t| [t.language, t.problems.count]}]
   end
 end
