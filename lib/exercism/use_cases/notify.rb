5186ae41b33d0bdac6ba7a5b9a81d160dc3907d1
Katrina Owen <katrina.owen@gmail.com>
2016-03-10 13:14:53 -0700

    Fix typo

--- lib/exercism/use_cases/notify.rb
+++ lib/exercism/use_cases/notify.rb
@@ -1,6 +1,6 @@
 class Notify
   def self.everyone(iteration, action, actor)
-    ConversationSubscription.subscriber_ids(submission).each do |id|
+    ConversationSubscription.subscriber_ids(iteration).each do |id|
       next if id == actor.id
       Notification.on(iteration, user_id: id, action: action, actor_id: actor.id)
     end
