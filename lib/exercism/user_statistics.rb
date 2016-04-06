d2dfd2ba754721854fae0fe994b72de0d955a370
Katrina Owen <katrina.owen@gmail.com>
2016-04-06 15:27:25 -0600

    Create new API endpoint for user statistics
    * created a new api route for user statistics: '/users/:username/statistics'
    * wrote controller tests for users with and without submitted problems.
    * created user statistics service to handle the api call to this endpoint and response formatting.
    * created method on user model for response formatting

---
+++ lib/exercism/user_statistics.rb
@@ -0,0 +1,28 @@
+class UserStatistics
+  def self.of(user)
+    by_username(user)
+  end
+
+  def self.by_username(user)
+    {
+      user: user.public_user_attributes,
+      statistics: user_problem_statistcs(user)
+    }
+  end
+
+  def self.problem_completion(user, track)
+    user.exercises.where(language: track.id).map do |exercise|
+      exercise.slug
+    end
+  end
+
+  def self.user_problem_statistcs(user)
+    X::Track.all.map do |track|
+      { track_id: track.id,
+        language: track.language,
+        total: track.problems.count,
+        completed: problem_completion(user, track) }
+    end
+  end
+
+end
