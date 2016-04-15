d8da945b8b5eac3541558cbb930d3a013d1bc252
Katrina Owen <katrina.owen@gmail.com>
2016-04-15 15:36:52 -0600

    Handle error states in README

--- test/x/exercise_test.rb
+++ test/x/exercise_test.rb
@@ -17,17 +17,5 @@ module X
         assert_equal expected_files, expected.files
       end
     end
-
-    def test_readme
-      f = './test/fixtures/xapi_v3_exercise_readme.json'
-      Xapi.stub(:get, [200, File.read(f)]) do
-        expected = Exercise::Readme.find('fake', 'three')
-
-        assert_equal "Fake", expected.language
-        assert_equal "Three", expected.name
-        assert_equal "This is three.", expected.blurb
-        assert_match "[view source](http://example.com/3)", expected.readme
-      end
-    end
   end
 end
