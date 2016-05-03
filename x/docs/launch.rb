6f04d829df552f5df02c73f2890457b1d9b8d481
joeltaylor <joel@codeprivy.com>
2016-05-02 20:47:44 -0400

    gsub LAUNCH.md REPO for real url

--- x/docs/launch.rb
+++ x/docs/launch.rb
@@ -1,15 +1,19 @@
 module X
   module Docs
     class Launch
+      def initialize(repository)
+        @repository = repository
+      end
 
       def launch
         read
       end
 
       private
+      attr_reader :repository
 
       def read
-        File.read("./x/docs/md/track/LAUNCH.md")
+        File.read("./x/docs/md/track/LAUNCH.md").gsub('REPO', repository)
       end
     end
   end
