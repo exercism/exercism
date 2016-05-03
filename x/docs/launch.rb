604dd6b4fc8660073a4cdd01e1447d11c3f599f4
Joel Taylor <joel@codeprivy.com>
2016-05-03 00:11:17 -0400

    Add launch help tab to upcoming language tracks (#2825)

---
+++ x/docs/launch.rb
@@ -0,0 +1,20 @@
+module X
+  module Docs
+    class Launch
+      def initialize(repository)
+        @repository = repository
+      end
+
+      def launch
+        read
+      end
+
+      private
+      attr_reader :repository
+
+      def read
+        File.read("./x/docs/md/track/LAUNCH.md").gsub('REPO', repository)
+      end
+    end
+  end
+end
