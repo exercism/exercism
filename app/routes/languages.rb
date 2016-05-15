2d70e3cadcdbbdca7ce9f1fed4dfc7b0bafc7097
Paul Schipitsch <pschipitsch@truecar.com>
2016-05-14 18:10:32 -0700

    Handle request for track that does not exist

--- app/routes/languages.rb
+++ app/routes/languages.rb
@@ -11,11 +11,17 @@ module ExercismWeb
       end
 
       get '/languages/:track_id' do |track_id|
-        track = X::Track.find(track_id)
-        erb :"languages/language", locals: {
-          track: track,
-          docs: X::Docs::Launch.new(track.repository),
-        }
+        _, body = X::Xapi.get('tracks', track_id)
+        parsed_body = JSON.parse(body)
+        if parsed_body['error'] == "No track '#{track_id}'"
+          erb :"languages/not_found", locals: { track_id: track_id }
+        else
+          track = X::Track.new(parsed_body['track'])
+          erb :"languages/language", locals: {
+            track: track,
+            docs: X::Docs::Launch.new(track.repository),
+          }
+        end
       end
     end
   end
