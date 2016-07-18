b6287503e7560b0df5256e1a295f97465d31d037
ramyaravindranath <ramya.ravindranathp@gmail.com>
2016-07-18 20:37:32 +1000

    fixes issues as per katrina's comment

--- app/routes/languages.rb
+++ app/routes/languages.rb
@@ -1,47 +1,46 @@
 require_relative '../../x'
 
 module ExercismWeb
-  module Routes
-    class Languages < Core
-      get '/languages' do
-        tracks = X::Track.all
-        active, inactive = tracks.partition(&:active?)
-        inactive.sort! { |a, b| b.problems.count <=> a.problems.count }
-        erb :"languages/index", locals: { active: active, inactive: inactive }
-      end
+	module Routes
+		class Languages < Core
+			get '/languages' do
+				tracks = X::Track.all
+				active, inactive = tracks.partition(&:active?)
+				inactive.sort! { |a, b| b.problems.count <=> a.problems.count }
+				erb :"languages/index", locals: { active: active, inactive: inactive }
+			end
 
-      get '/repository' do
-        tracks = X::Track.all
-        active, inactive = tracks.partition(&:active?)
-        inactive.sort! { |a, b| b.problems.count <=> a.problems.count }
-        planned, upcoming = inactive.partition(&:planned?)
-        erb :"languages/repository", locals: { active: active, inactive: inactive,
-                                               planned: planned, upcoming: upcoming }
-      end
+			get '/repositories' do
+				active, inactive = X::Track.all.partition(&:active?)
+				inactive.sort! { |a, b| b.problems.count <=> a.problems.count }
+				planned, upcoming = inactive.partition(&:planned?)
+				erb :"languages/repositories", locals: { active: active, inactive: inactive,
+																							 planned: planned, upcoming: upcoming }
+			end
 
-      get '/languages/:track_id' do |track_id|
-        _, body = X::Xapi.get('tracks', track_id)
-        parsed_body = JSON.parse(body)
-        if parsed_body['error'] == "No track '#{track_id}'"
-          erb :"languages/not_found", locals: { track_id: track_id }
-        else
-          track = X::Track.new(parsed_body['track'])
-          erb :"languages/language", locals: {
-            track: track,
-            docs: X::Docs::Launch.new(track.repository, track.checklist_issue),
-          }
-        end
-      end
+			get '/languages/:track_id' do |track_id|
+				_, body = X::Xapi.get('tracks', track_id)
+				parsed_body = JSON.parse(body)
+				if parsed_body['error'] == "No track '#{track_id}'"
+					erb :"languages/not_found", locals: { track_id: track_id }
+				else
+					track = X::Track.new(parsed_body['track'])
+					erb :"languages/language", locals: {
+						track: track,
+						docs: X::Docs::Launch.new(track.repository, track.checklist_issue),
+					}
+				end
+			end
 
-      get '/languages/:track_id/contribute' do |track_id|
-        track = X::Todo.track(track_id)
-        if track.any?
-          erb :"languages/contribute", locals: { todos: track.with_implementations,
-                                                 language: track.language,
-                                                 repository: track.repository,
-                                                 track_id: track_id }
-        end
-      end
-    end
-  end
+			get '/languages/:track_id/contribute' do |track_id|
+				track = X::Todo.track(track_id)
+				if track.any?
+					erb :"languages/contribute", locals: { todos: track.with_implementations,
+																								 language: track.language,
+																								 repository: track.repository,
+																								 track_id: track_id }
+				end
+			end
+		end
+	end
 end
