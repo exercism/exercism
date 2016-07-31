5984528a8be1241decdb64f74b108dd150695769
Geoff Hubbard <ghub@instihost.com>
2016-07-31 20:14:48 +0100

    Refactor language/track/contribute page.
    Summary:
    
    Change sort order on the contribute page to sort exercises by decending
    implementations.count then by name alphabetically.
    
    Show helpful 404 page instead of 500 erroring when language track not found.
    
    Increase test coverage.
    
    Details:
    
    test/app/languages_test.rb
      Test coverage for languages/track/contribute
      Add test for track with no unimplemented exercises
      Add test for invalid track_id
    
    app/routes/languages.rb
      Show helpful 404 page instead of 500 erroring when language track not found.
      Removed condtional with unimplemented 'else' branch.
      Pass object to template rather than it's individual properties.
    
    app/views/languages/contribute.erb
      Now contains logic for exercise display order.
      Get track information from exercises object
      Display helpful message when there are no unimplemented exercises.
      Sort exercises by decending implementations.count then by name alphabetically.
    
    test/x/todo_test.rb -> test/x/todos/todo_test.rb
      Moved test file to mirror real file location.
      100% test coverage for X:Todo
    
    test/fixtures/xapi_v3_todos.json
      Modified fixture data to look more like test data.
    
    test/fixtures/xapi_v3_todos_none.json
      Added test fixture for track with no unimplemented exercises.
    
    test/x/todos/exercise_test.rb
      100% test coverage for X:Todo:Exercise
    
    x.rb
    x/errors.rb
      Added LanguageNotFound error
    
    x/todos/exercise.rb
      Renamed METHODS to PROPERTIES because that's what they are.
    
    x/todos/todo.rb
      raise LanguageNotFound error if the language is not found.
      Renamed METHODS to PROPERTIES because that's what they are.
      Removed redundant methods.
      include Enumerable and proxy to the exercise array.

--- x/todos/todo.rb
+++ x/todos/todo.rb
@@ -1,31 +1,30 @@
+require 'forwardable'
+require_relative '../errors'
+
 module X
   class Todo
+    include Enumerable
+    extend Forwardable
+
+    def_delegators :@exercises, :each
+
+    PROPERTIES = [:language, :track_id, :repository].freeze
+
+    attr_reader(*PROPERTIES, :exercises)
+
     def self.track(id)
-      _, body = X::Xapi.get('tracks', id, 'todo')
-      new(JSON.parse(body))
+      code, body = X::Xapi.get('tracks', id, 'todo')
+      case code
+      when 200
+        new(JSON.parse(body))
+      else
+        fail LanguageNotFound.new(code: code, body: body, track_id: id)
+      end
     end
-
-    METHODS = [
-      :todos, :language, :track_id, :repository
-    ].freeze
-
-    attr_reader(*METHODS)
 
     def initialize(data)
-      METHODS.each do |name|
-        instance_variable_set(:"@#{name}", data[name.to_s])
-      end
-      @todos = data['todos'].inject([]) do |array, problem|
-        array << Todo::Exercise.new(problem)
-      end
-    end
-
-    def with_implementations
-      todos.select { |x| x.implementations.any? }
-    end
-
-    def any?
-      todos.any?
+      PROPERTIES.each { |name| instance_variable_set(:"@#{name}", data[name.to_s]) }
+      @exercises = data['todos'].map { |problem| Todo::Exercise.new(problem) }
     end
   end
 end
