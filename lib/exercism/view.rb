6b0e6decebf13183b3357f0902adc7ac6d68f5c8
Katrina Owen <kytrinyx@github.com>
2016-10-04 20:39:20 -0600

    Add rake task to delete views below the watermarks

--- lib/exercism/view.rb
+++ lib/exercism/view.rb
@@ -1,2 +1,14 @@
 class View < ActiveRecord::Base
+  def self.delete_below_watermarks
+    sql = <<-SQL
+      DELETE FROM views v
+      USING user_exercises ex, watermarks w
+      WHERE w.user_id=v.user_id
+      AND v.exercise_id=ex.id
+      AND w.track_id=ex.language
+      AND w.slug=ex.slug
+      AND v.last_viewed_at <= w.at;
+    SQL
+    connection.execute(sql)
+  end
 end
