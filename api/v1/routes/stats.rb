607b48b90a12804ff827fdb650f371e633a1b874
Katrina Owen <kytrinyx@github.com>
2016-10-08 05:29:54 -0500

    Swap in trackler in api stats endpoint

--- api/v1/routes/stats.rb
+++ api/v1/routes/stats.rb
@@ -2,8 +2,8 @@ module ExercismAPI
   module Routes
     class Stats < Core
       get '/stats/:track_id/activity/recent' do |track_id|
-        track = X::Track.find(track_id)
-        stats = ExercismLib::Stats.new(track_id, track.problems.map(&:slug))
+        track = Trackler.tracks[track_id]
+        stats = ExercismLib::Stats.new(track_id, track.problems)
         halt 200, { track: { id: track_id, problems: stats.apize } }.to_json
       end
     end
