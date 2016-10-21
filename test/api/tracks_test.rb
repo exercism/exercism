ac21793b6a31971d73f115d625958d78b88cde98
Nick Borromeo <ngborromeo@gmail.com>
2016-10-20 23:45:53 -0700

    Use Trackler for Tracks Route
    Helps with https://github.com/exercism/exercism.io/pull/3164
    
    This removes the call to `Xapi` from the route that checks the status of
    the language. It now uses the Trackler gem to determine if the the track
    exists.
    
    The variable name of the route was also changed from `id` to `language`
    so that it makes a little more sense.

---
+++ test/api/tracks_test.rb
@@ -0,0 +1,20 @@
+require_relative '../api_helper'
+
+class TrackssApiTest < Minitest::Test
+  include Rack::Test::Methods
+  include DBCleaner
+
+  def app
+    ExercismAPI::App
+  end
+
+  def setup
+    super
+    @alice = User.create!(username: 'alice', github_id: 1)
+  end
+
+  def test_language_status_returns_404_when_language_does_not_exist
+    get '/tracks/invalid-language/status', key: @alice.key
+    assert_equal 404, last_response.status
+  end
+end
