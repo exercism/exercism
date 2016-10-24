dfecefe159138b0f33481c36b136765204c22c61
Katrina Owen <kytrinyx@github.com>
2016-10-24 08:11:46 +0100

    Swap trackler into profile dependencies

--- test/exercism/user_tracks_summary_test.rb
+++ test/exercism/user_tracks_summary_test.rb
@@ -8,31 +8,26 @@ class UserTracksSummaryTest < Minitest::Test
   def setup
     super
     @user = User.create
-    @f = './test/fixtures/xapi_v3_tracks.json'
   end
 
   def test_user_tracks_summary_completed
-    X::Xapi.stub(:get, [200, File.read(@f)]) do
-      UserExercise.create(user: @user, language: 'animal', iteration_count: 1)
-      actual = UserTracksSummary.call(@user).first
-      assert_equal UserTracksSummary::TrackSummary, actual.class
-      assert_equal true, actual.completed?
-      assert_equal 1, actual.completed
-      assert_equal false, actual.reviewed?
-    end
+    UserExercise.create(user: @user, language: 'animal', iteration_count: 1)
+    actual = UserTracksSummary.call(@user).first
+    assert_equal UserTracksSummary::TrackSummary, actual.class
+    assert_equal true, actual.completed?
+    assert_equal 1, actual.completed
+    assert_equal false, actual.reviewed?
   end
 
   def test_user_tracks_summary_reviewed
-    X::Xapi.stub(:get, [200, File.read(@f)]) do
-      user2 = User.create
-      exercise = UserExercise.create(user: user2, language: 'animal', iteration_count: 1)
-      submission = Submission.create(user: user2, language: 'animal', slug: 'dog', version: 1, user_exercise: exercise)
-      Comment.create(user: @user, body: 'test', html_body: '<p>test</p>', submission: submission)
-      actual = UserTracksSummary.call(@user).first
-      assert_equal UserTracksSummary::TrackSummary, actual.class
-      assert actual.reviewed?
-      assert_equal 1, actual.reviewed
-      refute actual.completed?
-    end
+    user2 = User.create
+    exercise = UserExercise.create(user: user2, language: 'animal', iteration_count: 1)
+    submission = Submission.create(user: user2, language: 'animal', slug: 'dog', version: 1, user_exercise: exercise)
+    Comment.create(user: @user, body: 'test', html_body: '<p>test</p>', submission: submission)
+    actual = UserTracksSummary.call(@user).first
+    assert_equal UserTracksSummary::TrackSummary, actual.class
+    assert actual.reviewed?
+    assert_equal 1, actual.reviewed
+    refute actual.completed?
   end
 end
