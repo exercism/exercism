ed7137b73a62ddce037aa02bc389b6bc04827dea
Nick Borromeo <ngborromeo@gmail.com>
2016-10-26 01:34:03 -0700

    (fix) adding test for when language is valid

--- test/api/tracks_test.rb
+++ test/api/tracks_test.rb
@@ -17,4 +17,10 @@ class TracksApiTest < Minitest::Test
     get '/tracks/invalid-language/status', key: @alice.key
     assert_equal 404, last_response.status
   end
+
+  def test_language_status_when_language_is_valid_but_has_no_solutions
+    get '/tracks/animal/status', key: @alice.key
+    assert_equal 200, last_response.status
+    assert_match "You haven't submitted any solutions yet.", last_response.body
+  end
 end
