75afb510b7d06b252e532e4e05440b123e920ca3
Geoff Hubbard <ghub@instihost.com>
2016-11-23 07:58:52 +0000

    Give a helpful error message if css not generated.
    If the application.css file has not been generated the error message
    was not very helpful, the new message provides more details about what
    went wrong and what needs to be done to rectify the problem.
    
    Old message:
    
    Errno::ENOENT at /
    No such file or directory @ rb_file_s_mtime - ./public/css/application.css
    
    New message:
    
    IOError at /
    '<developers exercism.io path>/public/css/application.css' not found.
    Try running 'bundle exec compass compile' to generate it.
    For more information: https://github.com/exercism/exercism.io/blob/master/CONTRIBUTING.md#scss

--- app/routes/core.rb
+++ app/routes/core.rb
@@ -102,8 +102,26 @@ module ExercismWeb
         end
 
         def css_url
-          @css_url || "/css/application.css?t=#{File.mtime('./public/css/application.css').to_i}"
+          @css_url ||= css_url_generate('/css/application.css')
         end
+
+        def css_url_generate(resource)
+          css_filename = File.join(settings.public_folder, resource)
+          raise css_url_generate_error(css_filename) unless File.exists?(css_filename)
+          "#{resource}?t=#{File.mtime(css_filename).to_i}"
+        end
+
+        def css_url_generate_error(css_filename)
+          command = "bundle exec compass compile"
+          url = 'https://github.com/exercism/exercism.io/blob/master/CONTRIBUTING.md#scss'
+
+          IOError.new <<-MESSAGE.gsub(/^ */,'')
+            '#{css_filename}' not found.
+            Try running '#{command}' to generate it.
+            For more information: #{url}
+          MESSAGE
+        end
+
       end
     end
   end
