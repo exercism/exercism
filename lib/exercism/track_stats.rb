6aa58fb03ce1d20ec6afef1ca2acce9db0711b4a
Edward Anderson <nilbus@nilbus.com>
2017-04-02 22:14:23 -0400

    Remove unnecessary rubocop disable
    Metrics/MethodLength is apparently loose enough now.

--- lib/exercism/track_stats.rb
+++ lib/exercism/track_stats.rb
@@ -105,7 +105,6 @@ module ExercismLib
       @rows ||= ActiveRecord::Base.connection.execute(sql).to_a
     end
 
-    # rubocop:disable Metrics/MethodLength
     def sql
       <<-SQL
         SELECT s.slug, COUNT(s.id) AS iterations, COUNT(c.id) AS reviews
@@ -119,6 +118,5 @@ module ExercismLib
         GROUP BY s.slug
       SQL
     end
-    # rubocop:enable Metrics/MethodLength
   end
 end
