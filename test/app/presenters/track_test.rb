3afedaaebdce4d7d4637234b09a8bf8b9ad89dcc
Nick Holden <nick@realhq.com>
2017-04-23 07:57:12 -0700

    Clean up Presenters::Track tests

--- test/app/presenters/track_test.rb
+++ test/app/presenters/track_test.rb
@@ -5,20 +5,19 @@ require 'mocha/setup'
 
 class PresentersTrackTest < Minitest::Test
   def setup
-    @track = ExercismWeb::Presenters::Track.new("test_track")
-
-    trackler_tracks = Object.new
     @trackler_track = Object.new
-
-    Trackler.stubs(:tracks).returns(trackler_tracks)
-    trackler_tracks.stubs(:[]).returns(@trackler_track)
     @trackler_track.stubs(:repository).returns("https://github.com/exercism/test_track")
     @trackler_track.stubs(:doc_format).returns("md")
+
+    trackler_tracks = Object.new
+    trackler_tracks.stubs(:[]).returns(@trackler_track)
+
+    Trackler.stubs(:tracks).returns(trackler_tracks)
   end
 
   def test_docs_with_trackler_content
     @trackler_track.stubs(:docs).returns(OpenStruct.new(about: "Track-specific content from Trackler"))
-    topic_doc = @track.docs["about"]
+    topic_doc = ExercismWeb::Presenters::Track.new("test_track").docs["about"]
 
     assert topic_doc.include? "Track-specific content from Trackler"
     assert topic_doc.include? "Help us explain this better!"
@@ -28,16 +27,13 @@ class PresentersTrackTest < Minitest::Test
 
   def test_docs_without_trackler_content
     @trackler_track.stubs(:docs).returns(OpenStruct.new(about: ""))
-    topic_doc = @track.docs["about"]
+    topic_doc = ExercismWeb::Presenters::Track.new("test_track").docs["about"]
 
     assert topic_doc.include? "We're missing a short introduction about the language."
-    refute topic_doc.include? "Help us explain this better!"
-    refute topic_doc.include? "File a GitHub issue at https://github.com/exercism/test_track"
-    refute topic_doc.include? "https://github.com/exercism/test_track/blob/master/docs/TEST_TOPIC.md"
   end
 
   def test_method_delegated_to_trackler_track
     @trackler_track.stubs(:exists?).returns(true)
-    assert_equal @track.exists?, true
+    assert_equal ExercismWeb::Presenters::Track.new("test_track").exists?, true
   end
 end
