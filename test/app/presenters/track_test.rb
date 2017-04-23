827a210d380561a2f2f6913f50081cea27a5fce5
Nick Holden <nick@realhq.com>
2017-04-22 17:25:29 -0700

    Don't stub out the object under test

--- test/app/presenters/track_test.rb
+++ test/app/presenters/track_test.rb
@@ -6,32 +6,31 @@ require 'mocha/setup'
 class PresentersTrackTest < Minitest::Test
   def setup
     @track = ExercismWeb::Presenters::Track.new("test_track")
+
+    trackler_tracks = Object.new
     @trackler_track = Object.new
 
-    @track.stubs(:trackler_track).returns(@trackler_track)
-    @track.stubs(:fallback_topic_content).returns("Default topic content")
-
+    Trackler.stubs(:tracks).returns(trackler_tracks)
+    trackler_tracks.stubs(:[]).returns(@trackler_track)
     @trackler_track.stubs(:repository).returns("https://github.com/exercism/test_track")
     @trackler_track.stubs(:doc_format).returns("md")
   end
 
   def test_docs_with_trackler_content
-    trackler_docs = OpenStruct.new(test_topic: "Track-specific content from Trackler")
-    @trackler_track.stubs(:docs).returns(trackler_docs)
-    topic_doc = @track.docs["test_topic"]
+    @trackler_track.stubs(:docs).returns(OpenStruct.new(about: "Track-specific content from Trackler"))
+    topic_doc = @track.docs["about"]
 
     assert topic_doc.include? "Track-specific content from Trackler"
     assert topic_doc.include? "Help us explain this better!"
     assert topic_doc.include? "File a GitHub issue at https://github.com/exercism/test_track"
-    assert topic_doc.include? "https://github.com/exercism/test_track/blob/master/docs/TEST_TOPIC.md"
+    assert topic_doc.include? "https://github.com/exercism/test_track/blob/master/docs/ABOUT.md"
   end
 
   def test_docs_without_trackler_content
-    trackler_docs = OpenStruct.new(test_topic: "")
-    @trackler_track.stubs(:docs).returns(trackler_docs)
-    topic_doc = @track.docs["test_topic"]
+    @trackler_track.stubs(:docs).returns(OpenStruct.new(about: ""))
+    topic_doc = @track.docs["about"]
 
-    assert topic_doc.include? "Default topic content"
+    assert topic_doc.include? "We're missing a short introduction about the language."
     refute topic_doc.include? "Help us explain this better!"
     refute topic_doc.include? "File a GitHub issue at https://github.com/exercism/test_track"
     refute topic_doc.include? "https://github.com/exercism/test_track/blob/master/docs/TEST_TOPIC.md"
