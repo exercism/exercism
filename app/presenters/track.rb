33cdb6812164e186d88287c83359fa81bb032204
Nick Holden <nick@realhq.com>
2017-04-28 22:20:06 -0700

    Check for template existance and read template in same method

--- app/presenters/track.rb
+++ app/presenters/track.rb
@@ -47,17 +47,17 @@ module ExercismWeb
       end
 
       def contributing_instructions(topic_key)
-        content_with_trackler_data("./x/docs/md/track/BETTER.md").gsub('TOPIC', topic_key.to_s.upcase)
+        populate_template_with_trackler_data(File.read("./x/docs/md/track/BETTER.md")).gsub('TOPIC', topic_key.to_s.upcase)
       end
 
       def fallback_topic_content(topic_key)
         filepath = "./x/docs/md/track/#{topic_key.upcase}.md"
         return '' unless File.exist?(filepath)
-        content_with_trackler_data(filepath)
+        populate_template_with_trackler_data(File.read(filepath))
       end
 
-      def content_with_trackler_data(filepath)
-        File.read(filepath).gsub('REPO', trackler_track.repository).gsub('EXT', trackler_track.doc_format)
+      def populate_template_with_trackler_data(template)
+        template.gsub('REPO', trackler_track.repository).gsub('EXT', trackler_track.doc_format)
       end
 
     end
