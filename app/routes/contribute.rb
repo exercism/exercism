7efbf2ce5ab949a1d6b8775626c3846eea5ba5a7
Katrina Owen <kytrinyx@github.com>
2017-06-03 15:14:07 -0600

    Use trackler specifications in place of problems
    This is a refactoring to support our new and improved choices for terminology for the
    Exercism project.
    
    See https://github.com/exercism/discussions/issues/145

--- app/routes/contribute.rb
+++ app/routes/contribute.rb
@@ -8,11 +8,11 @@ module ExercismWeb
       get '/contribute/canonical-data/?:slug?' do |slug|
         slug ||= ''
 
-        active_problems = Trackler.problems.reject(&:deprecated?).sort_by(&:name)
+        active_problems = Trackler.specifications.reject(&:deprecated?).sort_by(&:name)
         need_canonical  = active_problems.reject(&:canonical_data_url)
 
         erb :"contribute/canonical_data", locals: {
-          current_problem: Trackler.problems[slug],
+          current_problem: Trackler.specifications[slug],
           implementations: Trackler.implementations[slug],
           problems: need_canonical,
           active_problems_count: active_problems.size,
