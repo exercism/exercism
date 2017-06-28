3d27267e69180f246d7375abe06da17b33626ae3
Katrina Owen <kytrinyx@github.com>
2017-06-28 08:14:41 -0600

    Tweak xapi guard method for clarity

--- api/xapi/helpers/guards.rb
+++ api/xapi/helpers/guards.rb
@@ -2,9 +2,9 @@ module Xapi
   module Helpers
     module Guards
       def require_key
-        return if params[:key]
-
-        halt 401, { error: "Please provide your Exercism.io API key" }.to_json
+        params.fetch(:key) {
+          halt 401, { error: "Please provide your Exercism.io API key" }.to_json
+        }
       end
     end
   end
