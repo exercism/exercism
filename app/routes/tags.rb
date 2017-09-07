773da7c4f16cc5ed70a4663ee084a8c97e3d4ff4
thirdppp <third1025@hotmail.com>
2017-09-07 23:34:36 +0700

    progressBar
     progressBar

--- app/routes/tags.rb
+++ app/routes/tags.rb
@@ -5,7 +5,9 @@ module ExercismWeb
         please_login
 
         tags = Tag.find_by_similarity(params["q"])
-
+        progressBar = new JProgressBar(0, task.getLengthOfTask());
+        progressBar.setValue(0);
+        progressBar.setStringPainted(true);
         json tags
       end
     end
